8207c4101cdad07f6fcf16d1385498bc4e55750e
tqchen <tqchenml@gmail.com>
2023-05-26 10:46:59 -0400

    Gihhub actions for docs
    This PR adds GH actions for docs

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,6 @@
+MLC LLM Contributors
+====================
+
+
+## List of Contributors
+- [Full List of Contributors](https://github.com/mlc-ai/mlc-llm/graphs/contributors)
---
+++ scripts/gh_deploy_site.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# NOTE: this script is triggered by github action automatically
+# when megred into main
+
+set -euxo pipefail
+
+scripts/build_site.sh
+
+git fetch
+git checkout -B gh-pages origin/gh-pages
+rm -rf docs .gitignore
+mkdir -p docs
+cp -rf site/_site/* docs
+touch docs/.nojekyll
+
+DATE=`date`
+git add docs && git commit -am "Build at ${DATE}"
+git push origin gh-pages
+git checkout main && git submodule update
+echo "Finish deployment at ${DATE}"
