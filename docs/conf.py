127013c3bdcf6ac01943c5899e10b9f68a9d779b
tqchen <tqchenml@gmail.com>
2023-05-26 14:32:14 -0400

    Use tlcpack theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,83 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -*- coding: utf-8 -*-
+import tlcpack_sphinx_addon
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- General configuration ------------------------------------------------
 
-project = 'MLC-LLM'
-copyright = '2023, MLC-LLM community'
-author = 'MLC-LLM community'
+# General information about the project.
+project = "mlc-llm"
+author = "MLC LLM Contributors"
+copyright = "2023, %s" % author
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Version information.
+
+version = "0.1.0"
+release = "0.1.0"
 
 extensions = ['sphinx_tabs.tabs', 'sphinx_toolbox.collapse']
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = [".rst"]
+
+language = "en"
+
+exclude_patterns = ["_build", "Thumbs.db", '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
+# A list of ignored prefixes for module index sorting.
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+# -- Options for HTML output ----------------------------------------------
+
+# The theme is set by the make target
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+templates_path = []
+
+html_static_path = []
+
+footer_copyright = "© 2022 MLC LLM"
+footer_note = " "
+
+html_theme_options = {
+    "logo_only": True,
+}
+
+header_links = [
+    ("Home", "https://mlc.ai/mlc-llm"),
+    ("Github", "https://github.com/mlc-ai/mlc-llm"),
+    ("Discord Server", "https://discord.gg/9Xpy2HGBuD"),
+]
+
+header_dropdown = {
+    "name": "Other Resources",
+    "items": [
+        ("MLC Course", "https://mlc.ai/"),
+        ("MLC Blog", "https://mlc.ai/blog"),
+        ("Web LLM", "https://mlc.ai/web-llm"),
+    ],
+}
+
+html_context = {
+    "footer_copyright": footer_copyright,
+    "footer_note": footer_note,
+    "header_links": header_links,
+    "header_dropdown": header_dropdown,
+    "display_github": True,
+    "github_user": "mlc-ai",
+    "github_repo": "mlc-llm",
+    "github_version": "main/docs/",
+    "theme_vcs_pageview_mode": "edit",
+    # "header_logo": "path/to/logo",
+    # "header_logo_link": "",
+    # "version_selecter": "",
+}
 
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# add additional overrides
+templates_path += [tlcpack_sphinx_addon.get_templates_path()]
+html_static_path += [tlcpack_sphinx_addon.get_static_path()]
