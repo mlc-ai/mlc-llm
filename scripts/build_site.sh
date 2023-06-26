01718e5cff219dc4ac89e17fb413f1c92e156591
Zihao <expye@outlook.com>
2023-06-26 06:23:28 -0700

    debug

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -2,9 +2,6 @@
 set -euxo pipefail
 
 ln -s $(python -c "import site; print(site.getsitepackages()[0])")/mlc_chat/*.so python/mlc_chat/
+python -c "import mlc_chat; print(mlc_chat)"
+ls python/mlc_chat/
 cd docs && make html && cd ..
-
-cd site && jekyll b && cd ..
-
-rm -rf site/_site/docs
-cp -r docs/_build/html site/_site/docs
