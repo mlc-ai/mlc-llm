763a1709a75b32bfcfa02892399e871ed51b04b5
Zihao <expye@outlook.com>
2023-06-26 06:42:03 -0700

    fix typo

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-ln -s $(python -c "import mlc_chat; print(mlc_chat.__path__[0])"/*.so python/mlc_chat/
+ln -s $(python -c "import mlc_chat; print(mlc_chat.__path__[0])")/*.so python/mlc_chat/
 python -c "import mlc_chat; print(mlc_chat)"
 ls python/mlc_chat/
 cd docs && make html && cd ..
