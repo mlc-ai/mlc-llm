9cb1e4a8fdd6a89a54f9458cefa692cadf518f0c
Zihao <expye@outlook.com>
2023-06-26 06:46:56 -0700

    do not link if build exists

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-ln -s $(python -c "import mlc_chat; print(mlc_chat.__path__[0])")/*.so python/mlc_chat/
-python -c "import mlc_chat; print(mlc_chat)"
-ls python/mlc_chat/
+if [ ! -d "build"] then
+    mkdir build
+    ln -s $(python -c "import mlc_chat; print(mlc_chat.__path__[0])")/*.so build/
+else
+
 cd docs && make html && cd ..
