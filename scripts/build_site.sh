1807b7bfd19d7a7cb46625b73fc876e0239b0f48
Zihao <expye@outlook.com>
2023-06-26 06:49:46 -0700

    typo

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 if [ ! -d "build"] then
     mkdir build
     ln -s $(python -c "import mlc_chat; print(mlc_chat.__path__[0])")/*.so build/
-else
+fi
 
 cd docs && make html && cd ..
