a1458dae7d744c01589818c584269eac8f85a7f5
Zihao Ye <zihaoye.cs@gmail.com>
2023-06-26 07:58:09 -0500

    [Hotfix] Fix CPU wheels for Python API reference (#471)

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+ln -s $(python -c "import site; print(site.getsitepackages()[0])")/mlc_chat/*.so python/mlc_chat/
 cd docs && make html && cd ..
 
 cd site && jekyll b && cd ..
