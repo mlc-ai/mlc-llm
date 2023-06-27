87d8b04bfdea9525a5dcf753a00c08606ff9978e
Zihao Ye <zihaoye.cs@gmail.com>
2023-06-26 22:24:06 -0500

    Clean up documentation (#475)
    * cleanup doc
    
    * remove redundant instructions

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-ln -s $(python -c "import site; print(site.getsitepackages()[0])")/mlc_chat/*.so python/mlc_chat/
 cd docs && make html && cd ..
 
 cd site && jekyll b && cd ..
