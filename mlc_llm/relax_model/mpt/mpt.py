6b6fdf171d29de37b410e033a0e2523bf94fd530
Valery Chernov <valery.chernov@deelvin.com>
2023-06-27 21:56:33 +0300

    flash attn implementation was transferred to outside mpt model implementation

--- mlc_llm/relax_model/mpt/mpt.py
+++ mlc_llm/relax_model/mpt/mpt.py
@@ -163,188 +163,78 @@ def check_valid_inputs(*tensors, valid_dtypes=["float16", "bfloat16"]):
     #   raise TypeError(f'Inputs must be cuda tensors (tensor.is_cuda={tensor.is_cuda!r}).')
 
 
-######################### FLASH ATTENTION IMPLEMENTATION TYPE FLASH (BEGIN) ##########################
+def flash_attn_fn(
+    query,
+    key,
+    value,
+    n_heads,
+    d_model,
+    past_key_value=None,
+    softmax_scale=None,
+    attn_bias=None,
+    key_padding_mask=None,
+    is_causal=False,
+    needs_weights=False,
+    multiquery=False
+):
+  from ..flash_attn import bert_padding_unpad_input, bert_padding_pad_input, flash_attn_unpadded_func
+  check_valid_inputs(query, key, value)
+  if past_key_value is not None:
+    if len(past_key_value) != 0:
+      key = nn.emit(relax.op.concat([past_key_value[0], key], axis=1))
+      value = nn.emit(relax.op.concat([past_key_value[1], value], axis=1))
+    past_key_value = (key, value)
+  batch_size = query.struct_info.shape[0]
+  seqlen = query.struct_info.shape[1]
+  key_shape_d1 = key.struct_info.shape[1]
+  if attn_bias is not None:
+    _s_q = relax.op.maximum(relax.const(0), attn_bias.struct_info.shape[2] - seqlen)
+    _s_k = relax.op.maximum(relax.const(0), attn_bias.struct_info.shape[3] - key_shape_d1)
+    # slicing attn_bias[:, :, _s_q:, _s_k:]
+    s_q_end = attn_bias.struct_info.shape[2]
+    s_k_end = attn_bias.struct_info.shape[3]
+    attn_bias = nn.emit(relax.op.strided_slice(attn_bias, [2, 3], [_s_q, _s_k], [s_q_end, s_k_end]))
+  if attn_bias is not None:
+    raise NotImplementedError(f'attn_bias not implemented for flash attn.')
+  if key_padding_mask is None:
+    key_shape_d0 = key.struct_info.shape[0]
+    key_padding_mask = nn.emit(relax.op.ones(Tuple(key_shape_d0, key_shape_d1, 1), dtype="bool"))
+  # slicing key_padding_mask[:, -query.struct_info.shape[1]:]
+  dim1_length = key_padding_mask.struct_info.shape[1]
+  query_padding_mask = nn.emit(relax.op.strided_slice(key_padding_mask, [1], [dim1_length - seqlen], [dim1_length]))
+  (query_unpad, indices_q, cu_seqlens_q, max_seqlen_q) = bert_padding_unpad_input(query, query_padding_mask)
 
-# class IndexFirstAxis(torch.autograd.Function):
+  qnnz = query_unpad.struct_info.shape[0]
+  query_unpad = nn.emit(relax.op.reshape(
+      query_unpad,
+      (qnnz, n_heads, d_model),
+  ))  # (nnz, (h d)) -> (nnz, h, d)
 
-#     @staticmethod
-#     def forward(ctx, input, indices):
-#         ctx.save_for_backward(indices)
-#         assert input.ndim >= 2
-#         ctx.first_axis_dim, other_shape = input.shape[0], input.shape[1:]
-#         second_dim = other_shape.numel()
-#         # TD [2022-03-04] For some reason torch.gather is a bit faster than indexing.
-#         # return input[indices]
-#         return torch.gather(rearrange(input, 'b ... -> b (...)'), 0,
-#                             repeat(indices, 'z -> z d', d=second_dim)).reshape(-1, *other_shape)
+  kv_nnz = key_unpad.struct_info.shape[0]
+  kv_n_heads = 1 if multiquery else n_heads
+  (key_unpad, _, cu_seqlens_k, max_seqlen_k) = bert_padding_unpad_input(key, key_padding_mask)
+  key_unpad = nn.emit(relax.op.reshape(
+      key_unpad,
+      (kv_nnz, kv_n_heads, d_model),
+  ))  # (nnz, (h d)) -> (nnz, h, d)
+  (value_unpad, _, _, _) = bert_padding_unpad_input(value, key_padding_mask)
+  value_unpad = nn.emit(relax.op.reshape(
+      value_unpad,
+      (kv_nnz, kv_n_heads, d_model),
+  ))  # (nnz, (h d)) -> (nnz, h, d)
 
-
-# index_first_axis = IndexFirstAxis.apply
-
-# import torch
-# class IndexPutFirstAxis(torch.autograd.Function):
-
-#     @staticmethod
-#     def forward(ctx, values, indices, first_axis_dim):
-#         ctx.save_for_backward(indices)
-#         assert indices.ndim == 1
-#         assert values.ndim >= 2
-#         output = torch.zeros(first_axis_dim, *values.shape[1:], device=values.device,
-#                              dtype=values.dtype)
-#         # TD [2022-03-04] For some reason torch.scatter is a bit faster than indexing.
-#         output[indices] = values
-#         # output.scatter_(0, repeat(indices, 'z -> z d', d=values.shape[1]), values)
-#         return output
-
-
-# index_put_first_axis = IndexPutFirstAxis.apply
-
-
-# def bert_padding_unpad_input(hidden_states, attention_mask):
-#   seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
-#   indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-#   max_seqlen_in_batch = seqlens_in_batch.max().item()
-#   cu_seqlens = F.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.torch.int32), (1, 0))
-#   # TD [2022-03-04] We don't want to index with a bool mask, because Pytorch will expand the
-#   # bool mask, then call nonzero to get the indices, then index with those. The indices is @dim
-#   # times larger than it needs to be, wasting memory. It's faster and more memory-efficient to
-#   # index with integer indices. Moreover, torch's index is a bit slower than it needs to be,
-#   # so we write custom forward and backward to make it a bit faster.
-#   return (index_first_axis(rearrange(hidden_states, 'b s ... -> (b s) ...'), indices), indices,
-#           cu_seqlens, max_seqlen_in_batch)
-
-
-# def bert_padding_pad_input(hidden_states, indices, batch, seqlen):
-#     """
-#     Arguments:
-#         hidden_states: (total_nnz, ...), where total_nnz = number of tokens in selected in attention_mask.
-#         indices: (total_nnz)
-#     Return:
-#         hidden_states: (batch, seqlen, ...)
-#     """
-#     dim = hidden_states.shape[-1]
-#     # output = torch.zeros((batch * seqlen), dim, device=hidden_states.device, dtype=hidden_states.dtype)
-#     # output[indices] = hidden_states
-#     output = index_put_first_axis(hidden_states, indices, batch * seqlen)
-#     return rearrange(output, '(b s) ... -> b s ...', b=batch)
-
-# import flash_attn_cuda
-# def _flash_attn_forward(q, k, v, out, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k,
-#                         dropout_p, softmax_scale, causal, return_softmax, num_splits=0,
-#                         generator=None):
-#     """
-#     num_splits: how much to parallelize over the seqlen_q dimension. num_splits=0 means
-#     it will be set by an internal heuristic. We're exposing num_splits mostly for benchmarking.
-#     Don't change it unless you know what you're doing.
-#     """
-#     softmax_lse, rng_state, *rest = flash_attn_cuda.fwd(
-#         q, k, v, out, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, dropout_p,
-#         softmax_scale, False, causal, return_softmax, num_splits, generator
-#     )
-#     # if out.isnan().any() or softmax_lse.isnan().any():
-#     #     breakpoint()
-#     S_dmask = rest[0] if return_softmax else None
-#     return out, softmax_lse, rng_state, S_dmask
-
-
-# class FlashAttnFunc(torch.autograd.Function):
-#     @staticmethod
-#     def forward(ctx, q, k, v, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, dropout_p,
-#                 softmax_scale, causal, return_softmax, deterministic):
-#         if softmax_scale is None:
-#             softmax_scale = q.shape[-1] ** (-0.5)
-#         out, softmax_lse, rng_state, S_dmask = _flash_attn_forward(
-#             q, k, v, torch.empty_like(q), cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k,
-#             dropout_p, softmax_scale, causal=causal, return_softmax=return_softmax
-#         )
-#         ctx.save_for_backward(q, k, v, out, softmax_lse, cu_seqlens_q, cu_seqlens_k, rng_state)
-#         ctx.dropout_p = dropout_p
-#         ctx.max_seqlen_q = max_seqlen_q
-#         ctx.max_seqlen_k = max_seqlen_k
-#         ctx.softmax_scale = softmax_scale
-#         ctx.causal = causal
-#         ctx.deterministic = deterministic
-#         return out if not return_softmax else (out, softmax_lse, S_dmask)
-
-
-# def flash_attn_interface_flash_attn_unpadded_func(q, k, v, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k,
-#     dropout_p, softmax_scale=None, causal=False, return_attn_probs=False,
-#     deterministic=False
-# ):
-#   return FlashAttnFunc.apply(q, k, v, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k,
-#                               dropout_p, softmax_scale, causal, return_attn_probs, deterministic)
-
-
-# def flash_attn_fn(
-#     query,
-#     key,
-#     value,
-#     n_heads,
-#     d_model,
-#     past_key_value=None,
-#     softmax_scale=None,
-#     attn_bias=None,
-#     key_padding_mask=None,
-#     is_causal=False,
-#     needs_weights=False,
-#     multiquery=False
-# ):
-#   check_valid_inputs(query, key, value)
-#   if past_key_value is not None:
-#     if len(past_key_value) != 0:
-#       key = nn.emit(relax.op.concat([past_key_value[0], key], axis=1))
-#       value = nn.emit(relax.op.concat([past_key_value[1], value], axis=1))
-#     past_key_value = (key, value)
-#   if attn_bias is not None:
-#     _s_q = relax.op.maximum(relax.const(0), attn_bias.struct_info.shape[2] - query.struct_info.shape[1])
-#     _s_k = relax.op.maximum(relax.const(0), attn_bias.struct_info.shape[3] - key.struct_info.shape[1])
-#     # slicing attn_bias[:, :, _s_q:, _s_k:]
-#     s_q_end, s_k_end = attn_bias.struct_info.shape[-2:]
-#     attn_bias = nn.emit(relax.op.strided_slice(attn_bias, [2, 3], [_s_q, _s_k], [s_q_end, s_k_end]))
-#   if attn_bias is not None:
-#     raise NotImplementedError(f'attn_bias not implemented for flash attn.')
-#   batch_size, seqlen = query.struct_info.shape[:2]
-#   if key_padding_mask is None:
-#     key_shape = key.struct_info.shape[:2]
-#     key_padding_mask = nn.emit(relax.op.ones(Tuple(*key_shape, 1), dtype="bool"))
-#   # slicing key_padding_mask[:, -query.struct_info.shape[1]:]
-#   dim1_length = key_padding_mask.struct_info.shape[1]
-#   query_padding_mask = nn.emit(relax.op.strided_slice(key_padding_mask, [1], [dim1_length - seqlen], [dim1_length]))
-#   (query_unpad, indices_q, cu_seqlens_q, max_seqlen_q) = bert_padding_unpad_input(query, query_padding_mask)
-
-#   qnnz, _, _ = query_unpad.struct_info.shape
-#   query_unpad = nn.emit(relax.op.reshape(
-#       query_unpad,
-#       (qnnz, n_heads, d_model),
-#   ))  # (nnz, (h d)) -> (nnz, h, d)
-
-#   kv_nnz, _, _ = key_unpad.struct_info.shape
-#   kv_n_heads = 1 if multiquery else n_heads
-#   (key_unpad, _, cu_seqlens_k, max_seqlen_k) = bert_padding_unpad_input(key, key_padding_mask)
-#   key_unpad = nn.emit(relax.op.reshape(
-#       key_unpad,
-#       (kv_nnz, kv_n_heads, d_model),
-#   ))  # (nnz, (h d)) -> (nnz, h, d)
-#   (value_unpad, _, _, _) = bert_padding_unpad_input(value, key_padding_mask)
-#   value_unpad = nn.emit(relax.op.reshape(
-#       value_unpad,
-#       (kv_nnz, kv_n_heads, d_model),
-#   ))  # (nnz, (h d)) -> (nnz, h, d)
-
-#   if multiquery:
-#     key_unpad = relax.op.broadcast_to(key_unpad, (kv_nnz, n_heads, d_model))
-#     value_unpad = relax.op.broadcast_to(value_unpad, (kv_nnz, n_heads, d_model))
-#   reset_is_causal = _reset_is_causal(query.struct_info.shape[1], key.struct_info.shape[1], is_causal)
-#   output_unpad = flash_attn_interface_flash_attn_unpadded_func(query_unpad, key_unpad, value_unpad, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, 0.0, softmax_scale=softmax_scale, causal=reset_is_causal, return_attn_probs=needs_weights)
-#   nnz, _, _ = output_unpad.struct_info.shape
-#   output_unpad = nn.emit(relax.op.reshape(
-#       output_unpad,
-#       (nnz, n_heads*d_model),
-#   ))  # (nnz, h, d)) -> (nnz, (h d))
-#   output = bert_padding_pad_input(output_unpad, indices_q, batch_size, seqlen)
-#   return (output, None, past_key_value)
-
-######################### FLASH ATTENTION IMPLEMENTATION TYPE FLASH (END) ##########################
+  if multiquery:
+    key_unpad = relax.op.broadcast_to(key_unpad, (kv_nnz, n_heads, d_model))
+    value_unpad = relax.op.broadcast_to(value_unpad, (kv_nnz, n_heads, d_model))
+  reset_is_causal = _reset_is_causal(query.struct_info.shape[1], key_shape_d1, is_causal)
+  output_unpad = flash_attn_unpadded_func(query_unpad, key_unpad, value_unpad, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, 0.0, softmax_scale=softmax_scale, causal=reset_is_causal, return_attn_probs=needs_weights)
+  nnz = output_unpad.struct_info.shape[0]
+  output_unpad = nn.emit(relax.op.reshape(
+      output_unpad,
+      (nnz, n_heads*d_model),
+  ))  # (nnz, h, d)) -> (nnz, (h d))
+  output = bert_padding_pad_input(output_unpad, indices_q, batch_size, seqlen)
+  return (output, None, past_key_value)
 
 
 ######################### FLASH ATTENTION IMPLEMENTATION TYPE TRITON (BEGIN) ##########################
@@ -462,8 +352,7 @@ class MultiheadAttention(nn.Module):
       self.q_ln = layernorm_class(self.d_model, dtype)
       self.k_ln = layernorm_class(self.d_model, dtype)
     if self.attn_impl == 'flash':
-      raise NotImplemented("Flash type of flash attention has not been implemented yet")
-      # self.attn_fn = flash_attn_fn
+      self.attn_fn = flash_attn_fn
     elif self.attn_impl == 'triton':
       # While `attn_impl: triton` can be faster than `attn_impl: flash` it uses more memory.
       # When training larger models this can trigger alloc retries which hurts performance.
