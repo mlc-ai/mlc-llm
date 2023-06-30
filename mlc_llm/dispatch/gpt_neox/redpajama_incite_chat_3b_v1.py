1106733d68f0ade3f90a7bbe02a2806e616185e7
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-06-30 14:53:21 -0400

    Clean up `op_pattern` attr in all places
    Recently, the introduction of ParamManager in #477 changes the graph
    fusion pattern in certain level, which may cause TIR mismatch in
    the dispatch and/or database application of the build stage: on one
    side there exists the attr `op_pattern`, and on the other side there
    does not exist the attr now.
    
    Though some mismatches in dispatch/database application can be easily
    noticed due to rapid performance dropdown, some other mismatches might
    be hard to catch. These mismatches may contribute to performance
    degradation alltogether.
    
    Therefore, this PR introduces a pass that cleans up the `op_pattern`
    attrs in the IRModule. This PR also removes all the `op_pattern` attrs
    in existing databases and dispatches.
    
    With this PR, we no longer need to worry about TIR mismatch due to
    atttributes.

--- mlc_llm/dispatch/gpt_neox/redpajama_incite_chat_3b_v1.py
+++ mlc_llm/dispatch/gpt_neox/redpajama_incite_chat_3b_v1.py
@@ -811,7 +811,7 @@ def fused_min_max_triu_te_broadcast_to(sch: tir.Schedule):
 
 @T.prim_func
 def softmax_mxn_before(var_rxplaceholder: T.handle, var_T_softmax_norm: T.handle):
-    T.func_attr({"op_pattern": 4, "tir.noalias": T.bool(True)})
+    T.func_attr({"tir.noalias": T.bool(True)})
     n = T.int64()
     m = T.int64()
     rxplaceholder = T.match_buffer(var_rxplaceholder, (T.int64(1), T.int64(32), n, m))
@@ -853,7 +853,7 @@ def softmax_mxn_before(var_rxplaceholder: T.handle, var_T_softmax_norm: T.handle
 
 @T.prim_func
 def softmax_mxn_after(var_A: T.handle, var_T_softmax_norm: T.handle):
-    T.func_attr({"op_pattern": 4, "tir.noalias": T.bool(True), "tir.is_scheduled": 1})
+    T.func_attr({"tir.noalias": T.bool(True), "tir.is_scheduled": 1})
     n = T.int64()
     m = T.int64()
     A = T.match_buffer(var_A, (T.int64(1), T.int64(32), n, m))
