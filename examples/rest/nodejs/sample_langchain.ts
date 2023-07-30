698d6c285c08de56503701de48d17175a6c5938e
SingLi <Sing-Li@users.noreply.github.com>
2023-07-30 11:27:52 -0500

    Add nodejs access examples for REST APIs (#602)
    Entry point for mainstream node/JS/TS developers. 
    
    Add NodeJS streaming and non-streaming access examples for the REST
    APIs.
    
    Based on the python examples from @ sudeepag 🙏

---
+++ examples/rest/nodejs/sample_langchain.ts
@@ -0,0 +1,75 @@
+import { OpenAI } from "langchain/llms/openai";
+import { BufferWindowMemory } from "langchain/memory";
+import { LLMChain } from "langchain/chains";
+import { PromptTemplate } from "langchain/prompts";
+import {TextLoader } from "langchain/document_loaders/fs/text";
+import { loadQAStuffChain } from "langchain/chains";
+
+const color = { 
+    PURPLE : '\x1b[95m',
+    CYAN : '\x1b[96m',
+    DARKCYAN : '\x1b[36m',
+    BLUE : '\x1b[94m',
+    GREEN : '\x1b[92m',
+    YELLOW : '\x1b[93m',
+    RED : '\x1b[91m',
+    BOLD : '\x1b[1m',
+    UNDERLINE : '\x1b[4m',
+    END : '\x1b[0m'
+};
+
+function print(str: string) {
+    process.stdout.write(str);
+}
+
+const newline = () => {
+    print('\n');
+}
+  
+  const chat = new OpenAI( {
+      openAIApiKey: "empty",
+      temperature: 0 
+    },   {
+        basePath: 'http://127.0.0.1:8000/v1'
+    });
+ 
+// Conversational LLMChain example
+  const memory = new BufferWindowMemory({ memoryKey: "history", k: 1 });
+
+  const template = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+    Current conversation:
+    {history}
+    Human: {human_input}
+    AI:`;
+
+
+  const prompt = PromptTemplate.fromTemplate(template);
+  let chain = new LLMChain({ llm: chat, prompt, memory });
+
+  let input = "Write a poem about Pittsburgh.";
+  print(color.BOLD + input + "..." + color.END);
+  newline();
+  let res = await chain.call({ human_input:  input });
+  newline();
+  print(color.GREEN + res.text + color.END);
+  newline();
+  input = "What does it mean?";
+  print(color.BOLD + input + "..." + color.END);
+  newline();
+  res = await chain.call({ human_input: input });
+  newline();
+  print(color.GREEN + res.text + color.END);
+  newline();
+  
+// Question and answer stuff chain example with text loader
+const loader = new TextLoader('../resources/linux.txt');
+const documents = await loader.load();
+const schain =  loadQAStuffChain(chat);
+const query = "When was Linux released?";
+newline(); newline();
+print(color.BOLD + "Query: " + color.END + color.BLUE + query + color.END);
+newline();
+const result = await schain.call({ input_documents: documents,  question: query});
+print(color.BOLD + "Response: " + color.END +  color.GREEN + result.text  + color.END);
+
---
+++ examples/rest/nodejs/sample_openai.js
@@ -0,0 +1,77 @@
+import { Configuration, OpenAIApi }  from "openai";
+import dotenv from "dotenv";
+dotenv.config();
+
+( async () =>  {
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    basePath : process.env.OPENAI_API_BASE
+})
+const openai = new OpenAIApi(configuration);
+let model = "vicuna-v1-7b"
+
+const color = { 
+    PURPLE : '\x1b[95m',
+    CYAN : '\x1b[96m',
+    DARKCYAN : '\x1b[36m',
+    BLUE : '\x1b[94m',
+    GREEN : '\x1b[92m',
+    YELLOW : '\x1b[93m',
+    RED : '\x1b[91m',
+    BOLD : '\x1b[1m',
+    UNDERLINE : '\x1b[4m',
+    END : '\x1b[0m'
+};
+
+const print = ( str ) => {
+    process.stdout.write(str);
+};
+
+const newline = () => {
+    print('\n');
+}
+
+// Chat completion example without streaming
+newline();
+print(color.BOLD + "OpenAI chat completion example without streaming:" + color.END);
+newline();
+
+let completion = await openai.createChatCompletion({
+  model: model,
+  messages: [{"role": "user", "content": "Write a poem about OpenAI"}]
+});
+
+
+print(color.GREEN + completion.data.choices[0].message.content + color.END)
+newline();  newline();
+
+
+// Chat completion example with streaming  
+// (raw implementation since npm module does not support it yet - it will have support in upcoming 4.x)
+
+print(color.BOLD + "OpenAI chat completion example with streaming:" + color.END);
+newline();
+completion = await openai.createChatCompletion({
+    model: model,
+    messages: [{"role": "user", "content": "Write a poem about OpenAI"}],
+    stream: true,
+}, {responseType: 'stream'});
+
+completion.data.on('data', async (data) => {
+        const parsed = JSON.parse(data.toString().substring(6));
+        print(color.GREEN + parsed.choices[0].delta.content + color.END);
+});
+
+completion.data.on('close', async ()  => {
+    newline(); newline();
+
+    // Completion example
+    print(color.BOLD + "OpenAI completion example:" + color.END)
+    newline();
+    let res = await openai.createCompletion({ prompt: "Write a poem about OpenAI", model: model});
+    print(color.GREEN + res.data.choices[0].text + color.END);
+    newline();  newline();
+
+    });       
+})()
