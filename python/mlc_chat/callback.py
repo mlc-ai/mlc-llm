d3010ebf76338aae0ac5bb1d943b28bf0a9cce4f
Sudeep Agarwal <hello@sudeep.co>
2023-08-08 01:03:40 -0400

    Add new callback for updating class variable

--- python/mlc_chat/callback.py
+++ python/mlc_chat/callback.py
@@ -29,3 +29,25 @@ class stream_to_stdout(object):
             print()
         else:
             print(message, end="", flush=True)
+
+
+class update_var(object):
+
+    def __init__(self):
+        r"""Initialize the callback class.
+        """
+        self.var = ""
+        self.interval = 0
+
+    def __call__(self, message: str = "", stopped: bool = False):
+        r"""Update `var` with the delta message.
+
+        Parameters
+        ----------
+        message : str
+            The new piece of message that has not been added to `var` yet.
+        stopped : bool
+            Whether streaming reaches the end.
+        """
+        if not stopped:
+            self.var += message
