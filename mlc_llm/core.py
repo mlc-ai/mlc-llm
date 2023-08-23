mlc-ai/mlc-llm

commit 4127782c338ed6341474a707790b33d635a690ce

Update Llama2 cached sin/cos to use max_sequence_length (#780)
* Update the llama2 model to use max sequence length for the cached sin and cos functions.

* Comment change

* Fix style

mlc_llm/core.py
@@ -358,7 +358,10 @@ def mod_transform_before_build(
     )  # pylint: disable=not-callable
 
     if "num_attention_heads" in config and "hidden_size" in config:
-        mod = fuse_split_rotary_embedding(mod, config["num_attention_heads"], config["hidden_size"])
+        if args.max_seq_len:
+            mod = fuse_split_rotary_embedding(mod, config["num_attention_heads"], config["hidden_size"], args.max_seq_len)
+        else:
+            mod = fuse_split_rotary_embedding(mod, config["num_attention_heads"], config["hidden_size"])
 
     if args.target_kind == "cuda":
         patterns = []

mlc_llm/transform/fuse_split_rotary_embedding.py
@@ -13,7 +13,7 @@ from tvm.relax.dpl import (
 from tvm.script import relax as R
 
 
-def get_split_rotary(num_attention_heads, head_dim):
+def get_split_rotary(num_attention_heads, head_dim, max_sequence_length=2048):
     hidden_size = num_attention_heads * head_dim
 
     @T.prim_func
@@ -27,8 +27,8 @@ def get_split_rotary(num_attention_heads, head_dim):
         n: T.int64,
     ):
         A = T.match_buffer(qkv, [1, 1, hidden_size * 3], dtype="float16")
-        cos = T.match_buffer(cos_h, [2048, head_dim], dtype="float16")
-        sin = T.match_buffer(sin_h, [2048, head_dim], dtype="float16")
+        cos = T.match_buffer(cos_h, [max_sequence_length, head_dim], dtype="float16")
+        sin = T.match_buffer(sin_h, [max_sequence_length, head_dim], dtype="float16")
         T_split = T.match_buffer(split_0, [1, 1, hidden_size], dtype="float16")
         T_split_1 = T.match_buffer(split_1, [1, 1, hidden_size], dtype="float16")
         T_split_2 = T.match_buffer(split_2, [1, 1, hidden_size], dtype="float16")
@@ -75,10 +75,11 @@ def get_split_rotary(num_attention_heads, head_dim):
     return split_rotary
 
 
-def fuse_split_rotary_embedding(mod, num_attention_heads, hidden_size):
+def fuse_split_rotary_embedding(mod, num_attention_heads, hidden_size, max_sequence_length=2048):
     head_dim = hidden_size // num_attention_heads
+    print(f"fuse_split_rotary_embedding {max_sequence_length}")
 
-    mod["split_rotary"] = get_split_rotary(num_attention_heads, head_dim)
+    mod["split_rotary"] = get_split_rotary(num_attention_heads, head_dim, max_sequence_length)
 
     gvar = mod.get_global_var("split_rotary")
     relax.expr._update_struct_info(gvar, mod.get_global_var("rotary_embedding1").struct_info)

mlc_llm/relax_model/llama.py
@@ -612,13 +612,13 @@ class LlamaForCausalLM(nn.Module):
         assert config.hidden_size % config.num_attention_heads == 0
         head_dim = config.hidden_size // config.num_attention_heads
 
-        # Hardcode the cached sin/cos for 2048.
+        # Set the cached sin/cos to the max seq len.
         # This will be eliminated further with online rotary embedding calculation.
         self.cos_cached = nn.Parameter(
-            (2048, head_dim), dtype=config.dtype, name="cos_cached"
+            (config.max_sequence_length, head_dim), dtype=config.dtype, name="cos_cached"
         )
         self.sin_cached = nn.Parameter(
-            (2048, head_dim), dtype=config.dtype, name="sin_cached"
+            (config.max_sequence_length, head_dim), dtype=config.dtype, name="sin_cached"
         )
         ############ End ############
 
@@ -955,9 +955,9 @@ def get_model(args, hf_config):
         config.position_embedding_base
         ** (np.arange(0, head_dim, 2).astype("float32") / head_dim)
     )
-    # Hardcode the cached sin/cos for 2048.
+    # Set the cached sin/cos to the max sequence length.
     # This will be eliminated further with online rotary embedding calculation.
-    t = np.arange(2048, dtype=inv_freq.dtype)
+    t = np.arange(config.max_sequence_length, dtype=inv_freq.dtype)
     freqs = np.einsum("i,j->ij", t, inv_freq)
     emb = np.concatenate((freqs, freqs), axis=-1)
     param_list[-2] = tvm.nd.array(np.cos(emb).astype(config.dtype), device)

commit 87f19bf127bc069f9002a2dbf2e15b5840b42287

[FIX] Fix error `max_seq_len == -1`
This one fixes error introduced in #780, where `max_seq_len` is set to
`-1` when `max_seq_len` is not specified in the config file.

mlc_llm/core.py
@@ -358,7 +358,7 @@ def mod_transform_before_build(
     )  # pylint: disable=not-callable
 
     if "num_attention_heads" in config and "hidden_size" in config:
-        if args.max_seq_len:
+        if args.max_seq_len != -1:
             mod = fuse_split_rotary_embedding(mod, config["num_attention_heads"], config["hidden_size"], args.max_seq_len)
         else:
             mod = fuse_split_rotary_embedding(mod, config["num_attention_heads"], config["hidden_size"])

mlc_llm/transform/fuse_split_rotary_embedding.py
@@ -77,7 +77,6 @@ def get_split_rotary(num_attention_heads, head_dim, max_sequence_length=2048):
 
 def fuse_split_rotary_embedding(mod, num_attention_heads, hidden_size, max_sequence_length=2048):
     head_dim = hidden_size // num_attention_heads
-    print(f"fuse_split_rotary_embedding {max_sequence_length}")
 
     mod["split_rotary"] = get_split_rotary(num_attention_heads, head_dim, max_sequence_length)
 