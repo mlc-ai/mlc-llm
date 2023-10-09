cfbf95b77ed98d24ca4852841813b7e1eacc072b
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-10-09 17:15:27 -0400

    [CPP] Separate common utils out from llm_chat.cc
    This PR separates out the tokenizer creation function, the
    random number generator out from `llm_chat.cc` as a preparation
    step for batching inference support, since these functions/modules
    are also used in the same way in batching inference.

--- cpp/llm_chat.h
+++ cpp/llm_chat.h
@@ -6,17 +6,7 @@
 #include <tvm/runtime/container/string.h>
 #include <tvm/runtime/module.h>
 
-#ifndef MLC_LLM_DLL
-#ifdef _WIN32
-#ifdef MLC_LLM_EXPORTS
-#define MLC_LLM_DLL __declspec(dllexport)
-#else
-#define MLC_LLM_DLL __declspec(dllimport)
-#endif
-#else
-#define MLC_LLM_DLL __attribute__((visibility("default")))
-#endif
-#endif
+#include "base.h"
 
 namespace mlc {
 namespace llm {
