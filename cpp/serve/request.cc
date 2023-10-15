059e46b6d5144c692812761a3ec6ebce6ae3787f
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-10-15 09:13:50 -0400

    [Serving] Python-side data structure abstractions (#1066)
    This PR introduces the Python-side abstraction for the basic
    data structures introduced in #1046. These abstraction in Python
    leverages the object system and FFI in TVM.

--- cpp/serve/request.cc
+++ cpp/serve/request.cc
@@ -6,6 +6,7 @@
 #include "request.h"
 
 #include <tvm/runtime/packed_func.h>
+#include <tvm/runtime/registry.h>
 
 #include "data.h"
 
@@ -17,13 +18,27 @@ namespace serve {
 
 TVM_REGISTER_OBJECT_TYPE(RequestNode);
 
-Request::Request(Array<Data> inputs, String generation_cfg_json, PackedFunc fcallback) {
+Request::Request(Array<Data> inputs, GenerationConfig generation_cfg, PackedFunc fcallback) {
   ObjectPtr<RequestNode> n = make_object<RequestNode>();
   n->inputs = std::move(inputs);
-  n->generation_cfg = GenerationConfig(generation_cfg_json);
+  n->generation_cfg = std::move(generation_cfg);
   n->fcallback = std::move(fcallback);
   data_ = std::move(n);
 }
+
+TVM_REGISTER_GLOBAL("mlc.serve.Request")
+    .set_body_typed([](Array<Data> inputs, String generation_cfg_json, PackedFunc fcallback) {
+      return Request(std::move(inputs), GenerationConfig(std::move(generation_cfg_json)),
+                     std::move(fcallback));
+    });
+
+TVM_REGISTER_GLOBAL("mlc.serve.RequestGetInputs").set_body_typed([](Request request) {
+  return request->inputs;
+});
+
+TVM_REGISTER_GLOBAL("mlc.serve.RequestGetGenerationConfigJSON").set_body_typed([](Request request) {
+  return request->generation_cfg->AsJSONString();
+});
 
 }  // namespace serve
 }  // namespace llm
