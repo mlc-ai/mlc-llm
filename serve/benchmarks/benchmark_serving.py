Performance improvement and cache size calculation bug fix (#28)

serve/benchmarks/benchmark_serving.py
@@ -7,9 +7,7 @@ from typing import AsyncGenerator, List, Tuple
 
 import aiohttp
 import numpy as np
-from transformers import PreTrainedTokenizerBase
-from vllm.transformers_utils.tokenizer import get_tokenizer
-
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
 
 # (prompt len, output len, latency)
 REQUEST_LATENCY: List[Tuple[int, float]] = []
@@ -145,7 +143,8 @@ def main(args: argparse.Namespace):
     np.random.seed(args.seed)
 
     api_url = f"http://127.0.0.1:8000/v1/chat/completions"
-    tokenizer = get_tokenizer(args.tokenizer, trust_remote_code=False)
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer, trust_remote_code=False)
+
     input_requests = sample_requests(args.dataset, args.num_prompts, tokenizer)
 
     benchmark_start_time = time.time()