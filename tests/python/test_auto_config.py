16dd2aeaf2da7f6b3d7188dc3f0778eb2c53ac13
Lesheng Jin <34279105+LeshengJin@users.noreply.github.com>
2023-10-22 19:51:10 -0700

    [Slim-LM] Smart path finding for config and weight (#1088)

---
+++ tests/python/test_auto_config.py
@@ -0,0 +1,44 @@
+# pylint: disable=missing-docstring
+import json
+import logging
+import tempfile
+from pathlib import Path
+
+import pytest
+from mlc_chat.support.auto_config import detect_config
+
+logging.basicConfig(
+    level=logging.INFO,
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="{asctime} {levelname} {filename}:{lineno}: {message}",
+)
+
+
+def _create_json_file(json_path, data):
+    with open(json_path, "w", encoding="utf-8") as i_f:
+        json.dump(data, i_f)
+
+
+def test_detect_config():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        base_path = Path(tmpdir)
+        config_json_path = base_path / "config.json"
+        _create_json_file(config_json_path, {})
+
+        assert detect_config(base_path) == config_json_path
+        assert detect_config(config_json_path) == config_json_path
+
+
+def test_detect_config_fail():
+    with pytest.raises(ValueError):
+        detect_config(Path("do/not/exist"))
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        base_path = Path(tmpdir)
+        with pytest.raises(ValueError):
+            assert detect_config(base_path)
+
+
+if __name__ == "__main__":
+    pass
