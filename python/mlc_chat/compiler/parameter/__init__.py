cf2e871b623ebee8f99e722096766f49f6e8afe6
Lesheng Jin <leshenj15@gmail.com>
2023-10-23 20:29:13 +0000

    Introduce HFLoad for loading Pytorch and SafeTensor weights

--- python/mlc_chat/compiler/parameter/__init__.py
+++ python/mlc_chat/compiler/parameter/__init__.py
@@ -2,5 +2,5 @@
 A subpackage of the compiler that represents mapping between external parameters, quantized
 parameters and parameters in MLC-defined models.
 """
-from .hf_torch_loader import HFTorchLoader
+from .hf_loader import HFLoader
 from .mapping import ExternMapping, QuantizeMapping
