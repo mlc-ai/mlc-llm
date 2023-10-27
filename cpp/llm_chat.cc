28cf7515034cd28a2f31c6741ac0e8723f90f2b7
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2023-10-27 09:34:37 +0800

    fix llm_chat.cc rwkv bug

--- cpp/llm_chat.cc
+++ cpp/llm_chat.cc
@@ -1109,7 +1109,7 @@ class LLMChat {
     if (static_cast<int64_t>(output_ids_.size()) >= gen_max_gen_len) {
       stop_triggered_ = true;
     } 
-    // max_window_size_ != -1 to handle https://github.com/GiantPandaCV/mlc-llm/blob/main/mlc_llm/relax_model/rwkv.py#L588-L589
+    // max_window_size_ != -1 to handle https://github.com/mlc-ai/mlc-llm/blob/main/mlc_llm/relax_model/rwkv.py#L588-L589
     else if (max_window_size_ != -1 && total_seq_len_ >= max_window_size_) {
       stop_triggered_ = true;
     }
