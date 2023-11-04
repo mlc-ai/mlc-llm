7b2bc1c3e400ae5862751754dca246231f8db561
Junru Shao <junrushao@apache.org>
2023-11-04 01:26:48 -0700

    Merge `llama_config.CONFIG` into `MODEL_PRESETS`

--- tests/python/model/test_llama.py
+++ tests/python/model/test_llama.py
@@ -1,15 +1,15 @@
 # pylint: disable=invalid-name,missing-docstring
 import pytest
 
-from mlc_chat.compiler import MODELS
+from mlc_chat.compiler import MODEL_PRESETS, MODELS
 
 
 @pytest.mark.parametrize("model_name", ["llama2_7b", "llama2_13b", "llama2_70b"])
 def test_llama2_creation(model_name: str):
-    model_info = MODELS["llama"]
-    config = model_info.config.from_predefined(model_name)
-    model = model_info.model(config)
-    mod, named_params = model.export_tvm(spec=model.get_default_spec())
+    model = MODELS["llama"].model(MODEL_PRESETS[model_name])
+    mod, named_params = model.export_tvm(
+        spec=model.get_default_spec(),  # type: ignore
+    )
     mod.show(black_format=False)
     for name, param in named_params:
         print(name, param.shape, param.dtype)
