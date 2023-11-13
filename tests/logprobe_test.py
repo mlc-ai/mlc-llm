7c23aadc22a1f0c2d76108a6977b4e46e4b390ce
Valery Chernov <valery.chernov@deelvin.com>
2023-11-13 09:36:16 +0400

    refactor test script for logprob interface

--- tests/logprobe_test.py
+++ tests/logprobe_test.py
@@ -4,23 +4,15 @@ import json
 # Get a response using a prompt without streaming
 payload = {
    "model": "Llama-2-7b-chat-hf-q0f16",
-   "prompt": "",    # required parameter, do it dummy
    "context": "Cheerleading: A group of cheerleaders are seen walking on stage while the audience cheers. The group",
    "continuation": " start performing cheer roping on a fake stage while many watch from the stands.",
-   "stream": False,
-   "logprobs": True
 }
-response = requests.post("http://127.0.0.1:8000/v1/completions", json=payload)
+response = requests.post("http://127.0.0.1:8000/v1/logprob", json=payload)
 
 if response.status_code != 200:
    print(f"Error: {response.status_code} - {response.text}")
 
 response = json.loads(response.text)
-logprob_dict = json.loads(response['choices'][0]['text'])
-log_probes = logprob_dict["logprobes"]
-is_greedy = logprob_dict["is_greedy"]
+log_probes = response["logprob"]       # expected -84.03089141845703
+is_greedy = response["is_greedy"]      # expected False
 print(f"Logprobs: {log_probes}; Is_greedy: {is_greedy}")
-
-# Get the latest runtime stats
-r = requests.get("http://127.0.0.1:8000/stats")
-print(f"Runtime stats: {r.json()}\n")
