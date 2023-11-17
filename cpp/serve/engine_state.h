e006ac7fb695e190b0b545dffdb8801773eab2d8
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-11-17 17:35:54 -0500

    [Serving][Refactor] EngineState and EngineAction (#1285)
    Following #1277, this PR continues to refactor the engine.
    
    Particularly, this PR introduces the `EngineState` object
    to denote the running state of an Engine, and the `EngineAction`
    abstraction to denote the action (such as prefill, decode,
    speculate, etc.) that the engine can take at each time step.
    
    The EngineState consists of
    * the queues of running requests, waiting requests and the requests
    to be aborted,
    * the request states of all requests,
    * the engine runtime statistics.
    
    The EngineAction contains the core interface
    ```c++
    bool Step(EngineState estate)
    ```
    which takes the current engine state as input and then update
    the engine state after taking the action. With the introduction
    of EngineAction, we now further move a bunch of detailed implementation
    functions under Engine to their respective actions.

---
+++ cpp/serve/engine_state.h
@@ -0,0 +1,103 @@
+/*!
+ *  Copyright (c) 2023 by Contributors
+ * \file serve/engine_state.h
+ */
+#ifndef MLC_LLM_SERVE_ENGINE_STATE_H_
+#define MLC_LLM_SERVE_ENGINE_STATE_H_
+
+#include <tvm/runtime/container/string.h>
+
+#include "request.h"
+#include "request_state.h"
+
+namespace mlc {
+namespace llm {
+namespace serve {
+
+using namespace tvm::runtime;
+
+/*! \brief Runtime statistics of engine. */
+struct EngineStats {
+  /*! \brief The current total sequence length in the first model. */
+  int64_t current_total_seq_len;
+  /*! \brief The sum of "prefill time of each request". */
+  double request_total_prefill_time = 0.0f;
+  /*! \brief The sum of "decode time of each request". */
+  double request_total_decode_time = 0.0f;
+  /*! \brief The total engine time on prefill. */
+  double engine_total_prefill_time = 0.0f;
+  /*! \brief The total engine time on decode. */
+  double engine_total_decode_time = 0.0f;
+  /*! \brief The total number of processed tokens in prefill. */
+  int64_t total_prefill_length = 0;
+  /*! \brief The total number of processed tokens in decode. */
+  int64_t total_decode_length = 0;
+
+  /*!
+   * \brief Return the engine runtime statistics in JSON string.
+   * We collect the following entries:
+   * - single token prefill latency (s/tok): avg latency of processing one token in prefill
+   * - single token decode latency (s/tok): avg latency of processing one token in decode
+   * - engine time for prefill (sec)
+   * - engine time for decode (sec)
+   * - total number of processed tokens in prefill.
+   * - total number of processed tokens in decode.
+   * \return The statistics in JSON string.
+   */
+  String AsJSON() const;
+  /*! \brief Reset all the statistics. */
+  void Reset();
+};
+
+/*!
+ * \brief The state of the running engine.
+ * It contains the requests and their states submitted to the Engine.
+ */
+class EngineStateObj : public Object {
+ public:
+  /*! \brief The requests being processed. */
+  std::vector<Request> running_queue;
+  /*! \brief The requests that have not started for process yet. */
+  std::vector<Request> waiting_queue;
+  /*! \brief The requests to abort. */
+  std::vector<Request> abort_queue;
+  /*! \brief The states of all requests. */
+  std::unordered_map<String, RequestState, ObjectPtrHash, ObjectPtrEqual> request_states;
+  /*! \brief Runtime statistics. */
+  EngineStats stats;
+
+  /*! \brief Reset the engine state and clear the statistics. */
+  void Reset();
+  /*! \brief Get the request state of the given request. */
+  RequestState GetRequestState(Request request);
+
+  static constexpr const char* _type_key = "mlc.serve.EngineState";
+  static constexpr const bool _type_has_method_sequal_reduce = false;
+  static constexpr const bool _type_has_method_shash_reduce = false;
+  TVM_DECLARE_FINAL_OBJECT_INFO(EngineStateObj, Object);
+};
+
+/*!
+ * \brief Managed reference of EngineStateObj.
+ * \sa EngineStateObj
+ */
+class EngineState : public ObjectRef {
+ public:
+  explicit EngineState();
+
+  // Default constructors.
+  EngineState(const EngineState& other) = default;
+  EngineState(EngineState&& other) = default;
+  EngineState& operator=(const EngineState& other) = default;
+  EngineState& operator=(EngineState&& other) = default;
+
+  explicit EngineState(::tvm::runtime::ObjectPtr<::tvm::runtime::Object> n) : ObjectRef(n) {}
+  EngineStateObj* operator->() const { return static_cast<EngineStateObj*>(data_.get()); }
+  using ContainerType = EngineStateObj;
+};
+
+}  // namespace serve
+}  // namespace llm
+}  // namespace mlc
+
+#endif  // MLC_LLM_SERVE_ENGINE_STATE_H_
