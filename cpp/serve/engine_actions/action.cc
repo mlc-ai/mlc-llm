273995efcc69d590f9ea1b21e91ef1c7009a2ede
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-11-17 17:35:54 -0500

    [Serving][Refactor] EngineState and EngineAction (#1285)
    Following #1277, this PR continues to refactor the engine.
    
    Particularly, this PR introduces the `EngineState` object
    to denote the running state of an Engine, and the `EngineAction`
    abstraction to denote the action (such as prefill, decode,
    speculate, etc.) that the engine can take at each time step.
    
    The EngineState consists of
    * the queues of running requests, waiting requests and the requests
    to be aborted,
    * the request states of all requests,
    * the engine runtime statistics.
    
    The EngineAction contains the core interface
    ```c++
    bool Step(EngineState estate)
    ```
    which takes the current engine state as input and then update
    the engine state after taking the action. With the introduction
    of EngineAction, we now further move a bunch of detailed implementation
    functions under Engine to their respective actions.

---
+++ cpp/serve/engine_actions/action.cc
@@ -0,0 +1,16 @@
+/*!
+ *  Copyright (c) 2023 by Contributors
+ * \file serve/engine_actions/action.cc
+ */
+
+#include "action.h"
+
+namespace mlc {
+namespace llm {
+namespace serve {
+
+TVM_REGISTER_OBJECT_TYPE(EngineActionObj);
+
+}  // namespace serve
+}  // namespace llm
+}  // namespace mlc
