e9c8da5d395b9e1bebbaae955d03c27b01435ed0
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-11-17 13:57:06 -0500

    [Serving][Refactor] Major codebase refactor (#1277)
    This PR is a major refactor of the serving framework.
    It contains the following aspects:
    
    * Changing Model and Sampler from `runtime.Module` to Object
    in TVM. Exposing the public interface through public member
    function (rather than through `GetFunction` which returns PackedFunc).
    Separating the definition and implementation of Model/Sampler
    classes. Now a base Model/Sampler class definition is in their
    respective header files, and the implementations are in .cc files.
    * Removing the TokenizerModule class, and directly using the
    Tokenizer class in `tokenizer_cpp`, to reduce indirection.
    * Introducing unique string `id` to Request. This id is passed
    in from frontend as a Request constructor parameter, and is the
    unique identifier of a request.
    * Reducing the uses of `ShapeTuple` after the Model/Sampler/Tokenizer
    interface changes.
    * Moving some previous member functions in Engine (such as "getting
    data length", "getting data embedding") to the corresponding
    data structure side.
    * Introducing struct `EngineStats` to contain all the runtime
    statistics of the engine.
    * Classifying and reordering the member functions in Engine.

--- cpp/serve/sampler.h
+++ cpp/serve/sampler.h
@@ -11,6 +11,8 @@
 #include <tvm/runtime/module.h>
 
 #include "../base.h"
+#include "model.h"
+#include "request_state.h"
 
 namespace mlc {
 namespace llm {
@@ -20,11 +22,45 @@ using tvm::Device;
 using namespace tvm::runtime;
 
 /*!
- * \brief Create the runtime module for sampler functions.
- * \param device The device to run the sampling-related functions on.
- * \return The created runtime module.
+ * \brief The base class of runtime sampler.
+ * Its main function is `SampleTokens`, which takes a batch of
+ * logits and corresponding configuration, and sample one token
+ * for each instance of the batch.
  */
-MLC_LLM_DLL tvm::runtime::Module CreateSamplerModule(DLDevice device);
+class SamplerObj : public Object {
+ public:
+  /*!
+   * \brief Sample tokens from the input batch of logits.
+   * \param logits_on_device The logits to sample tokens from.
+   * \param model The LLM model which contains the softmax
+   * function on device that might be used to compute probability distribution.
+   * \param request_mstates The request states of each sequence in
+   * the batch with regard to the given model.
+   * \param generation_cfg The generation config of each request
+   * in the input batch.
+   * \return The sampled tokens, one for each request in the batch.
+   */
+  virtual std::vector<int32_t> SampleTokens(NDArray logits_on_device, Model model,
+                                            Array<RequestModelState> request_mstates,
+                                            Array<GenerationConfig> generation_cfg) = 0;
+
+  static constexpr const char* _type_key = "mlc.serve.Sampler";
+  static constexpr const bool _type_has_method_sequal_reduce = false;
+  static constexpr const bool _type_has_method_shash_reduce = false;
+  TVM_DECLARE_BASE_OBJECT_INFO(SamplerObj, Object);
+};
+
+class Sampler : public ObjectRef {
+ public:
+  /*!
+   * \brief Create the runtime sampler module.
+   * \param sampler_kind The sampler name denoting which sampler to create.
+   * \return The created runtime module.
+   */
+  TVM_DLL static Sampler Create(std::string sampler_kind);
+
+  TVM_DEFINE_MUTABLE_OBJECT_REF_METHODS(Sampler, ObjectRef, SamplerObj);
+};
 
 }  // namespace serve
 }  // namespace llm
