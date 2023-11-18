fb7a2245bff4ab20a848411cc8034fc76c864bf6
Kartik Khandelwal <kartikkhandelwal1998@gmail.com>
2023-11-18 12:30:03 -0500

    add python, rest api test (#1278)
    * add python, rest api test
    
    * remove mistral, fix pylint
    
    * fix pylint requests import error

--- ci/task/pylint.sh
+++ ci/task/pylint.sh
@@ -9,7 +9,7 @@ export PYTHONPATH="./python:$PYTHONPATH"
 set -x
 
 # TVM Unity is a dependency to this testing
-pip install --quiet --pre -U -f https://mlc.ai/wheels mlc-ai-nightly
+pip install --quiet --pre -U -f https://mlc.ai/wheels mlc-ai-nightly requests
 
 pylint --jobs $NUM_THREADS ./python/
 pylint --jobs $NUM_THREADS --recursive=y ./tests/python/
