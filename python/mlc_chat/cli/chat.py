ee5d7648513bf07882f3f679723517b2bde500ff
Sing Li <westmakaha@yahoo.com>
2023-11-19 19:40:23 -0800

    all first review comments in

--- python/mlc_chat/cli/chat.py
+++ python/mlc_chat/cli/chat.py
@@ -5,11 +5,7 @@ import os
 from ..support.argparse import ArgumentParser
 
 
-
-# From the mlc-llm directory, run
-# $ python mlc_chat_cli.py
-
-def printhelp():
+def printAvailableCommands():
     print("You can use the following special commands:")
     print("\t/help\t\tprint the special commands")
     print("\t/exit\t\tquite the cli")
@@ -19,17 +15,24 @@ def printhelp():
 
 
 def chat(
-    modelname: str    
+    modelname: str,
+    devicename: str,
+    chatconfig: str,
+    modellib: str    
     ):
+
     # Create a ChatModule instance
-    cm = ChatModule(model=modelname, device="auto")
-    # cm = ChatModule(model="Mistral-7B-Instruct-v0.1-q4f16_1")
+    cm = ChatModule(model=modelname, 
+                    device=devicename,
+                    chat_config=chatconfig,
+                    model_lib_path=modellib)
+
 
     print("Using MLC config: ", os.path.abspath( cm.config_file_path) )
     print("Using model weights:",os.path.abspath( cm.model_path))
     print("Using model library:", os.path.abspath(cm.model_lib_path))
-    printhelp()
 
+    printAvailableCommands()
 
     PS=cm._get_role_0()
 
@@ -38,7 +41,7 @@ def chat(
     while( prompt != '/exit'):
         match prompt:
             case "/help":
-                printhelp()
+                printAvailableCommands()
             case "/stats":
                 print(f"Statistics: {cm.stats()}\n")
             case "/reset":
@@ -61,13 +64,38 @@ def main(argv):
     parser.add_argument(
         "--model",
         type=str,
-        default="Llama-2-7b-chat-hf-q4f16_1",
-        help="full name of LLM model",
+        help="Name of the model or path to the quantized weights",
         required=True,
     )
+    parser.add_argument(
+        "--model-lib",
+        type=str,
+        default=None,
+        help="Path to the model library",
+        required=False,
+    )
+    parser.add_argument(
+        "--chat-config",
+        type=str,
+        default=None,
+        help="The model's configuration file",
+        required=False,
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="auto",
+        help="The GPU to use.",
+        required=False,
+    )
+
     args = parser.parse_args(argv)
+
     chat(
-        modelname=args.model
+        modelname=args.model,
+        devicename=args.device,
+        chatconfig=args.chat_config,
+        modellib=args.model_lib,
     )
 
 if __name__ == "__main__":
