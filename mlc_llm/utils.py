9bd62302f1e2ca4a7a8adda1bc7c6593e211dba6
x <x@xs-MacBook-Pro.local>
2023-11-22 21:48:50 -0500

    adding check for NDK environment variable

--- mlc_llm/utils.py
+++ mlc_llm/utils.py
@@ -623,6 +623,11 @@ def parse_target(args: argparse.Namespace) -> None:
         )
         args.target_kind = "android"
     elif args.target in ["mali"]:
+        if "TVM_NDK_CC" in os.environ:
+            from tvm.contrib import ndk
+            args.export_kwargs = {
+                "fcompile": ndk.create_shared,
+            }
         target = tvm.target.Target(
             "opencl -device=mali",
             host="llvm -mtriple=aarch64-linux-gnu",
