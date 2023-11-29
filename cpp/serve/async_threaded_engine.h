a0e676c71b537cdb04b68f500eb2b06c9f25725f
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-11-28 19:30:54 -0500

    [Serving] AsyncThreadedEngine and PopenServer
    This PR brings two major features:
    * AsyncThreadedEngine, which runs the underlying engine
    execution (prefill, decode, etc.) on a standalone thread other
    than the thread doing async request handling and tokenization.
    * PopenServer, which launches a REST-ful server on an underlying
    background subprocess.
    
    In addition, this PR also involve many other changes, including:
    * wrapped the cpp tokenizer into a TVM Object, and exposed its
    encode/decode API through FFI, so that we can use the tokenizer
    on Python side. This change will be backported to the main branch
    soon.
    * supported `v1/models` OpenAI API,
    * formalized the interface of Engine in a header file.

---
+++ cpp/serve/async_threaded_engine.h
@@ -0,0 +1,57 @@
+/*!
+ *  Copyright (c) 2023 by Contributors
+ * \file serve/async_threaded_engine.h
+ * \brief The header of threaded asynchronous serving engine in MLC LLM.
+ */
+#ifndef MLC_LLM_SERVE_ASYNC_THREADED_ENGINE_H_
+#define MLC_LLM_SERVE_ASYNC_THREADED_ENGINE_H_
+
+#include <tvm/runtime/packed_func.h>
+
+#include "data.h"
+#include "engine.h"
+#include "request.h"
+
+namespace mlc {
+namespace llm {
+namespace serve {
+
+using namespace tvm::runtime;
+
+/*!
+ * \brief The interface asynchronous threaded engine in MLC LLM.
+ * The threaded engine keeps running a background request processing
+ * loop on a standalone thread. Ensuring thread safety, it exposes
+ * `AddRequest` and `AbortRequest` to receive new requests or
+ * abortions from other threads, and the internal request processing
+ * is backed by a normal engine wrapped inside.
+ */
+class AsyncThreadedEngine {
+ public:
+  virtual ~AsyncThreadedEngine() = default;
+
+  /*! \brief Create an AsyncThreadedEngine with the given background engine. */
+  static std::unique_ptr<AsyncThreadedEngine> Create(std::unique_ptr<Engine> engine);
+
+  /*! \brief Starts the background request processing loop. */
+  virtual void RunBackgroundLoop() = 0;
+
+  /*!
+   * \brief Notify the AsyncThreadedEngine to exit the background
+   * request processing loop. This method is invoked by threads
+   * other than the engine-driving thread.
+   */
+  virtual void ExitBackgroundLoop() = 0;
+
+  /*! \brief Add a new request to the engine. */
+  virtual void AddRequest(Request request) = 0;
+
+  /*! \brief Abort the input request (specified by id string) from engine. */
+  virtual void AbortRequest(const String& request_id) = 0;
+};
+
+}  // namespace serve
+}  // namespace llm
+}  // namespace mlc
+
+#endif  // MLC_LLM_SERVE_ASYNC_THREADED_ENGINE_H_
