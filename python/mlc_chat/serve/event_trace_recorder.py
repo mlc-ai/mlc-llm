76684c093f002bd449e7683f74902c1f05e63c9e
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-12-08 14:07:31 -0500

    [Serving] Introducing EventTraceRecorder (#1399)
    This PR introduces the EventTraceRecorder in MLC LLM serving, which can
    record event times for each request on invocation, and can dump
    all the recorded events to Chrome Trace Event format, so that
    we can visualize the events and the timeline of all requests.
    
    This PR brings the optional flag `enable_tracing` to Engine,
    AsyncEngine/AsyncThreadedEngine, server entry and PopenServer.
    This flag defaults to `False`, in which case we do not enable
    tracing.
    
    This PR introduces a new server entrypoint `/debug/dump_event_trace`,
    under file `python/mlc_chat/serve/entrypoints/debug_entrypoints.py`.
    This entrypoint accepts a model name, and returns the dumped events
    in Chrome Trace Event format (in JSON string).
    
    This PR contains a basic EventTraceRecorder test and a basic test for
    entrypoint `/debug/dump_event_trace`.

---
+++ python/mlc_chat/serve/event_trace_recorder.py
@@ -0,0 +1,41 @@
+"""The event trace recorder in MLC LLM serving"""
+
+import tvm._ffi
+from tvm.runtime import Object
+
+from . import _ffi_api
+
+
+@tvm._ffi.register_object("mlc.serve.EventTraceRecorder")  # pylint: disable=protected-access
+class EventTraceRecorder(Object):
+    """The event trace recorder for requests."""
+
+    def __init__(self) -> None:
+        """Initialize a trace recorder."""
+        self.__init_handle_by_constructor__(
+            _ffi_api.EventTraceRecorder  # type: ignore  # pylint: disable=no-member
+        )
+
+    def add_event(self, request_id: str, event: str) -> None:
+        """Record a event for the the input request in the trace recorder.
+
+        Parameters
+        ----------
+        request_id : str
+            The subject request of the event.
+
+        event : str
+            The event in a string name.
+            It can have one of the following patterns:
+            - "start xxx", which marks the start of event "xxx",
+            - "finish xxx", which marks the finish of event "xxx",
+            - "yyy", which marks the instant event "yyy".
+            The "starts" and "finishes" will be automatically paired in the trace recorder.
+        """
+        return _ffi_api.EventTraceRecorderAddEvent(  # type: ignore  # pylint: disable=no-member
+            self, request_id, event
+        )
+
+    def dump_json(self) -> str:
+        """Dump the logged events in Chrome Trace Event Format in JSON string."""
+        return _ffi_api.EventTraceRecorderDumpJSON(self)  # type: ignore  # pylint: disable=no-member
