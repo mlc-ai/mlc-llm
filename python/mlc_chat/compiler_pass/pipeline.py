197cfa10c6f2ac419680cdac4db98e1ea9920bf3
Hongyi Jin <jinhongyi02@gmail.com>
2023-12-29 16:56:44 -0500

    cublas

--- python/mlc_chat/compiler_pass/pipeline.py
+++ python/mlc_chat/compiler_pass/pipeline.py
@@ -18,6 +18,7 @@ from .fuse_dequantize_take import FuseDequantizeTake
 from .fuse_dequantize_transpose import FuseDequantizeTranspose
 from .fuse_transpose_matmul import FuseTransposeMatmul
 from .lift_global_buffer_alloc import LiftTIRGlobalBufferAlloc
+from .cublas_dispatch import CublasDispatch
 
 logger = logging.getLogger(__name__)
 
@@ -57,6 +58,7 @@ class _DebugDump:  # pylint: disable=too-few-public-methods
 
 @register_pipeline("mlc_llm")
 def _mlc_llm_pipeline(  # pylint: disable=too-many-arguments
+    opt: "OptimizationFlags",
     variable_bounds: Dict[str, int] = None,
     additional_tirs: Dict[str, tvm.tir.PrimFunc] = None,
     metadata: Dict[str, Any] = None,
@@ -80,6 +82,7 @@ def _mlc_llm_pipeline(  # pylint: disable=too-many-arguments
                 # Phase 1. Passes on high-level operator graph
                 _LogProgress("Running TVM Relax graph-level optimizations"),
                 FuseDequantizeTranspose(skip_gemm=skip_gemm),
+                CublasDispatch() if opt.cublas_gemm else tvm.transform.Sequential([]),
                 FuseTransposeMatmul(),
                 _DebugDump("debug-phase1.py", debug_dump, show_meta=False),
                 # Phase 2. Lowering to TIR, inherited TVM Relax's official "zero" pipeline
