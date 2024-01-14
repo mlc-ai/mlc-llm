acb70dd6ceb5d823059f8e5cfee074514da08ff4
ist192448 <david.pissarra@tecnico.ulisboa.pt>
2024-01-14 03:40:43 +0000

    [SLM] ios refactor

--- ios/MLCChat/Models/ModelConfig.swift
+++ ios/MLCChat/Models/ModelConfig.swift
@@ -4,25 +4,15 @@
 //
 
 struct ModelConfig: Decodable {
-    let modelLib: String
-    let localID: String
     let tokenizerFiles: [String]
-    private let internalDisplayName: String?
-    private let internalEstimatedVRAMReq: Int?
+    var modelLib: String?
+    var modelID: String?
+    var estimatedVRAMReq: Int?
 
     enum CodingKeys: String, CodingKey {
-        case modelLib = "model_lib"
-        case localID = "local_id"
         case tokenizerFiles = "tokenizer_files"
-        case internalDisplayName = "display_name"
-        case internalEstimatedVRAMReq = "estimated_vram_req"
-    }
-
-    var displayName: String {
-        internalDisplayName ?? localID.components(separatedBy: "-")[0]
-    }
-
-    var estimatedVRAMReq: Int {
-        internalEstimatedVRAMReq ?? 4000000000
+        case modelLib = "model_lib"
+        case modelID = "model_id"
+        case estimatedVRAMReq = "estimated_vram_req"
     }
 }
