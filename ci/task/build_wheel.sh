88a6f27541d259c10a1112aa3c1ab69905048acb
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-01-23 23:42:38 -0500

    [CI] Metal and iOS model compilation CI
    This PR attempts to introduce the CI for Metal and iOS.

--- ci/task/build_wheel.sh
+++ ci/task/build_wheel.sh
@@ -13,8 +13,15 @@ elif [[ ${GPU} == cuda* ]]; then
 	AUDITWHEEL_OPTS="--exclude libcuda --exclude libcudart --exclude libnvrtc --exclude libcublas --exclude libcublasLt ${AUDITWHEEL_OPTS}"
 fi
 
-cd ${WORKSPACE_CWD}/python && python setup.py bdist_wheel && auditwheel repair ${AUDITWHEEL_OPTS} dist/*.whl
+cd ${WORKSPACE_CWD}/python && python setup.py bdist_wheel
 
 rm -rf ${WORKSPACE_CWD}/wheels/
-mv ${WORKSPACE_CWD}/python/repaired_wheels/ ${WORKSPACE_CWD}/wheels/
+if [[ ${GPU} != metal ]]; then
+	auditwheel repair ${AUDITWHEEL_OPTS} dist/*.whl
+	mv ${WORKSPACE_CWD}/python/repaired_wheels/ ${WORKSPACE_CWD}/wheels/
+else
+	mkdir ${WORKSPACE_CWD}/wheels/
+	mv dist/*.whl ${WORKSPACE_CWD}/wheels/
+fi
+
 chown -R $ENV_USER_ID:$ENV_GROUP_ID ${WORKSPACE_CWD}/wheels/
