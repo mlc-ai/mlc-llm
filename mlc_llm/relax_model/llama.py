mlc-ai/mlc-llm

commit 5953ccec533ab6e2cff21339426ff6c26ca24a2e

[ANDROID][ADRENO] Various enhancements for Adreno target  (#1536)
[ANDROID][ADRENO] Various enhancements for Adreno target to pmprove performance

This improves the adreno dispatches for best unitization of hardware resources
Changes to the network to avoid unnecessary transposes which are not required for Adreno.

The performance of on Snapdragon Gen 2 is now improved upto

Prefille: 30 toks/sec
Decode  : 11 toks/sec

mlc_llm/relax_model/llama.py
@@ -39,6 +39,7 @@ class LlamaConfig:
         build_model_only=False,
         num_shards=1,
         sliding_window=None,
+        target_kind=None,
         **kwargs,
     ):
         self.dtype = dtype
@@ -59,6 +60,7 @@ class LlamaConfig:
         self.position_embedding_base = position_embedding_base
         self.combine_matmul = combine_matmul
         self.sliding_window = sliding_window
+        self.target_kind = target_kind
 
         if build_model_only and num_shards > 1:
             self.num_shards = num_shards
@@ -431,6 +433,7 @@ class LlamaPagedAttention(LlamaAttentionBase):
 class LlamaAttention(LlamaAttentionBase):
     def __init__(self, config: LlamaConfig):
         super().__init__(config)
+        self.config = config
 
     def attention_fwd(
         self,
@@ -510,14 +513,21 @@ class LlamaAttention(LlamaAttentionBase):
             key_states = nn.emit(relax.op.repeat(key_states, n_rep, axis=2))
             value_states = nn.emit(relax.op.repeat(value_states, n_rep, axis=2))
 
-        query_states = nn.emit(permute_dims(query_states, [0, 2, 1, 3]))
-        key_states = nn.emit(permute_dims(key_states, [0, 2, 1, 3]))
-        value_states = nn.emit(permute_dims(value_states, [0, 2, 1, 3]))
+        if self.config.target_kind == "android":
+            attn_weights = nn.emit(
+                matmul(permute_dims(query_states, [0, 2, 1, 3]), permute_dims(key_states, [0, 2, 3, 1]))
+                / relax.const(math.sqrt(self.head_dim), query_states.struct_info.dtype)
+            )
+        else:
+            query_states = nn.emit(permute_dims(query_states, [0, 2, 1, 3]))
+            key_states = nn.emit(permute_dims(key_states, [0, 2, 1, 3]))
+            value_states = nn.emit(permute_dims(value_states, [0, 2, 1, 3]))
 
-        attn_weights = nn.emit(
-            matmul(query_states, permute_dims(key_states, [0, 1, 3, 2]))
-            / relax.const(math.sqrt(self.head_dim), query_states.struct_info.dtype)
-        )
+            attn_weights = nn.emit(
+                matmul(permute_dims(query_states, [0, 2, 1, 3]), permute_dims(key_states, [0, 2, 3, 1]))
+                / relax.const(math.sqrt(self.head_dim), query_states.struct_info.dtype)
+            )
+
 
         tvm.ir.assert_structural_equal(
             attention_mask.struct_info.shape.values,
@@ -541,8 +551,10 @@ class LlamaAttention(LlamaAttentionBase):
         attn_weights = nn.emit(softmax(attn_weights, axis=-1))
         if attn_weights.struct_info.dtype != query_states.struct_info.dtype:
             attn_weights = astype(attn_weights, query_states.struct_info.dtype)
-        attn_output = nn.emit(matmul(attn_weights, value_states))
-
+        if self.config.target_kind == "android":
+            attn_output = nn.emit(matmul(attn_weights, permute_dims(value_states, [0, 2, 1, 3])))
+        else:
+            attn_output = nn.emit(matmul(attn_weights, value_states))
         attn_output = nn.emit(permute_dims(attn_output, [0, 2, 1, 3]))
         return attn_output, past_key_values
 
@@ -1411,6 +1423,7 @@ def get_model(args, hf_config):
             combine_matmul=True,
             num_shards=args.num_shards,
             build_model_only=args.build_model_only,
+            target_kind=args.target_kind,
         )
     elif "max_position_embeddings" in hf_config:
         config = LlamaConfig(
@@ -1421,6 +1434,7 @@ def get_model(args, hf_config):
             combine_matmul=True,
             num_shards=args.num_shards,
             build_model_only=args.build_model_only,
+            target_kind=args.target_kind,
         )
     else:
         raise Exception(

mlc_llm/core.py
@@ -661,6 +661,9 @@ def mod_transform_before_build(
                 ]
             )(mod)
 
+    if args.target_kind == "android":
+        mod = mlc_llm.transform.FuseTranspose1Matmul()(mod)
+        mod = mlc_llm.transform.FuseTranspose2Matmul()(mod)
     mod = mlc_llm.transform.FuseTransposeMatmul()(mod)
     mod = relax.pipeline.get_pipeline()(mod)  # pylint: disable=no-value-for-parameter
     mod = mlc_llm.transform.FuseDecodeMatmulEwise()(mod)
@@ -747,6 +750,10 @@ def build(mod_deploy: tvm.IRModule, args: argparse.Namespace) -> None:
             else tvm.target.Target("apple/m1-gpu-restricted")
         )
         with dispatch_target:
+            if args.target_kind == "android":
+                mod_deploy = mlc_llm.dispatch.DispatchTIROperatorAdreno()(  # pylint: disable=not-callable
+                    mod_deploy
+                )
             mod_deploy = dl.ApplyDefaultSchedule(  # pylint: disable=not-callable
                 dl.gpu.Matmul(),
                 dl.gpu.GEMV(),

mlc_llm/transform/__init__.py
@@ -6,4 +6,4 @@ from .fuse_split_rotary_embedding import fuse_split_rotary_embedding
 from .lift_tir_global_buffer_alloc import LiftTIRGlobalBufferAlloc
 from .reorder_transform_func import ReorderTransformFunc
 from .rewrite_attention import rewrite_attention
-from .transpose_matmul import FuseTransposeMatmul
+from .transpose_matmul import FuseTransposeMatmul, FuseTranspose1Matmul, FuseTranspose2Matmul

mlc_llm/transform/transpose_matmul.py
@@ -135,3 +135,215 @@ class FuseTransposeMatmul:
             transpose_matmul_codegen.builder_.update_func(gv, func)
 
         return transpose_matmul_codegen.builder_.get()
+
+@relax.expr_functor.mutator
+class Transpose1MatmulCodeGenerator(relax.PyExprMutator):
+    def __init__(self, mod):
+        super().__init__(mod)
+
+    @staticmethod
+    def pattern():
+        w = wildcard()
+        x = wildcard()
+        xT = is_op("relax.permute_dims")(x)
+        wT = is_op("relax.permute_dims")(w)
+        o = is_op("relax.matmul")(xT, wT)
+        annotations = {"o": o, "w": w, "x": x, "xT": xT, "wT": wT}
+
+        def _check(context: relax.transform.PatternCheckContext) -> bool:
+            x_transpose_call = context.annotated_expr["o"]
+            w_transpose_call = context.annotated_expr["o"]
+            x_shape = context.annotated_expr["x"].struct_info.shape
+            w_shape = context.annotated_expr["w"].struct_info.shape
+            xT_shape = x_transpose_call.args[0].struct_info.shape
+            wT_shape = w_transpose_call.args[1].struct_info.shape
+
+            if not (
+                xT_shape[0] == x_shape[0] and xT_shape[1] == x_shape[2]
+                and xT_shape[2] == x_shape[1] and xT_shape[3] == x_shape[3]
+            ):
+                return False
+
+            if not (
+                wT_shape[0] == w_shape[0] and wT_shape[1] == w_shape[2]
+                and wT_shape[2] == w_shape[3] and wT_shape[3] == w_shape[1]
+            ):
+                return False
+
+            return True
+
+        return o, annotations, _check
+
+    def visit_call_(self, call: relax.Call) -> relax.Expr:
+        out_dtype = None
+
+        def te_transposed_matmul(a: te.Tensor, b: te.Tensor) -> te.Tensor:
+            nonlocal out_dtype
+            a_shape = list(a.shape)
+            b_shape = list(b.shape)
+
+            aT_shape = list(a.shape)
+            aT_shape[-2], aT_shape[-3] = aT_shape[-3], aT_shape[-2]
+            aT_relax = relax.Var("a", relax.TensorStructInfo(aT_shape))
+            bT_shape = list(b.shape)
+            bT_shape[-1], bT_shape[-2], bT_shape[-3] = bT_shape[-3], bT_shape[-1], bT_shape[-2]
+            bT_relax = relax.Var("b", relax.TensorStructInfo(bT_shape))
+            output_shape = self.builder_.normalize(
+                relax.op.matmul(aT_relax, bT_relax)
+            ).struct_info.shape
+            def matmul_compute(*idx_spatial):
+                k = te.reduce_axis((0, a_shape[-1]), name="k")
+                def multiply_compute(idx_reduce):
+                    a_indices = [idx_spatial[0], idx_spatial[2], idx_spatial[1], idx_reduce]
+                    b_indices = [idx_spatial[0], idx_spatial[3], idx_spatial[1], idx_reduce]
+                    dtype = out_dtype
+                    if dtype != "":
+                        return a(*a_indices).astype(dtype) * b(*b_indices).astype(dtype)
+                    return a(*a_indices) * b(*b_indices)
+
+                return te.sum(multiply_compute(k), axis=k)
+
+            return te.compute(
+                output_shape,
+                lambda *idx: matmul_compute(*idx),  # pylint: disable=unnecessary-lambda
+                name="NT_matmul",
+            )
+
+        if isinstance(call.op, relax.GlobalVar):
+            function = self.builder_.get()[call.op]
+            if (
+                "Composite" in function.attrs
+                and function.attrs["Composite"] == "transpose1_matmul_fuse"
+            ):
+                out_dtype = function.ret_struct_info.dtype
+                return self.builder_.call_te(
+                    te_transposed_matmul,
+                    call.args[0],
+                    call.args[1],
+                    primfunc_name_hint="NT_matmul",
+                )
+
+        return super().visit_call_(call)
+
+
+@tvm.transform.module_pass(opt_level=0, name="FuseTranspose1Matmul")
+class FuseTranspose1Matmul:
+    def transform_module(
+        self, mod: IRModule, ctx: tvm.transform.PassContext
+    ) -> IRModule:
+        mod = relax.transform.FuseOpsByPattern(
+            [("transpose1_matmul_fuse", *Transpose1MatmulCodeGenerator.pattern())]
+        )(mod)
+
+        transpose_matmul_codegen = Transpose1MatmulCodeGenerator(mod)
+        for gv in mod.functions:
+            func = mod[gv]
+            if not isinstance(func, relax.Function):
+                continue
+            func = transpose_matmul_codegen.visit_expr(func)
+            transpose_matmul_codegen.builder_.update_func(gv, func)
+
+        return transpose_matmul_codegen.builder_.get()
+
+
+@relax.expr_functor.mutator
+class Transpose2MatmulCodeGenerator(relax.PyExprMutator):
+    def __init__(self, mod):
+        super().__init__(mod)
+
+    @staticmethod
+    def pattern():
+        w = wildcard()
+        x = wildcard()
+        wT = is_op("relax.permute_dims")(w)
+        o = is_op("relax.permute_dims")(is_op("relax.matmul")(x, wT))
+        #oT = is_op("relax.permute_dims")(o)
+        annotations = {"o": o, "w": w, "x": x, "wT": wT}
+
+        def _check(context: relax.transform.PatternCheckContext) -> bool:
+            w_transpose_call = context.annotated_expr["wT"]
+            w_shape = w_transpose_call.args[0].struct_info.shape
+            wT_shape = w_transpose_call.struct_info.shape
+            oT_call = context.annotated_expr["o"]
+            o_shape = oT_call.args[0].struct_info.shape
+            oT_shape = oT_call.struct_info.shape
+
+            if not (
+                wT_shape[0] == w_shape[0] and wT_shape[1] == w_shape[2]
+                and wT_shape[2] == w_shape[1] and wT_shape[3] == w_shape[3]
+            ):
+                return False
+
+            if not (
+                oT_shape[0] == o_shape[0] and oT_shape[1] == o_shape[2]
+                and oT_shape[2] == o_shape[1] and oT_shape[3] == o_shape[3]
+            ):
+                return False
+
+            return True
+
+        return o, annotations, _check
+
+    def visit_call_(self, call: relax.Call) -> relax.Expr:
+        out_dtype = None
+
+        def te_transposed_matmul(a: te.Tensor, b: te.Tensor) -> te.Tensor:
+            nonlocal out_dtype
+            a_shape = list(a.shape)
+            b_shape = list(b.shape)
+            output_shape = [a_shape[0], b_shape[-2], a_shape[2], a_shape[3]]
+            def matmul_compute(*idx_spatial):
+                k = te.reduce_axis((0, b_shape[-1]), name="k")
+                def multiply_compute(idx_reduce):
+                    a_indices = [idx_spatial[0], idx_reduce, idx_spatial[2], idx_spatial[3]]
+                    b_indices = [idx_spatial[0], idx_spatial[2], idx_spatial[1], idx_reduce]
+
+                    dtype = out_dtype
+                    if dtype != "":
+                        return a(*a_indices).astype(dtype) * b(*b_indices).astype(dtype)
+                    return a(*a_indices) * b(*b_indices)
+
+                return te.sum(multiply_compute(k), axis=k)
+
+            return te.compute(
+                output_shape,
+                lambda *idx: matmul_compute(*idx),  # pylint: disable=unnecessary-lambda
+                name="NT_matmul",
+            )
+
+        if isinstance(call.op, relax.GlobalVar):
+            function = self.builder_.get()[call.op]
+            if (
+                "Composite" in function.attrs
+                and function.attrs["Composite"] == "transpose2_matmul_fuse"
+            ):
+                out_dtype = function.ret_struct_info.dtype
+                #NT_output_shape = function.ret_struct_info.shape
+                return self.builder_.call_te(
+                    te_transposed_matmul,
+                    call.args[0],
+                    call.args[1],
+                    primfunc_name_hint="NT_matmul",
+                )
+
+        return super().visit_call_(call)
+
+
+@tvm.transform.module_pass(opt_level=0, name="FuseTranspose2Matmul")
+class FuseTranspose2Matmul:
+    def transform_module(
+        self, mod: IRModule, ctx: tvm.transform.PassContext
+    ) -> IRModule:
+        mod = relax.transform.FuseOpsByPattern(
+            [("transpose2_matmul_fuse", *Transpose2MatmulCodeGenerator.pattern())]
+        )(mod)
+
+        transpose_matmul_codegen = Transpose2MatmulCodeGenerator(mod)
+        for gv in mod.functions:
+            func = mod[gv]
+            if not isinstance(func, relax.Function):
+                continue
+            func = transpose_matmul_codegen.visit_expr(func)
+            transpose_matmul_codegen.builder_.update_func(gv, func)
+
+        return transpose_matmul_codegen.builder_.get()

commit ae7fa9ba14b69c87939c77098f359db0b2e58351

[Fix] Fix llama model compilation failure
This PR fixes the Llama compilation failure in the old flow.

* updating the uses of `relax.call_inplace_paked`
and other ops to follow the signature in TVM.
* fixing the typo on attention computation introduced by #1536.

mlc_llm/relax_model/llama.py
@@ -477,7 +477,8 @@ class LlamaAttention(LlamaAttentionBase):
         k_cache = nn.emit(
             relax.op.call_inplace_packed(
                 f_kv_cache_append,
-                args=[k_cache, squeezed_key],
+                k_cache,
+                squeezed_key,
                 inplace_indices=[0],
                 sinfo_args=[relax.ObjectStructInfo()],
             )
@@ -485,7 +486,8 @@ class LlamaAttention(LlamaAttentionBase):
         v_cache = nn.emit(
             relax.op.call_inplace_packed(
                 f_kv_cache_append,
-                args=[v_cache, squeezed_value],
+                v_cache,
+                squeezed_value,
                 inplace_indices=[0],
                 sinfo_args=[relax.ObjectStructInfo()],
             )
@@ -495,14 +497,16 @@ class LlamaAttention(LlamaAttentionBase):
         k_cache = nn.emit(
             relax.call_pure_packed(
                 f_kv_cache_view,
-                args=[k_cache, kv_cache_shape],
+                k_cache,
+                kv_cache_shape,
                 sinfo_args=[R.Tensor(kv_cache_shape, kv_states_dtype)],
             )
         )
         v_cache = nn.emit(
             relax.call_pure_packed(
                 f_kv_cache_view,
-                args=[v_cache, kv_cache_shape],
+                v_cache,
+                kv_cache_shape,
                 sinfo_args=[R.Tensor(kv_cache_shape, kv_states_dtype)],
             )
         )
@@ -515,7 +519,9 @@ class LlamaAttention(LlamaAttentionBase):
 
         if self.config.target_kind == "android":
             attn_weights = nn.emit(
-                matmul(permute_dims(query_states, [0, 2, 1, 3]), permute_dims(key_states, [0, 2, 3, 1]))
+                matmul(
+                    permute_dims(query_states, [0, 2, 1, 3]), permute_dims(key_states, [0, 2, 3, 1])
+                )
                 / relax.const(math.sqrt(self.head_dim), query_states.struct_info.dtype)
             )
         else:
@@ -524,10 +530,9 @@ class LlamaAttention(LlamaAttentionBase):
             value_states = nn.emit(permute_dims(value_states, [0, 2, 1, 3]))
 
             attn_weights = nn.emit(
-                matmul(permute_dims(query_states, [0, 2, 1, 3]), permute_dims(key_states, [0, 2, 3, 1]))
+                matmul(query_states, permute_dims(key_states, [0, 1, 3, 2]))
                 / relax.const(math.sqrt(self.head_dim), query_states.struct_info.dtype)
             )
-
 
         tvm.ir.assert_structural_equal(
             attention_mask.struct_info.shape.values,
@@ -699,7 +704,9 @@ class LlamaModelBase(nn.Module):
 
 
 class LlamaModelForSingleSequence(LlamaModelBase):
-    def __init__(self, config: LlamaConfig, vocab_size_var: tvm.tir.SizeVar, sep_embed: bool = False):
+    def __init__(
+        self, config: LlamaConfig, vocab_size_var: tvm.tir.SizeVar, sep_embed: bool = False
+    ):
         super().__init__(config, vocab_size_var, sep_embed, enable_batching=False)
 
     def _prepare_decoder_attention_mask(self, input_shape, src_len, dtype):
@@ -824,7 +831,7 @@ class LlamaForCausalLM(nn.Module):
         cache_len = te.var("cached_rotary_embedding_len", "int64")
         self.cos_cached = nn.Parameter((cache_len, head_dim), dtype=config.dtype, name="cos_cached")
         self.sin_cached = nn.Parameter((cache_len, head_dim), dtype=config.dtype, name="sin_cached")
-        
+
         # Mark if output_all_logits is True
         self.output_all_logits = output_all_logits
         ############ End ############
@@ -1054,7 +1061,7 @@ def create_decoding_func_for_batching(
     mod = bb.get()
     gv = mod.get_global_var(func_name)
     bb.update_func(gv, mod[gv].with_attr("num_input", 2))
-    
+
 
 def create_verification_func_for_batching(
     bb: relax.BlockBuilder,
@@ -1063,12 +1070,16 @@ def create_verification_func_for_batching(
     quant_scheme: QuantizationScheme,
 ) -> None:
     func_name = "verify_with_embed"
-    
+
     total_seq_len = tvm.tir.SizeVar("num_tokens_including_cache", "int64")
     hidden_size = config.hidden_size
     with bb.function(func_name):
         model = LlamaForCausalLM(
-            config, tvm.tir.SizeVar("vocab_size", "int64"), sep_embed=True, enable_batching=True, output_all_logits=True
+            config,
+            tvm.tir.SizeVar("vocab_size", "int64"),
+            sep_embed=True,
+            enable_batching=True,
+            output_all_logits=True,
         )
         param_manager.register_params(model, func_name, quant_scheme, get_param_quant_kind)
 
@@ -1110,7 +1121,9 @@ def create_kv_cache_func(bb: relax.BlockBuilder, config: LlamaConfig) -> None:
                     bb.emit(
                         relax.call_pure_packed(
                             f_kv_cache_create,
-                            args=[zeros, init_shape, relax.PrimValue(0)],
+                            zeros,
+                            init_shape,
+                            relax.PrimValue(0),
                             sinfo_args=[relax.ObjectStructInfo()],
                         )
                     )