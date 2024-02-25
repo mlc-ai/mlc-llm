60adb91d768987024bbcfafed86da0b254dccf7f
Shushi Hong <820958424@qq.com>
2024-02-26 01:06:50 +0800

    Update internlm_quantization.py

--- python/mlc_chat/model/internlm/internlm_quantization.py
+++ python/mlc_chat/model/internlm/internlm_quantization.py
@@ -1,4 +1,4 @@
-"""This file specifies how MLC's InternLM2 parameters are quantized using group quantization
+"""This file specifies how MLC's InternLM parameters are quantized using group quantization
 or other formats."""
 from typing import Tuple
 
