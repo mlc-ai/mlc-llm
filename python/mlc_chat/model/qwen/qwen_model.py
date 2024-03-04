6a1d28f2f1dc20fc8e4bc2c7e746f5f84bf8e923
DiegoCao <caohangrui2000@gmail.com>
2024-03-04 16:39:54 -0500

    Support Qwen1.0 Paged KV Cache

--- python/mlc_chat/model/qwen/qwen_model.py
+++ python/mlc_chat/model/qwen/qwen_model.py
@@ -10,6 +10,7 @@ from tvm.relax.frontend import nn
 from tvm.relax.frontend.nn import Tensor, op
 
 from mlc_chat import op as op_ext
+from mlc_chat.nn import PagedKVCache, RopeMode
 from mlc_chat.support import logging
 from mlc_chat.support.config import ConfigBase
 from mlc_chat.support.style import bold
@@ -80,10 +81,9 @@ class QWenAttention(nn.Module):  # pylint: disable=too-many-instance-attributes
     def __init__(self, config: QWenConfig):
         self.hidden_size = config.hidden_size
         self.rope_theta = config.rotary_emb_base
-        self.num_heads = config.num_attention_heads
+        self.num_heads = config.num_attention_heads // config.tensor_parallel_shards
         self.head_dim = self.hidden_size // self.num_heads
         self.projection_size = config.kv_channels * config.num_attention_heads
-
         self.c_attn = nn.Linear(
             in_features=config.hidden_size,
             out_features=3 * self.projection_size,
@@ -98,31 +98,34 @@ class QWenAttention(nn.Module):  # pylint: disable=too-many-instance-attributes
     def forward(  # pylint: disable=too-many-locals
         self,
         hidden_states: Tensor,
-        attention_mask: Tensor,
-        total_seq_len: tir.Var,
+        paged_kv_cache: PagedKVCache,
+        layer_id: int,
     ):
-        d, h, t = self.head_dim, self.num_heads, total_seq_len
+        d, h = self.head_dim, self.num_heads
         b, s, _ = hidden_states.shape
-        assert b == 1, "Only support batch size 1 at this moment."
-        # Step 1. QKV Projection
+
         qkv = self.c_attn(hidden_states)
         qkv = op.reshape(qkv, (b, s, 3 * h, d))
-        # Step 2. Apply QK rotary embedding
-        q, k, v = op_ext.llama_rope(qkv, t, self.rope_theta, h, h)
-        # Step 3. Query and update KVCache
-        self.k_cache.append(op.squeeze(k, axis=0))
-        self.v_cache.append(op.squeeze(v, axis=0))
-        k = self.k_cache.view(t)
-        v = self.v_cache.view(t)
-        # Step 4. Compute softmax(Q @ K^T / sqrt(d)) @ V
-        output = op_ext.attention(q, k, v, casual_mask=attention_mask)
-        # Step 5. Apply output projection
+        output = op.reshape(
+            paged_kv_cache.attention_with_fused_qkv(layer_id, qkv, self.num_heads), (b, s, h * d)
+        )
+        return self.c_proj(output)
+
+    def batch_forward(self, hidden_states: Tensor, paged_kv_cache: PagedKVCache, layer_id: int):
+        b, s, _ = hidden_states.shape
+        qkv = self.c_attn(hidden_states)
+        qkv = op.reshape(qkv, (b, s, 3 * self.head_dim, self.num_heads))
+        # try batch forward
+        output = op.reshape(
+            paged_kv_cache.attention_with_fused_qkv(layer_id, qkv, self.num_heads),
+            (b, s, self.head_dim * self.num_heads),
+        )
         return self.c_proj(output)
 
 
 class QWenMLP(nn.Module):
     def __init__(self, config: QWenConfig):
-        self.intermediate_size = config.intermediate_size
+        self.intermediate_size = config.intermediate_size // config.tensor_parallel_shards
         self.gate_up_proj = nn.Linear(
             in_features=config.hidden_size,
             out_features=self.intermediate_size,
@@ -144,8 +147,15 @@ class QWenBlock(nn.Module):
         self.ln_1 = nn.RMSNorm(config.hidden_size, -1, rms_norm_eps, bias=False)
         self.ln_2 = nn.RMSNorm(config.hidden_size, -1, rms_norm_eps, bias=False)
 
-    def forward(self, hidden_states: Tensor, attention_mask: Tensor, total_seq_len: tir.Var):
-        out = self.attn(self.ln_1(hidden_states), attention_mask, total_seq_len)
+    def forward(self, hidden_states: Tensor, paged_kv_cache: PagedKVCache, layer_id: int):
+        out = self.attn(self.ln_1(hidden_states), paged_kv_cache, layer_id)
+        hidden_states = out + hidden_states
+        out = self.mlp(self.ln_2(hidden_states))
+        hidden_states = out + hidden_states
+        return hidden_states
+
+    def batch_forward(self, hidden_states: Tensor, paged_kv_cache: PagedKVCache, layer_id: int):
+        out = self.attn.batch_forward(self.ln_1(hidden_states), paged_kv_cache, layer_id)
         hidden_states = out + hidden_states
         out = self.mlp(self.ln_2(hidden_states))
         hidden_states = out + hidden_states
@@ -159,19 +169,34 @@ class QWenModel(nn.Module):
         self.h = nn.ModuleList([QWenBlock(config) for _ in range(config.num_hidden_layers)])
         self.ln_f = nn.RMSNorm(config.hidden_size, -1, config.layer_norm_epsilon, bias=False)
 
-    def forward(self, input_ids: Tensor, total_seq_len: tir.Var, attention_mask: Tensor):
-        hidden_states = self.wte(input_ids)
-        for layer in self.h:
-            hidden_states = layer(hidden_states, attention_mask, total_seq_len)
+    def forward(self, inputs: Tensor, paged_kv_cache: PagedKVCache):
+        # hidden_states = self.wte(input_ids)
+        hidden_states = inputs
+        for layer_id, layer in enumerate(self.h):
+            hidden_states = layer(hidden_states, paged_kv_cache, layer_id)
+        hidden_states = self.ln_f(hidden_states)
+        return hidden_states
+
+    def batch_forward(self, inputs, paged_kv_cache: PagedKVCache):
+        # hidden_states = self.wte(input_ids)
+        hidden_states = inputs
+        for layer_id, layer in enumerate(self.h):
+            hidden_states = layer.batch_forward(hidden_states, paged_kv_cache, layer_id)
         hidden_states = self.ln_f(hidden_states)
         return hidden_states
 
 
-class QWenLMHeadModel(nn.Module):
+class QWenLMHeadModel(nn.Module):  # pylint: disable=too-many-instance-attributes
     def __init__(self, config: QWenConfig):
         self.transformer = QWenModel(config)
-        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False, dtype="float32")
+        self.hidden_size = config.hidden_size
         self.vocab_size = config.vocab_size
+        self.num_hidden_layers = config.num_hidden_layers
+        self.num_attention_heads = config.num_attention_heads
+        self.head_dim = self.hidden_size // self.num_attention_heads
+        self.tensor_parallel_shards = config.tensor_parallel_shards
+        self.rotary_emb_base = config.rotary_emb_base
         self.dtype = "float32"
 
     def to(self, dtype: Optional[str] = None):
@@ -179,72 +204,151 @@ class QWenLMHeadModel(nn.Module):
         if dtype is not None:
             self.dtype = dtype
 
-    def forward(self, inputs: Tensor, total_seq_len: tir.Var, attention_mask: Tensor):
-        def _index(x: te.Tensor):  # x[:-1,:]
-            b, s, d = x.shape
-            return te.compute((b, 1, d), lambda i, _, k: x[i, s - 1, k], name="index")
-
-        hidden_states = self.transformer(inputs, total_seq_len, attention_mask)
-        hidden_states = op.tensor_expr_op(_index, name_hint="index", args=[hidden_states])
+    def batch_forward(
+        self,
+        inputs: Tensor,
+        paged_kv_cache: PagedKVCache,
+        logit_positions: Optional[Tensor] = None,
+    ):
+        op_ext.configure()
+        hidden_states = self.transformer.batch_forward(inputs, paged_kv_cache)
+        if logit_positions is not None:
+            hidden_states = op.take(hidden_states, logit_positions, axis=1)
         logits = self.lm_head(hidden_states)
         if logits.dtype != "float32":
             logits = logits.astype("float32")
         return logits
 
-    def prefill(self, inputs: Tensor, total_seq_len: tir.Var):
-        def _attention_mask(batch_size, seq_len, total_seq_len):
-            return te.compute(
-                (batch_size, 1, seq_len, total_seq_len),
-                lambda b, _, i, j: tir.if_then_else(
-                    i < j - (total_seq_len - seq_len),
-                    tir.min_value(self.dtype),
-                    tir.max_value(self.dtype),
-                ),
-                name="attention_mask_prefill",
-            )
+    def embed(self, input_ids: Tensor):
+        return self.transformer.wte(input_ids)
 
-        batch_size, seq_len = inputs.shape
-        attention_mask = op.tensor_expr_op(
-            _attention_mask,
-            name_hint="attention_mask_prefill",
-            args=[batch_size, seq_len, total_seq_len],
-        )
-        return self.forward(inputs, total_seq_len, attention_mask)
+    def prefill(self, inputs: Tensor, paged_kv_cache: PagedKVCache):
+        op_ext.configure()
 
-    def decode(self, inputs: Tensor, total_seq_len: tir.Var):
-        batch_size, seq_len = inputs.shape
-        attention_mask = op.full(
-            shape=[batch_size, 1, seq_len, total_seq_len],
-            fill_value=tir.max_value(self.dtype),
-            dtype=self.dtype,
+        def _index(x: te.Tensor):  # x[:-1,:]
+            b, s, d = x.shape
+            return te.compute((b, 1, d), lambda i, _, k: x[i, s - 1, k], name="index")
+
+        hidden_states = self.transformer(inputs, paged_kv_cache)
+        hidden_states = op.tensor_expr_op(
+            _index,
+            name_hint="index",
+            args=[hidden_states],
         )
-        return self.forward(inputs, total_seq_len, attention_mask)
+        logits = self.lm_head(hidden_states)
+        if logits.dtype != "float32":
+            logits = logits.astype("float32")
+        return logits, paged_kv_cache
+
+    def decode(self, inputs: Tensor, paged_kv_cache: PagedKVCache):
+        op_ext.configure()
+
+        hidden_states = self.transformer(inputs, paged_kv_cache)
+        logits = self.lm_head(hidden_states)
+        if logits.dtype != "float32":
+            logits = logits.astype("float32")
+        return logits, paged_kv_cache
+
+    def batch_prefill(self, inputs: Tensor, logit_positions: Tensor, paged_kv_cache: PagedKVCache):
+        logits = self.batch_forward(inputs, paged_kv_cache, logit_positions)
+        return logits, paged_kv_cache
+
+    def batch_decode(self, inputs: Tensor, paged_kv_cache: PagedKVCache):
+        logits = self.batch_forward(inputs, paged_kv_cache)
+        return logits, paged_kv_cache
+
+    def batch_verify(self, inputs: Tensor, paged_kv_cache: PagedKVCache):
+        logits = self.batch_forward(inputs, paged_kv_cache)
+        return logits, paged_kv_cache
 
     def softmax_with_temperature(self, logits: Tensor, temperature: Tensor):
-        return op.softmax(logits / temperature, axis=-1)
+        return op.softmax(logits / op.reshape(temperature, (temperature.shape[0], 1, 1)), axis=-1)
+
+    def create_paged_kv_cache(
+        self,
+        max_batch_size: tir.Var,
+        max_total_seq_len: tir.Var,
+        prefill_chunk_size: tir.Var,
+        page_size: tir.Var,
+    ) -> PagedKVCache:
+        return PagedKVCache.create_generic(
+            max_batch_size=max_batch_size,
+            max_total_seq_len=max_total_seq_len,
+            prefill_chunk_size=prefill_chunk_size,
+            page_size=page_size,
+            num_hidden_layers=self.num_hidden_layers,
+            num_attention_heads=self.num_attention_heads // self.tensor_parallel_shards,
+            num_key_value_heads=self.num_attention_heads // self.tensor_parallel_shards,
+            head_dim=self.head_dim,
+            rope_mode=RopeMode.NORMAL,
+            rope_scale=1,
+            rope_theta=self.rotary_emb_base,
+            dtype=self.dtype,
+        )
 
     def get_default_spec(self):
-        batch_size = 1
         mod_spec = {
-            "prefill": {
-                "inputs": nn.spec.Tensor([batch_size, "seq_len"], "int32"),
-                "total_seq_len": int,
+            "embed": {
+                "input_ids": nn.spec.Tensor([1, "seq_len"], "int32"),
                 "$": {
                     "param_mode": "packed",
-                    "effect_mode": "packed",
+                    "effect_mode": "none",
+                },
+            },
+            "prefill": {
+                "inputs": nn.spec.Tensor([1, "seq_len", self.hidden_size], self.dtype),
+                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
+                "$": {
+                    "param_mode": "packed",
+                    "effect_mode": "none",
                 },
             },
             "decode": {
-                "inputs": nn.spec.Tensor([batch_size, 1], "int32"),
-                "total_seq_len": int,
+                "inputs": nn.spec.Tensor([1, 1, self.hidden_size], self.dtype),
+                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
                 "$": {
                     "param_mode": "packed",
-                    "effect_mode": "packed",
+                    "effect_mode": "none",
+                },
+            },
+            "batch_prefill": {
+                "inputs": nn.spec.Tensor([1, "seq_len", self.hidden_size], self.dtype),
+                "logit_positions": nn.spec.Tensor(["batch_size"], "int32"),
+                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
+                "$": {
+                    "param_mode": "packed",
+                    "effect_mode": "none",
+                },
+            },
+            "batch_decode": {
+                "inputs": nn.spec.Tensor(["batch_size", 1, self.hidden_size], self.dtype),
+                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
+                "$": {
+                    "param_mode": "packed",
+                    "effect_mode": "none",
+                },
+            },
+            "batch_verify": {
+                "inputs": nn.spec.Tensor([1, "seq_len", self.hidden_size], self.dtype),
+                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
+                "$": {
+                    "param_mode": "packed",
+                    "effect_mode": "none",
                 },
             },
             "softmax_with_temperature": {
-                "logits": nn.spec.Tensor([1, 1, "vocab_size"], "float32"),
-                "temperature": nn.spec.Tensor([], "float32"),
+                "logits": nn.spec.Tensor(["batch_size", 1, "vocab_size"], "float32"),
+                "temperature": nn.spec.Tensor(["batch_size"], "float32"),
+                "$": {
+                    "param_mode": "none",
+                    "effect_mode": "none",
+                },
+            },
+            "create_paged_kv_cache": {
+                "max_batch_size": int,
+                "max_total_seq_len": int,
+                "prefill_chunk_size": int,
+                "page_size": int,
                 "$": {
                     "param_mode": "none",
                     "effect_mode": "none",
