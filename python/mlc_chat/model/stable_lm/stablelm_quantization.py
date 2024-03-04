86d07623380051534cf7c58069947915879a391b
Shushi Hong <820958424@qq.com>
2024-03-04 17:10:37 +0800

    Update stablelm_quantization.py

--- python/mlc_chat/model/stable_lm/stablelm_quantization.py
+++ python/mlc_chat/model/stable_lm/stablelm_quantization.py
@@ -7,15 +7,15 @@ from tvm.relax.frontend import nn
 from mlc_chat.loader import QuantizeMapping
 from mlc_chat.quantization import FTQuantize, GroupQuantize, NoQuantize
 
-from .stablelm_model import StableLMEpochConfig, StableLMEpochForCausalLM
+from .stablelm_model import StableLmConfig, StableLmForCausalLM
 
 
 def group_quant(
-    model_config: StableLMEpochConfig,
+    model_config: StableLmConfig,
     quantization: GroupQuantize,
 ) -> Tuple[nn.Module, QuantizeMapping]:
     """Quantize a StableLM-architecture model using group quantization."""
-    model: nn.Module = StableLMEpochForCausalLM(model_config)
+    model: nn.Module = StableLmForCausalLM(model_config)
     model.to(quantization.model_dtype)
     quant_map = QuantizeMapping({}, {})
     model = quantization.quantize_model(
@@ -27,11 +27,11 @@ def group_quant(
 
 
 def ft_quant(
-    model_config: StableLMEpochConfig,
+    model_config: StableLmConfig,
     quantization: FTQuantize,
 ) -> Tuple[nn.Module, QuantizeMapping]:
     """Quantize a StableLM model using FasterTransformer quantization."""
-    model: nn.Module = StableLMEpochForCausalLM(model_config)
+    model: nn.Module = StableLmForCausalLM(model_config)
     model.to(quantization.model_dtype)
     quant_map = QuantizeMapping({}, {})
     model = quantization.quantize_model(
@@ -43,11 +43,11 @@ def ft_quant(
 
 
 def no_quant(
-    model_config: StableLMEpochConfig,
+    model_config: StableLmConfig,
     quantization: NoQuantize,
 ) -> Tuple[nn.Module, QuantizeMapping]:
     """Quantize a StableLM model without quantization."""
-    model: nn.Module = StableLMEpochForCausalLM(model_config)
+    model: nn.Module = StableLmForCausalLM(model_config)
     model.to(quantization.model_dtype)
     quant_map = QuantizeMapping({}, {})
     return model, quant_map
