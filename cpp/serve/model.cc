mlc-ai/mlc-llm

commit a41f9037c4a7d971f6ba70be935d7bf2b453f635

[Serving] Address embedding lookup OOM issue (#1899)
This PR addresses the OOM issue that may be caused by embedding
lookup when the batch size of a prefill action is large.
Prior to this PR, a large embedding tensor will be created for
each sequence in the prefilled batch, thus may take unexpectedly
large memory when the batch size is large.

cpp/serve/model.cc
@@ -19,72 +19,6 @@ namespace serve {
 
 /*********************** Utils ***********************/
 
-/*!
- * \brief Concatenate the input embeddings along the sequence dimension.
- * Store the concatenation result into the input destination NDarray.
- * Return concatenation result as an NDArray view of the destination array.
- * \param embedding_arr The array of embeddings to concatenate.
- * \param total_length The total length of the input embeddings along the sequence dim.
- * \param device The device where the embeddings locate.
- * \param initial_seq_len The initial sequence length to allocate for embeddings.
- * \param dst The destination of the concatenation
- * \return The concatenated embeddings.
- */
-NDArray ConcatEmbeddings(const Array<NDArray>& embedding_arr, int64_t total_length, DLDevice device,
-                         int initial_seq_len, NDArray* dst) {
-  ICHECK(!embedding_arr.empty());
-  if (embedding_arr.size() == 1) {
-    return embedding_arr[0];
-  }
-  ICHECK_NOTNULL(dst);
-  int hidden_size = -1;
-  DataType dtype;
-  for (NDArray inp_embeddings : embedding_arr) {
-    // inp_embedding: (1, n, h)
-    CHECK_EQ(inp_embeddings->ndim, 3);
-    CHECK_EQ(inp_embeddings->shape[0], 1);
-    CHECK_EQ(inp_embeddings->device.device_type, device.device_type);
-    CHECK_EQ(inp_embeddings->device.device_id, device.device_id);
-    if (hidden_size == -1) {
-      hidden_size = inp_embeddings->shape[2];
-      dtype = inp_embeddings.DataType();
-    } else {
-      CHECK_EQ(inp_embeddings->shape[2], hidden_size);
-      CHECK_EQ(inp_embeddings.DataType(), dtype);
-    }
-  }
-
-  // - Resize the shared embedding array.
-  if (dst->defined()) {
-    ICHECK_EQ((*dst)->ndim, 3);
-    ICHECK_EQ((*dst)->shape[0], 1);
-    ICHECK_EQ((*dst)->shape[2], hidden_size);
-  }
-  int64_t init_size = dst->defined() ? (*dst)->shape[1] : initial_seq_len;
-  while (init_size < total_length) {
-    init_size *= 2;
-  }
-  if (!dst->defined() || init_size != (*dst)->shape[1]) {
-    *dst = NDArray::Empty({1, init_size, hidden_size}, dtype, device);
-  }
-
-  // - Copy input embeddings.
-  int64_t start_pos = 0;
-  for (NDArray inp_embeddings : embedding_arr) {
-    int64_t length = inp_embeddings->shape[1];
-    CHECK_LE(start_pos + length, total_length);
-
-    DLTensor copy_dst = *(dst->operator->());
-    copy_dst.byte_offset = start_pos * hidden_size * dtype.bytes();
-    copy_dst.shape = inp_embeddings->shape;
-    NDArray::CopyFromTo(inp_embeddings.operator->(), &copy_dst);
-
-    start_pos += length;
-  }
-  CHECK_EQ(start_pos, total_length);
-  return dst->CreateView({1, total_length, hidden_size}, dtype);
-}
-
 /*! \brief Utility function that copies input array to the device. */
 template <typename T>
 NDArray CopyArrayToDevice(const std::vector<T>& array, NDArray* dst, DLDataType dtype,
@@ -159,37 +93,30 @@ class ModelImpl : public ModelObj {
 
   /*********************** Model Computation  ***********************/
 
-  NDArray TokenEmbed(IntTuple token_ids) final {
+  ObjectRef TokenEmbed(IntTuple token_ids, ObjectRef* dst, int offset) final {
     int num_tokens = token_ids.size();
     std::vector<int32_t> vec_token_ids(token_ids->data, token_ids->data + num_tokens);
     // Copy input token ids to device.
     DLDataType dtype(DataType::Int(32));
     NDArray token_ids_nd =
-        CopyArrayToDevice(vec_token_ids, &input_token_ids_, dtype, max_window_size_, device_);
+        CopyArrayToDevice(vec_token_ids, &input_token_ids_, dtype, prefill_chunk_size_, device_);
     ICHECK_EQ(token_ids_nd->ndim, 1);
     ICHECK_EQ(token_ids_nd->shape[0], num_tokens);
     token_ids_nd = token_ids_nd.CreateView({1, num_tokens}, dtype);
-
-    CHECK(ft_.embed_func_.defined())
-        << "`embed` function is not found in the model. Please make sure the model is compiled "
-           "with flag `--sep-embed` and `--enable-batching`";
-    auto token_ids_dref_or_nd = ft_.CopyToWorker0(token_ids_nd, "token_ids", {max_window_size_});
+    auto token_ids_dref_or_nd = ft_.CopyToWorker0(token_ids_nd, "token_ids", {prefill_chunk_size_});
 
     ObjectRef embeddings = ft_.embed_func_(token_ids_dref_or_nd, params_);
-    NDArray embeddings_ndarray;
-    if (ft_.use_disco) {
-      embeddings_ndarray = Downcast<DRef>(embeddings)->DebugGetFromRemote(0);
+    if (dst != nullptr) {
+      CHECK(dst->defined());
+      ft_.nd_copy_embedding_to_offset_func_(embeddings, *dst, offset);
+      return *dst;
     } else {
-      embeddings_ndarray = Downcast<NDArray>(embeddings);
+      CHECK_EQ(offset, 0);
+      return embeddings;
     }
-    // embeddings: (1, total_length, hidden_size)
-    ICHECK_EQ(embeddings_ndarray->ndim, 3);
-    ICHECK_EQ(embeddings_ndarray->shape[0], 1);
-    ICHECK_EQ(embeddings_ndarray->shape[1], num_tokens);
-    return embeddings_ndarray;
   }
 
-  NDArray BatchPrefill(const Array<NDArray>& embedding_arr, const std::vector<int64_t>& seq_ids,
+  NDArray BatchPrefill(const ObjectRef& embeddings, const std::vector<int64_t>& seq_ids,
                        const std::vector<int>& lengths) final {
     CHECK(!seq_ids.empty());
     CHECK_EQ(seq_ids.size(), lengths.size());
@@ -201,15 +128,6 @@ class ModelImpl : public ModelObj {
       total_length += lengths[i];
       logit_pos.push_back(total_length - 1);
     }
-
-    // embeddings: (1, n, h)
-    NDArray embeddings =
-        ConcatEmbeddings(embedding_arr, total_length, device_, max_window_size_, &embeddings_);
-    ICHECK_EQ(embeddings->ndim, 3);
-    ICHECK_EQ(embeddings->shape[0], 1);
-    ICHECK_EQ(embeddings->shape[1], total_length);
-    ICHECK_EQ(embeddings->device.device_type, device_.device_type);
-    ICHECK_EQ(embeddings->device.device_id, device_.device_id);
 
     NDArray logit_pos_nd =
         CopyArrayToDevice(logit_pos, &logit_pos_arr_, DataType::Int(32), 32, device_);
@@ -226,8 +144,23 @@ class ModelImpl : public ModelObj {
     IntTuple lengths_tuple(lengths.begin(), lengths.end());
     ft_.kv_cache_begin_forward_func_(kv_cache_, seq_ids_tuple, lengths_tuple);
 
-    ObjectRef embeddings_dref_or_nd = ft_.CopyToWorker0(
-        embeddings, "embedding_prefill", {1, max_window_size_, embeddings.Shape()[2]});
+    ObjectRef embeddings_dref_or_nd;
+    if (!embeddings->IsInstance<DRefObj>()) {
+      // embeddings: (1, n, h)
+      NDArray embeddings_nd = Downcast<NDArray>(embeddings);
+      ICHECK_NE(hidden_size_, -1);
+      ICHECK_EQ(embeddings_nd->ndim, 3);
+      ICHECK_EQ(embeddings_nd->shape[0], 1);
+      ICHECK_GE(embeddings_nd->shape[1], total_length);
+      ICHECK_EQ(embeddings_nd->shape[2], hidden_size_);
+      ICHECK_EQ(embeddings_nd->device.device_type, device_.device_type);
+      ICHECK_EQ(embeddings_nd->device.device_id, device_.device_id);
+      embeddings_dref_or_nd =
+          embeddings_nd.CreateView({1, total_length, hidden_size_}, embeddings_nd->dtype);
+    } else {
+      ShapeTuple embedding_shape{1, total_length, hidden_size_};
+      embeddings_dref_or_nd = ft_.nd_view_func_(embeddings, embedding_shape);
+    }
     ObjectRef logit_pos_dref_or_nd =
         ft_.CopyToWorker0(logit_pos_nd, "logit_pos", {max_num_sequence_});
     // args: embeddings, logit_pos, kv_cache, params
@@ -254,13 +187,8 @@ class ModelImpl : public ModelObj {
     return logits;
   }
 
-  NDArray BatchDecode(const NDArray& embeddings, const std::vector<int64_t>& seq_ids) final {
-    // embeddings: (b, 1, h)
-    CHECK_EQ(embeddings->ndim, 3);
-    CHECK_EQ(embeddings->shape[0], seq_ids.size());
-    CHECK_EQ(embeddings->shape[1], 1);
-    CHECK_EQ(embeddings->device.device_type, device_.device_type);
-    CHECK_EQ(embeddings->device.device_id, device_.device_id);
+  NDArray BatchDecode(const ObjectRef& embeddings, const std::vector<int64_t>& seq_ids) final {
+    int num_sequence = seq_ids.size();
 
     CHECK(ft_.decode_func_.defined())
         << "`decode_with_embed` function is not found in the model. Please make sure the model is "
@@ -272,11 +200,26 @@ class ModelImpl : public ModelObj {
     // Reserve in KV cache for the lengths of the input.
     // Begin forward with the sequence ids and new lengths.
     IntTuple seq_ids_tuple(seq_ids);
-    IntTuple lengths_tuple(std::vector<int64_t>(/*n=*/embeddings->shape[0], /*v=*/1));
+    IntTuple lengths_tuple(std::vector<int64_t>(/*n=*/seq_ids.size(), /*v=*/1));
     ft_.kv_cache_begin_forward_func_(kv_cache_, seq_ids_tuple, lengths_tuple);
 
-    ObjectRef embeddings_dref_or_nd = ft_.CopyToWorker0(
-        embeddings, "embedding_decode", {max_num_sequence_, 1, embeddings.Shape()[2]});
+    ObjectRef embeddings_dref_or_nd;
+    if (!embeddings->IsInstance<DRefObj>()) {
+      // embeddings: (1, b, h)
+      NDArray embeddings_nd = Downcast<NDArray>(embeddings);
+      ICHECK_NE(hidden_size_, -1);
+      ICHECK_EQ(embeddings_nd->ndim, 3);
+      ICHECK_EQ(embeddings_nd->shape[0], 1);
+      ICHECK_GE(embeddings_nd->shape[1], num_sequence);
+      ICHECK_EQ(embeddings_nd->shape[2], hidden_size_);
+      ICHECK_EQ(embeddings_nd->device.device_type, device_.device_type);
+      ICHECK_EQ(embeddings_nd->device.device_id, device_.device_id);
+      embeddings_dref_or_nd =
+          embeddings_nd.CreateView({num_sequence, 1, hidden_size_}, embeddings_nd->dtype);
+    } else {
+      ShapeTuple embedding_shape{num_sequence, 1, hidden_size_};
+      embeddings_dref_or_nd = ft_.nd_view_func_(embeddings, embedding_shape);
+    }
 
     // args: embeddings, kv_cache, params
     ObjectRef ret;
@@ -297,12 +240,12 @@ class ModelImpl : public ModelObj {
 
     // logits: (b, 1, v)
     ICHECK_EQ(logits->ndim, 3);
-    ICHECK_EQ(logits->shape[0], embeddings->shape[0]);
+    ICHECK_EQ(logits->shape[0], num_sequence);
     ICHECK_EQ(logits->shape[1], 1);
     return logits;
   }
 
-  NDArray BatchVerify(const NDArray& embeddings, const std::vector<int64_t>& seq_ids,
+  NDArray BatchVerify(const ObjectRef& embeddings, const std::vector<int64_t>& seq_ids,
                       const std::vector<int>& lengths) final {
     CHECK(!seq_ids.empty());
     CHECK_EQ(seq_ids.size(), lengths.size());
@@ -311,13 +254,6 @@ class ModelImpl : public ModelObj {
     for (int i = 0; i < num_sequences; ++i) {
       total_length += lengths[i];
     }
-
-    // embeddings: (1, n, h)
-    ICHECK_EQ(embeddings->ndim, 3);
-    ICHECK_EQ(embeddings->shape[0], 1);
-    ICHECK_EQ(embeddings->shape[1], total_length);
-    ICHECK_EQ(embeddings->device.device_type, device_.device_type);
-    ICHECK_EQ(embeddings->device.device_id, device_.device_id);
 
     CHECK(ft_.verify_func_.defined())
         << "`verify_with_embed` function is not found in the model. Please make sure the model is "
@@ -331,8 +267,23 @@ class ModelImpl : public ModelObj {
     IntTuple lengths_tuple(lengths.begin(), lengths.end());
     ft_.kv_cache_begin_forward_func_(kv_cache_, seq_ids_tuple, lengths_tuple);
 
-    ObjectRef embeddings_dref_or_nd = ft_.CopyToWorker0(
-        embeddings, "embedding_verify", {1, max_window_size_, embeddings.Shape()[2]});
+    ObjectRef embeddings_dref_or_nd;
+    if (!embeddings->IsInstance<DRefObj>()) {
+      // embeddings: (1, n, h)
+      NDArray embeddings_nd = Downcast<NDArray>(embeddings);
+      ICHECK_NE(hidden_size_, -1);
+      ICHECK_EQ(embeddings_nd->ndim, 3);
+      ICHECK_EQ(embeddings_nd->shape[0], 1);
+      ICHECK_GE(embeddings_nd->shape[1], total_length);
+      ICHECK_EQ(embeddings_nd->shape[2], hidden_size_);
+      ICHECK_EQ(embeddings_nd->device.device_type, device_.device_type);
+      ICHECK_EQ(embeddings_nd->device.device_id, device_.device_id);
+      embeddings_dref_or_nd =
+          embeddings_nd.CreateView({1, total_length, hidden_size_}, embeddings_nd->dtype);
+    } else {
+      ShapeTuple embedding_shape{1, total_length, hidden_size_};
+      embeddings_dref_or_nd = ft_.nd_view_func_(embeddings, embedding_shape);
+    }
     // args: embeddings, logit_pos, kv_cache, params
     ObjectRef ret = ft_.verify_func_(embeddings_dref_or_nd, kv_cache_, params_);
     NDArray logits;
@@ -407,6 +358,26 @@ class ModelImpl : public ModelObj {
     return max_window_size_;
   }
 
+  ObjectRef AllocEmbeddingTensor() final {
+    // Allocate the embedding tensor.
+    ObjectRef embedding = ft_.alloc_embedding_tensor_func_();
+    // Get the shape of the embedding tensor for hidden size.
+    ShapeTuple embedding_shape;
+    if (ft_.use_disco) {
+      ICHECK(embedding->IsInstance<DRefObj>());
+      ObjectRef shape_ref = ft_.nd_get_shape_func_(embedding);
+      embedding_shape = Downcast<DRef>(shape_ref)->DebugGetFromRemote(0);
+    } else {
+      NDArray embedding_nd = Downcast<NDArray>(embedding);
+      embedding_shape = embedding_nd.Shape();
+    }
+    ICHECK_EQ(embedding_shape.size(), 3);
+    ICHECK_EQ(embedding_shape[0], 1);
+    ICHECK_EQ(embedding_shape[1], prefill_chunk_size_);
+    this->hidden_size_ = embedding_shape[2];
+    return embedding;
+  }
+
   void Reset() final {
     // Reset the KV cache.
     if (kv_cache_.defined()) {
@@ -437,6 +408,12 @@ class ModelImpl : public ModelObj {
     } else {
       LOG(FATAL) << "Key \"tensor_parallel_shards\" not found.";
     }
+    if (config.count("prefill_chunk_size")) {
+      CHECK(config["prefill_chunk_size"].is<int64_t>());
+      this->prefill_chunk_size_ = config["prefill_chunk_size"].get<int64_t>();
+    } else {
+      LOG(FATAL) << "Key \"prefill_chunk_size\" not found.";
+    }
     if (config.count("vocab_size")) {
       CHECK(config["vocab_size"].is<int64_t>());
       this->vocab_size_ = config["vocab_size"].get<int64_t>();
@@ -452,6 +429,8 @@ class ModelImpl : public ModelObj {
   int max_window_size_ = -1;
   int num_shards_ = -1;
   int max_num_sequence_ = -1;
+  int prefill_chunk_size_ = -1;
+  int hidden_size_ = -1;
   int vocab_size_ = -1;
   //----------------------------
   // TVM related states
@@ -466,10 +445,27 @@ class ModelImpl : public ModelObj {
   ObjectRef params_;
   // Shared NDArray
   NDArray input_token_ids_{nullptr};
-  NDArray embeddings_{nullptr};
   NDArray logit_pos_arr_{nullptr};
-  NDArray temperature_arr_{nullptr};
 };
+
+TVM_REGISTER_GLOBAL("mlc.copy_embedding_to_offset")
+    .set_body_typed([](NDArray embedding, NDArray dst, int offset) {
+      // embedding: (1, m, hidden_size)
+      // dst: (1, prefill_chunk_size, hidden_size)
+      ICHECK_EQ(embedding->ndim, 3);
+      ICHECK_EQ(embedding->shape[0], 1);
+      ICHECK_EQ(dst->ndim, 3);
+      ICHECK_EQ(dst->shape[0], 1);
+      ICHECK_LE(embedding->shape[1] + offset, dst->shape[1]);
+      ICHECK_EQ(embedding->shape[2], dst->shape[2]);
+      const DLTensor& copy_src = *(embedding.operator->());
+      const DLTensor* p_copy_dst = dst.operator->();
+      DLTensor copy_dst = *p_copy_dst;
+      copy_dst.shape = embedding->shape;
+      copy_dst.byte_offset =
+          offset * embedding->shape[2] * ((embedding->dtype.bits * embedding->dtype.lanes + 7) / 8);
+      NDArray::CopyFromTo(&copy_src, &copy_dst);
+    });
 
 }  // namespace serve
 }  // namespace llm

cpp/serve/data.cc
@@ -31,7 +31,7 @@ int TextDataNode::GetLength() const {
                 "Please tokenize the text and construct a TokenData object.";
 }
 
-NDArray TextDataNode::GetEmbedding(Model model) const {
+ObjectRef TextDataNode::GetEmbedding(Model model, ObjectRef* dst, int offset) const {
   LOG(FATAL) << "\"GetEmbedding\" for TextData is not supported. "
                 "Please tokenize the text and construct a TokenData object.";
 }
@@ -62,7 +62,9 @@ TokenData::TokenData(std::vector<int32_t> token_ids) {
 
 int TokenDataNode::GetLength() const { return token_ids.size(); }
 
-NDArray TokenDataNode::GetEmbedding(Model model) const { return model->TokenEmbed(token_ids); }
+ObjectRef TokenDataNode::GetEmbedding(Model model, ObjectRef* dst, int offset) const {
+  return model->TokenEmbed(token_ids, dst, offset);
+}
 
 TVM_REGISTER_GLOBAL("mlc.serve.TokenData").set_body([](TVMArgs args, TVMRetValue* rv) {
   std::vector<int32_t> token_ids;

cpp/serve/data.h
@@ -29,8 +29,19 @@ class DataNode : public Object {
   /*! \brief Get the length (equivalent number of tokens) of the data. */
   virtual int GetLength() const = 0;
 
-  /*! \brief Compute the embedding of this data with regard to the input model. */
-  virtual NDArray GetEmbedding(Model model) const = 0;
+  /*!
+   * \brief Compute the embedding of this data with regard to the input model.
+   * When the input destination pointer is not nullptr, it in-place writes the
+   * embedding into the input destination array at the given offset.
+   * Otherwise, the embeddings will be directly returned back.
+   * \param model The model to take embeddings from.
+   * \param dst The destination array of the embedding lookup.
+   * \param offset The token offset where the computed embeddings will be written
+   * into the destination array.
+   * \return The updated destination embedding array or the computed embeddings.
+   * \note When `dst` is nullptr, we require `offset` to be 0.
+   */
+  virtual ObjectRef GetEmbedding(Model model, ObjectRef* dst = nullptr, int offset = 0) const = 0;
 
   static constexpr const char* _type_key = "mlc.serve.Data";
   static constexpr const bool _type_has_method_sequal_reduce = false;
@@ -52,7 +63,7 @@ class TextDataNode : public DataNode {
   String text;
 
   int GetLength() const final;
-  NDArray GetEmbedding(Model model) const final;
+  ObjectRef GetEmbedding(Model model, ObjectRef* dst = nullptr, int offset = 0) const final;
 
   static constexpr const char* _type_key = "mlc.serve.TextData";
   TVM_DECLARE_BASE_OBJECT_INFO(TextDataNode, DataNode);
@@ -74,7 +85,7 @@ class TokenDataNode : public DataNode {
   IntTuple token_ids;
 
   int GetLength() const final;
-  NDArray GetEmbedding(Model model) const final;
+  ObjectRef GetEmbedding(Model model, ObjectRef* dst = nullptr, int offset = 0) const final;
 
   static constexpr const char* _type_key = "mlc.serve.TokenData";
   TVM_DECLARE_BASE_OBJECT_INFO(TokenDataNode, DataNode);

cpp/serve/engine.cc
@@ -63,6 +63,7 @@ class EngineImpl : public Engine {
     // Step 2. Initialize each model independently.
     //         Create the logit processor and sampler.
     this->models_.clear();
+    this->model_workspaces_.clear();
     for (const auto& model_info : model_infos) {
       TVMArgValue model_lib = std::get<0>(model_info);
       String model_path = std::get<1>(model_info);
@@ -75,6 +76,7 @@ class EngineImpl : public Engine {
           << ", is smaller than the pre-defined max single sequence length, "
           << this->max_single_sequence_length_;
       this->models_.push_back(model);
+      this->model_workspaces_.push_back(ModelWorkspace{model->AllocEmbeddingTensor()});
     }
     int max_logit_processor_num_token = kv_cache_config_->max_num_sequence;
     if (engine_mode_->enable_speculative) {
@@ -88,22 +90,24 @@ class EngineImpl : public Engine {
       // Speculative decoding is only possible for more than one model.
       ICHECK_GT(this->models_.size(), 1U);
       this->actions_ = {
-          EngineAction::NewRequestPrefill(this->models_,           //
-                                          logit_processor,         //
-                                          sampler,                 //
-                                          this->kv_cache_config_,  //
-                                          this->engine_mode_,      //
+          EngineAction::NewRequestPrefill(this->models_,            //
+                                          logit_processor,          //
+                                          sampler,                  //
+                                          this->model_workspaces_,  //
+                                          this->kv_cache_config_,   //
+                                          this->engine_mode_,       //
                                           this->trace_recorder_),
           EngineAction::BatchDraft(this->models_, logit_processor, sampler, this->trace_recorder_,
                                    this->engine_mode_->spec_draft_length),
           EngineAction::BatchVerify(this->models_, logit_processor, sampler, this->kv_cache_config_,
                                     this->trace_recorder_)};
     } else {
-      this->actions_ = {EngineAction::NewRequestPrefill(this->models_,           //
-                                                        logit_processor,         //
-                                                        sampler,                 //
-                                                        this->kv_cache_config_,  //
-                                                        this->engine_mode_,      //
+      this->actions_ = {EngineAction::NewRequestPrefill(this->models_,            //
+                                                        logit_processor,          //
+                                                        sampler,                  //
+                                                        this->model_workspaces_,  //
+                                                        this->kv_cache_config_,   //
+                                                        this->engine_mode_,       //
                                                         this->trace_recorder_),
                         EngineAction::BatchDecode(this->models_, logit_processor, sampler,
                                                   this->trace_recorder_)};
@@ -250,6 +254,8 @@ class EngineImpl : public Engine {
   std::shared_ptr<GrammarStateInitContext> json_grammar_state_init_ctx_;
   // Models
   Array<Model> models_;
+  // Workspace of each model.
+  std::vector<ModelWorkspace> model_workspaces_;
   // Request stream callback function
   Optional<PackedFunc> request_stream_callback_;
   // Engine actions.

cpp/serve/engine_actions/action.h
@@ -55,14 +55,16 @@ class EngineAction : public ObjectRef {
    * \param models The models to run prefill in.
    * \param logit_processor The logit processor.
    * \param sampler The sampler to sample new tokens.
+   * \param model_workspaces The workspace of each model.
    * \param kv_cache_config The KV cache config to help decide prefill is doable.
    * \param engine_mode The engine operation mode.
    * \param trace_recorder The event trace recorder for requests.
    * \return The created action object.
    */
   static EngineAction NewRequestPrefill(Array<Model> models, LogitProcessor logit_processor,
-                                        Sampler sampler, KVCacheConfig kv_cache_config,
-                                        EngineMode engine_mode,
+                                        Sampler sampler,
+                                        std::vector<ModelWorkspace> model_workspaces,
+                                        KVCacheConfig kv_cache_config, EngineMode engine_mode,
                                         Optional<EventTraceRecorder> trace_recorder);
   /*!
    * \brief Create the action that runs one-step decode for requests in the

cpp/serve/engine_actions/batch_decode.cc
@@ -81,20 +81,16 @@ class BatchDecodeActionObj : public EngineActionObj {
 
     // - Compute embeddings.
     RECORD_EVENT(trace_recorder_, request_ids, "start embedding");
-    NDArray embeddings =
+    ObjectRef embeddings =
         models_[0]->TokenEmbed({IntTuple{input_tokens.begin(), input_tokens.end()}});
     RECORD_EVENT(trace_recorder_, request_ids, "finish embedding");
-    ICHECK_EQ(embeddings->ndim, 3);
-    ICHECK_EQ(embeddings->shape[0], 1);
-    ICHECK_EQ(embeddings->shape[1], num_rsentries);
-    embeddings = embeddings.CreateView({num_rsentries, 1, embeddings->shape[2]}, embeddings->dtype);
 
     // - Invoke model decode.
     RECORD_EVENT(trace_recorder_, request_ids, "start decode");
     NDArray logits = models_[0]->BatchDecode(embeddings, request_internal_ids);
     RECORD_EVENT(trace_recorder_, request_ids, "finish decode");
     ICHECK_EQ(logits->ndim, 3);
-    ICHECK_EQ(logits->shape[0], embeddings->shape[0]);
+    ICHECK_EQ(logits->shape[0], num_rsentries);
     ICHECK_EQ(logits->shape[1], 1);
 
     // - Update logits.

cpp/serve/engine_actions/batch_draft.cc
@@ -89,21 +89,16 @@ class BatchDraftActionObj : public EngineActionObj {
 
         // - Compute embeddings.
         RECORD_EVENT(trace_recorder_, request_ids, "start proposal embedding");
-        NDArray embeddings =
+        ObjectRef embeddings =
             models_[model_id]->TokenEmbed({IntTuple{input_tokens.begin(), input_tokens.end()}});
         RECORD_EVENT(trace_recorder_, request_ids, "finish proposal embedding");
-        ICHECK_EQ(embeddings->ndim, 3);
-        ICHECK_EQ(embeddings->shape[0], 1);
-        ICHECK_EQ(embeddings->shape[1], num_rsentries);
-        embeddings =
-            embeddings.CreateView({num_rsentries, 1, embeddings->shape[2]}, embeddings->dtype);
 
         // - Invoke model decode.
         RECORD_EVENT(trace_recorder_, request_ids, "start proposal decode");
         NDArray logits = models_[model_id]->BatchDecode(embeddings, request_internal_ids);
         RECORD_EVENT(trace_recorder_, request_ids, "finish proposal decode");
         ICHECK_EQ(logits->ndim, 3);
-        ICHECK_EQ(logits->shape[0], embeddings->shape[0]);
+        ICHECK_EQ(logits->shape[0], num_rsentries);
         ICHECK_EQ(logits->shape[1], 1);
 
         // - Update logits.

cpp/serve/engine_actions/batch_verify.cc
@@ -89,7 +89,7 @@ class BatchVerifyActionObj : public EngineActionObj {
     }
 
     RECORD_EVENT(trace_recorder_, request_ids, "start verify embedding");
-    NDArray embeddings = models_[verify_model_id_]->TokenEmbed(
+    ObjectRef embeddings = models_[verify_model_id_]->TokenEmbed(
         {IntTuple{all_tokens_to_verify.begin(), all_tokens_to_verify.end()}});
     RECORD_EVENT(trace_recorder_, request_ids, "finish verify embedding");
 

commit 94adcf62ab2d06426eb14385dc10d3d938414225

[Serving] Fix Model TokenEmbed function with TP
This PR fixes a severe bug introduced by #1899.

Since #1899, we no longer copy the embedding back from worker 0
when using tensor parallelism. However, we did not synchronize
with the worker 0.

This will cause the following issue: in batch prefill, we will
continuously call TokenEmbed for multiple times. Each time, we
will copy the token ids to the `token_ids` NDArray on worker 0.
If we do not synchronize with worker 0, then it is possible that
the local token ids have been updated for multiple times, before
the first `CopyToWorker0` really starts to execute on the worker 0
side. As a result, at the time of executing the token ids copy to
worker 0, the local token ids might be wrong (by "wrong", say we
are executing the copying of seq 0's token ids, then the actual
local token ids array might have already been seq 3's token ids).

As a result, the issue will cause the batch prefill behave completely
wrong. This PR adds a synchronization with worker 0 explicitly.

cpp/serve/model.cc
@@ -109,6 +109,9 @@ class ModelImpl : public ModelObj {
     if (dst != nullptr) {
       CHECK(dst->defined());
       ft_.nd_copy_embedding_to_offset_func_(embeddings, *dst, offset);
+      if (ft_.use_disco) {
+        ft_.sess->SyncWorker(0);
+      }
       return *dst;
     } else {
       CHECK_EQ(offset, 0);