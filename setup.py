36d3d02d852ac0cbf82d8ab1ee02238eaa615467
Mehrdad Hessar <mhessar@octo.ai>
2024-03-20 19:01:04 +0100

    Fix versioning in setup.py (#231)
    fix versioning in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ import os
 
 CURRENT_DIR = os.path.dirname(__file__)
 
+def get_lib_path():
+    """Get library path, name and version"""
+    # Directly exec libinfo to get the right setup
+    libinfo_py = os.path.join(CURRENT_DIR, "./python/mlc_chat/libinfo.py")
+    libinfo = {"__file__": libinfo_py}
+    with open(libinfo_py, "rb") as f:
+        exec(compile(f.read(), libinfo_py, "exec"), libinfo, libinfo)
+    version = libinfo["__version__"]
+    return version
 
 def git_describe_version(original_version):
     """Get git describe version."""
@@ -17,10 +26,14 @@ def git_describe_version(original_version):
     _, gd_version = libver["git_describe_version"]()
     if gd_version is not None and gd_version != original_version:
         print("Use git describe based version %s" % gd_version)
+    if gd_version is None:
+        print(f"Use original version {original_version}")
+        return original_version
     return gd_version
 
 
-__version__ = git_describe_version(None)
+__version__ = get_lib_path()
+__version__ = git_describe_version(__version__)
 
 setup(
     name="mlc_llm",
