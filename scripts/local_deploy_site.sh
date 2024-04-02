52de79860b935593e301cd70d1ab5bd1f2939a90
David Pissarra <61968959+davidpissarra@users.noreply.github.com>
2024-04-02 14:51:35 +0100

    [Docs] API Code Completion Guide (#2054)

--- scripts/local_deploy_site.sh
+++ scripts/local_deploy_site.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 
 scripts/build_site.sh
 
-cd site && jekyll serve  --skip-initial-build --host localhost --baseurl /mlc-llm --port 8888
+cd site && jekyll serve  --skip-initial-build --host localhost --baseurl / --port 8888
