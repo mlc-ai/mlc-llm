b5eea47571aa11e673141457f02b39030322902d
Shushi Hong <820958424@qq.com>
2024-04-03 01:30:07 +0800

    fix lint

--- python/mlc_llm/model/qwen/qwen_model.py
+++ python/mlc_llm/model/qwen/qwen_model.py
@@ -93,7 +93,6 @@ class QWenAttention(nn.Module):  # pylint: disable=too-many-instance-attributes
 
         self.c_proj = nn.Linear(self.num_heads * self.head_dim, config.hidden_size, bias=False)
 
-
     def forward(  # pylint: disable=too-many-locals
         self,
         hidden_states: Tensor,
@@ -155,7 +154,7 @@ class QWenBlock(nn.Module):
             _set(self.attn.c_proj.weight, tp.ShardSingleDim("_shard_attn_c_proj", dim=1))
             _set(
                 self.mlp.gate_up_proj.weight,
-                tp.ShardSingleDim("_shard_mlp_gate_up_proj", segs=[i, i], dim=0)
+                tp.ShardSingleDim("_shard_mlp_gate_up_proj", segs=[i, i], dim=0),
             )
             _set(self.mlp.c_proj.weight, tp.ShardSingleDim("_shard_mlp_c_proj", dim=1))
 
