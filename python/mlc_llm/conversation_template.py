3cb8d48afd254a420ca49f6c42c7bc2a6fe35c10
Shushi Hong <820958424@qq.com>
2024-04-06 14:56:27 +0800

    Update conversation_template.py

--- python/mlc_llm/conversation_template.py
+++ python/mlc_llm/conversation_template.py
@@ -475,3 +475,23 @@ ConvTemplateRegistry.register_conv_template(
         system_prefix_token_ids=[1],
     )
 )
+
+# GLM
+ConvTemplateRegistry.register_conv_template(
+    Conversation(
+        name="glm",
+        system_template=f"{MessagePlaceholders.SYSTEM.value}",
+        system_message="",
+        roles={
+            "user": "问",
+            "assistant": "答",
+            "tool": "问",
+        },
+        seps=["\n\n"],
+        role_content_sep=": ",
+        role_empty_sep=":",
+        stop_str=["</s>"],
+        stop_token_ids=[2],
+        system_prefix_token_ids=[64790, 64792],
+    )
+)
