12fa30bf5789b11cfee98870352518cf810fa802
Shushi Hong <820958424@qq.com>
2024-04-06 14:57:05 +0800

    Update model.py

--- python/mlc_llm/model/model.py
+++ python/mlc_llm/model/model.py
@@ -9,6 +9,7 @@ from mlc_llm.loader import ExternMapping, QuantizeMapping
 from mlc_llm.quantization.quantization import Quantization
 
 from .baichuan import baichuan_loader, baichuan_model, baichuan_quantization
+from .chatglm3 import chatglm3_loader, chatglm3_model, chatglm3_quantization
 from .gemma import gemma_loader, gemma_model, gemma_quantization
 from .gpt2 import gpt2_loader, gpt2_model, gpt2_quantization
 from .gpt_bigcode import gpt_bigcode_loader, gpt_bigcode_model, gpt_bigcode_quantization
