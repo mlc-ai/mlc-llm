386819a80b7cbf0b4dacc867a8fff35d79fe8ccb
Shushi Hong <820958424@qq.com>
2024-04-06 15:43:30 +0800

    fix isort

--- python/mlc_llm/model/chatglm3/chatglm3_quantization.py
+++ python/mlc_llm/model/chatglm3/chatglm3_quantization.py
@@ -2,9 +2,10 @@
 or other formats."""
 from typing import Tuple
 
-from mlc_llm.loader import QuantizeMapping
-from mlc_llm.quantization import FTQuantize, GroupQuantize, NoQuantize
 from tvm.relax.frontend import nn
+
+from mlc_llm.loader import QuantizeMapping
+from mlc_llm.quantization import AWQQuantize, FTQuantize, GroupQuantize, NoQuantize
 
 from .chatglm3_model import ChatGLMForCausalLM, GLMConfig
 
