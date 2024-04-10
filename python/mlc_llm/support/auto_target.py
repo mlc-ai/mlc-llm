f832bde67f5149d9a2a0332d72368d22cf64b7b7
Tianqi Chen <tqchen@users.noreply.github.com>
2024-04-10 10:43:18 -0400

    Revert "Allow "mlc_llm --host" option to override host triple the model compi…" (#2115)
    This reverts commit 12ca8fdbe2a24f43bbc72241a76735dbad8c2026.
    
    Co-authored-by: Mengshiun Yu <mengshyu@gmail.com>

--- python/mlc_llm/support/auto_target.py
+++ python/mlc_llm/support/auto_target.py
@@ -41,7 +41,7 @@ def detect_target_and_host(target_hint: str, host_hint: str = "auto") -> Tuple[T
         The hint for the host CPU, default is "auto".
     """
     target, build_func = _detect_target_gpu(target_hint)
-    if target.host is None or host_hint != "auto":
+    if target.host is None:
         target = Target(target, host=_detect_target_host(host_hint))
     if target.kind.name == "cuda":
         # Enable thrust for CUDA
