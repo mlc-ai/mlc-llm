b3943354388833aa4cbe6cd5579dbc3720da1f0d
spectrometerHBH <spectrometerh@gmail.com>
2024-04-20 23:09:32 -0400

    batch verify

---
+++ python/mlc_llm/op/batch_spec_verify.py
@@ -0,0 +1,113 @@
+"""Operators for batch verify in speculative decoding."""
+
+import tvm
+from tvm.relax.frontend.nn import Tensor, op
+from tvm.script import tir as T
+
+# mypy: disable-error-code="attr-defined,valid-type,name-defined"
+# pylint: disable=too-many-locals,invalid-name,too-many-arguments,too-many-statements
+
+
+def batch_spec_verify(num_nodes, vocab, nbatch):
+    """Return the TIR function for batch verify in speculative decoding."""
+    TX = 128
+    VEC = 4
+
+    def _var(dtype="int32"):
+        return T.alloc_buffer((1,), dtype, scope="local")
+
+    # fmt: off
+    @T.prim_func(check_well_formed=False, private=True)
+    def _func(
+        draft_probs: T.Buffer((num_nodes, vocab), "float32"),
+        draft_tokens: T.Buffer((num_nodes,), "int32"),
+        model_probs: T.Buffer((num_nodes, vocab), "float32"),
+        token_tree_first_child: T.Buffer((num_nodes,), "int32"),
+        token_tree_next_sibling: T.Buffer((num_nodes,), "int32"),
+        uniform_samples: T.Buffer((num_nodes,), "float32"),
+        token_tree_parent_ptr: T.Buffer((nbatch,), "int32"),
+        token_tree_child_ptr: T.Buffer((nbatch,), "int32"),
+    ):
+        """
+        [
+            blockIdx.x on batch,
+            threadIdx.x on vocab,
+            for loop over excessive amounts
+        ]
+        """
+        for _bx in T.thread_binding(0, nbatch, thread="blockIdx.x"):
+            for _tx in T.thread_binding(0, TX, thread="threadIdx.x"):
+                with T.block("CTA"):
+                    child_ptr = _var()
+                    parent_ptr = _var()
+                    child_token = _var()
+                    done = _var("bool")
+                    psum = _var("float32")
+                    t0 = _var("float32")
+                    model_prob_local = T.alloc_buffer((VEC,), "float32", scope="local")
+                    draft_prob_local = T.alloc_buffer((VEC,), "float32", scope="local")
+
+                    # batch size
+                    b = T.axis.S(nbatch, _bx)
+                    tx = T.axis.S(TX, _tx)
+                    # or simmply while true
+                    parent_ptr[0] = token_tree_parent_ptr[b]
+                    child_ptr[0] = token_tree_child_ptr[b]
+                    done[0] = False
+
+                    while T.Not(done[0]):
+                        if child_ptr[0] == -1:
+                            done[0] = True
+                        else:
+                            # decide to validate current ptr
+                            child_token[0] = draft_tokens[child_ptr[0]]
+                            p_child = T.meta_var(model_probs[parent_ptr[0], child_token[0]])
+                            q_child = T.meta_var(draft_probs[child_ptr[0], child_token[0]])
+                            uniform_sample = T.meta_var(uniform_samples[child_ptr[0]])
+                            # accept the proposal, we move to child
+                            if p_child / q_child >= uniform_sample:
+                                parent_ptr[0] = child_ptr[0]
+                                child_ptr[0] = token_tree_first_child[child_ptr[0]]
+                            else:
+                                psum[0] = 0.0
+                                # renormalize probability, predicated by stopped_expansion[b]:
+                                for i in T.serial(T.ceildiv(vocab, TX * VEC)):
+                                    for vec in T.vectorized(VEC):
+                                        k = T.meta_var(i * TX * VEC + tx * VEC + vec)
+                                        model_prob_local[vec] = T.if_then_else(k < vocab, model_probs[parent_ptr[0], k], 0.0)
+                                    for vec in T.vectorized(VEC):
+                                        k = T.meta_var(i * TX * VEC + tx * VEC + vec)
+                                        draft_prob_local[vec] = T.if_then_else(k < vocab, draft_probs[child_ptr[0], k], 0.0)
+                                    for vec in T.serial(VEC): # vectorize?
+                                        model_prob_local[vec] = T.max(model_prob_local[vec] - draft_prob_local[vec], 0.0)
+                                    for vec in T.vectorized(VEC):
+                                        k = T.meta_var(i * TX * VEC + tx * VEC + vec)
+                                        if k < vocab:
+                                            model_probs[parent_ptr[0], k] = model_prob_local[vec]
+                                    for vec in T.serial(VEC):
+                                        psum[0] += model_prob_local[vec]
+                                        
+                                with T.block("block_cross_thread"):
+                                    T.reads(psum[0])
+                                    T.writes(t0[0])
+                                    T.attr(
+                                        T.comm_reducer(lambda x0, y0: x0 + y0, [T.float32(0)]),
+                                        "reduce_scope",
+                                        T.reinterpret("handle", T.uint64(0)),
+                                    )
+                                    T.tvm_thread_allreduce(T.uint32(1), psum[0], True, t0[0], tx, dtype="handle")
+                                
+                                # renormalize
+                                for i in T.serial(T.ceildiv(vocab, TX * VEC)):
+                                    for vec in T.vectorized(VEC):
+                                        k = T.meta_var(i * TX * VEC + tx * VEC + vec)
+                                        if k < vocab:
+                                            model_probs[parent_ptr[0], k] = model_probs[parent_ptr[0], k] / t0[0]
+
+                                child_ptr[0] = token_tree_next_sibling[child_ptr[0]]
+
+                    token_tree_parent_ptr[b] = parent_ptr[0]
+                    token_tree_child_ptr[b] = child_ptr[0]
+    # fmt: on
+
+    return _func
---
+++ tests/python/op/test_batch_spec_verify.py
@@ -0,0 +1,148 @@
+import numpy as np
+
+import tvm
+from tvm.script import tir as T
+from tvm.script import relax as R
+import tvm.testing
+
+from mlc_llm.op.batch_spec_verify import batch_spec_verify
+
+
+def numpy_reference(
+    draft_probs,
+    draft_tokens,
+    model_probs,
+    token_tree_first_child,
+    token_tree_next_sibling,
+    uniform_samples,
+    token_tree_parent_ptr,
+    token_tree_child_ptr,
+):
+    nbatch = token_tree_parent_ptr.shape[0]
+    for b in range(nbatch):
+        parent_ptr = token_tree_parent_ptr[b]
+        child_ptr = token_tree_child_ptr[b]
+        while child_ptr != -1:
+            child_token = draft_tokens[child_ptr]
+            p_child = model_probs[parent_ptr, child_token]
+            q_child = draft_probs[child_ptr, child_token]
+            uniform_sample = uniform_samples[child_ptr]
+            if p_child / q_child >= uniform_sample:
+                parent_ptr = child_ptr
+                child_ptr = token_tree_first_child[child_ptr]
+            else:
+                model_probs[parent_ptr, :] = np.maximum(
+                    model_probs[parent_ptr, :] - draft_probs[child_ptr, :], 0.0
+                )
+                psum = np.sum(model_probs[parent_ptr, :])
+                model_probs[parent_ptr, :] /= psum
+                child_ptr = token_tree_next_sibling[child_ptr]
+        token_tree_parent_ptr[b] = parent_ptr
+        token_tree_child_ptr[b] = child_ptr
+
+
+np.random.seed(0)
+vocab = 32000
+nbatch = 32
+height = 5
+num_nodes_per_batch = 2**height - 1
+num_nodes = num_nodes_per_batch * nbatch
+TX = 128
+VEC = 4
+
+### Inputs
+draft_probs = np.random.rand(num_nodes, vocab).astype("float32")
+draft_probs /= np.sum(draft_probs, axis=1, keepdims=True)
+draft_tokens = np.random.randint(0, vocab, num_nodes).astype("int32")
+model_probs = np.random.rand(num_nodes, vocab).astype("float32")
+model_probs /= np.sum(model_probs, axis=1, keepdims=True)
+# binary tree
+token_tree_first_child = list()
+token_tree_next_sibling = list()
+for b in range(nbatch):
+    for i in range(num_nodes_per_batch):
+        token_tree_first_child.append(
+            b * num_nodes_per_batch + i * 2 + 1 if i * 2 + 1 < num_nodes_per_batch else -1
+        )
+        token_tree_next_sibling.append(
+            b * num_nodes_per_batch + i * 2 + 2 if i * 2 + 2 < num_nodes_per_batch else -1
+        )
+token_tree_first_child = np.array(token_tree_first_child).astype("int32")
+token_tree_next_sibling = np.array(token_tree_next_sibling).astype("int32")
+uniform_samples = np.random.rand(num_nodes).astype("float32")
+token_tree_parent_ptr = list()
+token_tree_child_ptr = list()
+for b in range(nbatch):
+    token_tree_parent_ptr.append(b * num_nodes_per_batch)
+    token_tree_child_ptr.append(b * num_nodes_per_batch + 1)
+token_tree_parent_ptr = np.array(token_tree_parent_ptr).astype("int32")
+token_tree_child_ptr = np.array(token_tree_child_ptr).astype("int32")
+
+### TVM Inputs
+dev = tvm.cuda(0)
+draft_probs_tvm = tvm.nd.array(draft_probs, dev)
+draft_tokens_tvm = tvm.nd.array(draft_tokens, dev)
+model_probs_tvm = tvm.nd.array(model_probs, dev)
+token_tree_first_child_tvm = tvm.nd.array(token_tree_first_child, dev)
+token_tree_next_sibling_tvm = tvm.nd.array(token_tree_next_sibling, dev)
+uniform_samples_tvm = tvm.nd.array(uniform_samples, dev)
+token_tree_parent_ptr_tvm = tvm.nd.array(token_tree_parent_ptr, dev)
+token_tree_child_ptr_tvm = tvm.nd.array(token_tree_child_ptr, dev)
+
+# print("draft_probs", draft_probs)
+# print("draft_tokens", draft_tokens)
+# print("model_probs", model_probs)
+# print("token_tree_first_child", token_tree_first_child)
+# print("token_tree_next_sibling", token_tree_next_sibling)
+# print("uniform_samples", uniform_samples)
+# print("token_tree_parent_ptr", token_tree_parent_ptr)
+# print("token_tree_child_ptr", token_tree_child_ptr)
+
+### Numpy reference
+numpy_reference(
+    draft_probs,
+    draft_tokens,
+    model_probs,
+    token_tree_first_child,
+    token_tree_next_sibling,
+    uniform_samples,
+    token_tree_parent_ptr,
+    token_tree_child_ptr,
+)
+# print("model_probs", model_probs)
+# print("token_tree_parent_ptr", token_tree_parent_ptr)
+# print("token_tree_child_ptr", token_tree_child_ptr)
+
+### TVM
+kernel = batch_spec_verify(num_nodes=num_nodes, vocab=vocab, nbatch=nbatch)
+mod = tvm.build(kernel, target="cuda")
+mod(
+    draft_probs_tvm,
+    draft_tokens_tvm,
+    model_probs_tvm,
+    token_tree_first_child_tvm,
+    token_tree_next_sibling_tvm,
+    uniform_samples_tvm,
+    token_tree_parent_ptr_tvm,
+    token_tree_child_ptr_tvm,
+)
+# print("model_probs", model_probs_tvm.asnumpy())
+# print("token_tree_parent_ptr", token_tree_parent_ptr_tvm.asnumpy())
+# print("token_tree_child_ptr", token_tree_child_ptr_tvm.asnumpy())
+
+tvm.testing.assert_allclose(model_probs, model_probs_tvm.asnumpy())
+tvm.testing.assert_allclose(token_tree_parent_ptr, token_tree_parent_ptr_tvm.asnumpy())
+tvm.testing.assert_allclose(token_tree_child_ptr, token_tree_child_ptr_tvm.asnumpy())
+
+time_f = mod.time_evaluator(mod.entry_name, dev, number=10, repeat=3)
+res = time_f(
+    draft_probs_tvm,
+    draft_tokens_tvm,
+    model_probs_tvm,
+    token_tree_first_child_tvm,
+    token_tree_next_sibling_tvm,
+    uniform_samples_tvm,
+    token_tree_parent_ptr_tvm,
+    token_tree_child_ptr_tvm,
+)
+print(res)
