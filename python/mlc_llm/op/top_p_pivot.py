93c560b470a5e4c8105eddd34e0cc118d2b9d9e6
Bohan Hou <bohanhou@andrew.cmu.edu>
2024-04-26 20:18:05 -0400

    [Op] Top-p cutoff pivot (#2221)
    This commit introduces the GPU top-p cutoff operator for efficient
    probability renormalization under top-p.

---
+++ python/mlc_llm/op/top_p_pivot.py
@@ -0,0 +1,315 @@
+"""Operators for choosing the pivot to cut-off top-p percentile """
+
+import tvm
+from tvm.script import tir as T
+
+# mypy: disable-error-code="attr-defined,valid-type,name-defined"
+# pylint: disable=too-many-locals,invalid-name,too-many-arguments,unnecessary-lambda
+# pylint: disable=too-many-statements,line-too-long,too-many-nested-blocks,too-many-branches
+
+
+def top_p_pivot(pN):
+    """Top-p pivot function. This function finds the pivot to cut-off top-p percentile.
+
+    A valide pivot should satisfy the following conditions:
+    - lsum >= top_p
+    - top_p > lsum - cmin * lmin
+    where lsum is the sum of elements that are larger or equal to the pivot,
+    lmin is the minimum elements that is larger or equal to the pivot,
+    cmin is the count of elements that are equal to lmin,
+
+    Parameters
+    ----------
+    prob:
+        The probability vector
+
+    top_p_global:
+        The top-p threshold
+
+    init_pivots:
+        The initial pivot candidates
+
+    final_pivot:
+        The final pivot to cut-off top-p percentile
+    """
+    TX = 1024
+    K = 32
+    eps_LR = 1e-7
+
+    def _var(dtype="int32"):
+        return T.alloc_buffer((1,), dtype, scope="local")
+
+    def valid(lsum, lmin, cmin, top_p):
+        return tvm.tir.all(lsum >= top_p, top_p > lsum - cmin * lmin)
+
+    # fmt: off
+    @T.prim_func(private=True)
+    def _func(
+        var_prob: T.handle,
+        top_p_global: T.buffer([1], dtype="float32"),
+        var_init_pivots: T.handle,
+        var_final_pivot: T.handle,
+        var_final_lsum: T.handle,
+    ):
+        T.func_attr({"tir.is_scheduled": 1, "tir.noalias": True})
+        B = T.int32()
+        N = T.int32()
+        prob = T.match_buffer(var_prob, (B, N,), "float32")
+        init_pivots = T.match_buffer(var_init_pivots, (pN,), "float32")
+        final_pivot = T.match_buffer(var_final_pivot, (B,), "float32")
+        final_lsum = T.match_buffer(var_final_lsum, (B,), "float32")
+
+        with T.block("kernel"):
+            pivot = T.alloc_buffer((pN,), "float32", scope="local")
+            top_p = _var("float32")
+
+            L = T.alloc_buffer((1,), "float32", scope="shared")
+            R = T.alloc_buffer((1,), "float32", scope="shared")
+            L_local = _var("float32")
+            R_local = _var("float32")
+
+            q = _var("float32")
+            lsum = T.alloc_buffer((pN,), "float32", scope="local")
+            lmin_broadcast = T.alloc_buffer((1), "float32", scope="shared")
+            lmin_broadcast_local = _var("float32")
+            lmin = T.alloc_buffer((pN,), "float32", scope="local")
+            cmin = T.alloc_buffer((pN,), "int32", scope="local")
+            total_sum = _var("float32")
+
+            it = _var("int32")
+            es_local = _var("bool")
+            es = T.alloc_buffer((1,), "bool", scope="shared")
+            find_pivot_local = _var("bool")
+            find_pivot = T.alloc_buffer((1,), "bool", scope="shared")
+
+            total_sum_reduce = _var("float32")
+            lsum_reduce = _var("float32")
+            lmin_reduce = _var("float32")
+            cmin_reduce = _var("int32")
+
+            for _bx in T.thread_binding(0, B, thread="blockIdx.x"):
+                for _tx in T.thread_binding(0, TX, thread="threadIdx.x"):
+                    with T.block("CTA"):
+                        b, tx = T.axis.remap("SS", [_bx, _tx])
+
+                        top_p[0] = top_p_global[0]
+
+                        if tx == 0:
+                            # leader thread initializes L, R
+                            L[0] = 1.0 - top_p[0]
+                            R[0] = eps_LR
+                            find_pivot[0] = False
+                        T.tvm_storage_sync("shared")
+
+                        L_local[0] = L[0]
+                        R_local[0] = R[0]
+                        for i in T.unroll(0, pN):
+                            # pivots are in descending order
+                            pivot[i] = init_pivots[i]
+                        find_pivot_local[0] = False
+
+                        while T.tvm_thread_invariant(
+                            L_local[0] - R_local[0] > eps_LR
+                            and T.Not(find_pivot_local[0])
+                        ):
+                            # sync before each iteration
+                            T.tvm_storage_sync("shared")
+
+                            ### get lsum, lmin, total_sum
+                            for pidx in T.unroll(0, pN):
+                                lsum[pidx] = 0.0
+                                lmin[pidx] = 1.0
+                                cmin[pidx] = 0
+                            total_sum[0] = 0.0
+                            it[0] = 0
+                            es_local[0] = False
+                            while it[0] < T.ceildiv(N, TX) and T.Not(es_local[0]):
+                                idx = T.meta_var(it[0] * TX + tx)
+                                q[0] = T.if_then_else(idx < N, prob[b, idx], 0.0)
+                                total_sum[0] += q[0]
+                                for pidx in T.unroll(0, pN):
+                                    if q[0] >= pivot[pidx]:
+                                        lsum[pidx] += q[0]
+                                        if lmin[pidx] > q[0]:
+                                            lmin[pidx] = q[0]
+                                            cmin[pidx] = 1
+                                        elif lmin[pidx] == q[0]:
+                                            cmin[pidx] += 1
+                                it[0] += 1
+
+                                # early stop every K iterations
+                                if it[0] % K == 0:
+                                    # reduce total_sum over tx
+                                    # T.tvm_storage_sync("shared")
+                                    with T.block("block_cross_thread"):
+                                        T.reads(total_sum[0])
+                                        T.writes(total_sum_reduce[0])
+                                        T.attr(
+                                            T.comm_reducer(lambda x0, y0: x0 + y0, [T.float32(0)]),
+                                            "reduce_scope",
+                                            T.reinterpret("handle", T.uint64(0)),
+                                        )
+                                        T.tvm_thread_allreduce(T.uint32(1), total_sum[0], True, total_sum_reduce[0], tx, dtype="handle")
+                                    # T.tvm_storage_sync("shared")
+
+                                    if tx == 0:
+                                        # leader thread checks if we can stop early
+                                        es[0] = 1 - total_sum_reduce[0] < pivot[pN - 1]
+                                    T.tvm_storage_sync("shared")
+                                    es_local[0] = es[0]
+
+                            T.tvm_storage_sync("shared")
+
+                            # reduce lsum, lmin, cmin, over tx
+                            for pidx in T.serial(0, pN):
+                                # reduce lsum over tx for pivot[j]
+                                with T.block("block_cross_thread"):
+                                    T.reads(lsum[pidx])
+                                    T.writes(lsum_reduce[0])
+                                    T.attr(
+                                        T.comm_reducer(lambda x0, y0: x0 + y0, [T.float32(0)]),
+                                        "reduce_scope",
+                                        T.reinterpret("handle", T.uint64(0)),
+                                    )
+                                    T.tvm_thread_allreduce(T.uint32(1), lsum[pidx], True, lsum_reduce[0], tx, dtype="handle")
+
+                                # reduce lmin over tx for pivot[j]
+                                with T.block("block_cross_thread"):
+                                    T.reads(lmin[pidx])
+                                    T.writes(lmin_reduce[0])
+                                    T.attr(
+                                        T.comm_reducer(lambda x0, y0: T.min(x0, y0), [T.float32(0)]),
+                                        "reduce_scope",
+                                        T.reinterpret("handle", T.uint64(0)),
+                                    )
+                                    T.tvm_thread_allreduce(T.uint32(1), lmin[pidx], True, lmin_reduce[0], tx, dtype="handle")
+
+                                if tx == 0:
+                                    # broadcast lmin to all threads
+                                    lmin_broadcast[0] = lmin_reduce[0]
+                                T.tvm_storage_sync("shared")
+                                lmin_broadcast_local[0] = lmin_broadcast[0]
+                                if lmin[pidx] > lmin_broadcast_local[0]:
+                                    cmin[pidx] = 0
+                                if tx == 0:
+                                    # only the leader thread updates lsum, lmin
+                                    lsum[pidx] = lsum_reduce[0]
+                                    lmin[pidx] = lmin_reduce[0]
+
+                                # reduce cmin over tx for pivot[j]
+                                with T.block("block_cross_thread"):
+                                    T.reads(cmin[pidx])
+                                    T.writes(cmin_reduce[0])
+                                    T.attr(
+                                        T.comm_reducer(lambda x0, y0: x0 + y0, [T.int32(0)]),
+                                        "reduce_scope",
+                                        T.reinterpret("handle", T.uint64(0)),
+                                    )
+                                    T.tvm_thread_allreduce(T.uint32(1), cmin[pidx], True, cmin_reduce[0], tx, dtype="handle")
+
+                                if tx == 0:
+                                    # only the leader thread updates cmin
+                                    cmin[pidx] = cmin_reduce[0]
+
+                            T.tvm_storage_sync("shared")
+
+                            if tx == 0:
+                                # leader thread checks if we have found the pivot, or updates L, R
+                                it[0] = 0
+                                while it[0] < pN and T.Not(find_pivot_local[0]):
+                                    pidx = T.meta_var(it[0])
+                                    if valid(lsum[pidx], lmin[pidx], cmin[pidx], top_p[0]):
+                                        find_pivot[0] = True
+                                        find_pivot_local[0] = True
+                                        # write back the pivot and lsum
+                                        final_pivot[b] = pivot[pidx]
+                                        final_lsum[b] = lsum[pidx]
+                                    elif lsum[pidx] - lmin[pidx] * cmin[pidx] >= top_p[0]:
+                                        R[0] = pivot[pidx]
+                                    elif lsum[pidx] < top_p[0]:
+                                        L[0] = pivot[pidx]
+                                    it[0] += 1
+
+                            T.tvm_storage_sync("shared")
+
+                            L_local[0] = L[0]
+                            R_local[0] = R[0]
+                            find_pivot_local[0] = find_pivot[0]
+                            # new pivots for next iteration
+                            # uniform spacing between L and R
+                            for pidx in T.unroll(0, pN):
+                                pivot[pidx] = L[0] - (pidx + 1) * (L_local[0] - R_local[0]) / (pN + 1)
+
+                        if tx == 0:
+                            # leader thread writes back the pivot
+                            if T.Not(find_pivot_local[0]):
+                                final_pivot[b] = -1e5
+    # fmt: on
+
+    return _func
+
+
+def top_p_renorm():
+    """Top-p renormalization function. This function renormalizes the probability vector.
+
+    Given the pivot, the probability vector is renormalized as follows:
+    - if prob >= pivot, renorm_prob = prob / lsum
+    - otherwise, renorm_prob = 0
+
+    Parameters
+    ----------
+    prob:
+        The probability vector
+
+    final_pivot:
+        The final pivot to cut-off top-p percentile
+
+    final_lsum:
+        The sum of elements that are larger or equal to the pivot
+
+    renorm_prob:
+        The renormalized probability vector
+    """
+    TX = 1024
+    CTA_COUNT = 512
+
+    def _var(dtype="int32"):
+        return T.alloc_buffer((1,), dtype, scope="local")
+
+    # fmt: off
+    @T.prim_func(private=True)
+    def _func(
+        var_prob: T.handle,
+        var_final_pivot: T.handle,
+        var_final_lsum: T.handle,
+        var_renorm_prob: T.handle,
+    ):
+        T.func_attr({"tir.is_scheduled": 1, "tir.noalias": True})
+        B = T.int32()
+        N = T.int32()
+        prob = T.match_buffer(var_prob, (B, N,), "float32")
+        final_pivot = T.match_buffer(var_final_pivot, (B,), "float32")
+        final_lsum = T.match_buffer(var_final_lsum, (B,), "float32")
+        renorm_prob = T.match_buffer(var_renorm_prob, (B, N,), "float32")
+
+        with T.block("kernel"):
+            pivot = _var("float32")
+            lsum = _var("float32")
+            BX = T.meta_var(T.ceildiv(CTA_COUNT, B))
+
+            for _by in T.thread_binding(0, B, thread="blockIdx.y"):
+                for _bx in T.thread_binding(0, BX, thread="blockIdx.x"):
+                    for _tx in T.thread_binding(0, TX, thread="threadIdx.x"):
+                        with T.block("CTA"):
+                            by, bx, tx = T.axis.remap("SSS", [_by, _bx, _tx])
+
+                            pivot[0] = final_pivot[by]
+                            lsum[0] = final_lsum[by]
+
+                            for i in T.serial(T.ceildiv(N, BX * TX)):
+                                idx = T.meta_var(i * BX * TX + bx * TX + tx)
+                                if idx < N:
+                                    renorm_prob[by, idx] = T.if_then_else(prob[by, idx] >= pivot[0], prob[by, idx] / lsum[0], 0.0)
+    # fmt: on
+
+    return _func
---
+++ tests/python/op/test_top_p_pivot.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pytest
+import tvm
+import tvm.testing
+
+from mlc_llm.op.top_p_pivot import top_p_pivot, top_p_renorm
+
+# mypy: disable-error-code="var-annotated"
+
+
+@pytest.mark.parametrize("batch_size", [32, 64])
+@pytest.mark.parametrize("vocab", [3, 32, 64, 128])
+def test_top_p_renorm(batch_size, vocab):
+    top_p = 0.95
+    init_pivots_np = np.array([1 - top_p, 0.02, 0.01]).astype(np.float32)
+    top_p_np = np.array([top_p]).astype(np.float32)
+
+    p_np = np.random.exponential(3, size=(batch_size, vocab)).astype(np.float32)
+    p_np /= np.sum(p_np, axis=-1, keepdims=True)
+    final_pivot_np = np.zeros(batch_size).astype(np.float32)
+    final_lsum_np = np.zeros(batch_size).astype(np.float32)
+
+    dev = tvm.cuda(0)
+    var_prob = tvm.nd.array(p_np, dev)
+    var_init_pivots = tvm.nd.array(init_pivots_np, dev)
+    top_p_global = tvm.nd.array(top_p_np, dev)
+    var_final_pivot = tvm.nd.array(final_pivot_np, dev)
+    var_final_lsum = tvm.nd.array(final_lsum_np, dev)
+
+    kernel = top_p_pivot(init_pivots_np.shape[0])
+    mod = tvm.build(kernel, target="cuda")
+    mod(var_prob, top_p_global, var_init_pivots, var_final_pivot, var_final_lsum)
+
+    final_pivot = var_final_pivot.asnumpy()
+    final_lsum = var_final_lsum.asnumpy()
+
+    renorm_np = p_np.copy()
+    var_renorm = tvm.nd.array(renorm_np, dev)
+
+    kernel_renorm = top_p_renorm()
+    mod_renorm = tvm.build(kernel_renorm, target="cuda")
+    mod_renorm(var_prob, var_final_pivot, var_final_lsum, var_renorm)
+
+    renorm = var_renorm.asnumpy()
+
+    def verify_pivot(probs: np.ndarray, pivot: float, lsum: float, renorm: np.ndarray):
+        sorted_probs = np.sort(probs, axis=-1)[::-1]
+        num_larger_than_pivot = np.sum(sorted_probs >= pivot)
+        filtered_sorted_probs = sorted_probs[:num_larger_than_pivot]
+        min_larger_than_pivot = min(filtered_sorted_probs)
+
+        sum_larger_than_pivot = np.sum(np.where(sorted_probs >= pivot, sorted_probs, 0))
+        sum_larger_than_pivot_exclude_min = np.sum(
+            np.where(filtered_sorted_probs != min_larger_than_pivot, filtered_sorted_probs, 0)
+        )
+
+        probs[probs < pivot] = 0
+        renorm_prob = probs / np.sum(probs, axis=-1, keepdims=True)
+        try:
+            assert sum_larger_than_pivot >= top_p
+            assert sum_larger_than_pivot_exclude_min < top_p
+            assert abs(lsum - sum_larger_than_pivot) < 1e-6
+            assert np.allclose(renorm, renorm_prob, atol=1e-6, rtol=1e-6)
+        except AssertionError:
+            print("Failed")
+            print("probs:", repr(probs))
+            print("pivot:", pivot)
+            print("sorted_probs:", sorted_probs)
+            print("num_larger_than_pivot:", num_larger_than_pivot)
+            print("filtered_sorted_probs:", filtered_sorted_probs)
+            print("min_larger_than_pivot:", min_larger_than_pivot)
+            print("sum_larger_than_pivot:", sum_larger_than_pivot)
+            print("sum_larger_than_pivot_exclude_min:", sum_larger_than_pivot_exclude_min)
+            print("renom_prob:", renorm_prob)
+            print("renorm:", renorm)
+            raise
+
+    for i in range(batch_size):
+        verify_pivot(p_np[i], final_pivot[i], final_lsum[i], renorm[i])
+
+
+if __name__ == "__main__":
+    tvm.testing.main()
