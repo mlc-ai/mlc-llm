c6b550e6ba24073513fa73527c0c41d1fb02431d
tqchen <tianqi.tchen@gmail.com>
2024-04-27 17:38:41 -0400

    [REFACTOR] Migrate JSONFFIEngine to formal namespace
    This PR migrates JSONFFIEngine to a formal namespace.
    Also list TODOs to further simplify the JSONFFIEngine.

---
+++ python/mlc_llm/json_ffi/__init__.py
@@ -0,0 +1,8 @@
+"""JSON FFI is a pure string based interface of MLC LLM Engine.
+
+We build interfacing with JSON FFI for both testing purposes
+and internal use. For most python API usage, please use MLCEngine
+and MLCAsyncEngine
+"""
+
+from .engine import JSONFFIEngine
