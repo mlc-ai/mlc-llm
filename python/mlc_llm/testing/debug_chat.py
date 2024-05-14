[DebugChat] Fix DebugChat softmax function and save logits to debug folder

python/mlc_llm/testing/debug_chat.py
@@ -351,7 +351,9 @@ class DebugChat:  # pylint: disable=too-many-instance-attributes, too-few-public
         if presence_penalty != 0.0 or frequency_penalty != 0.0:
             self._apply_presence_and_freq_penalty(logits_np, presence_penalty, frequency_penalty)
 
-        self._softmax_with_temperature(logits_np, temperature)
+        logits_np = self._softmax_with_temperature(logits_np, temperature)
+        np.savez(self.instrument.debug_out / f"logits.npz", logits_np)
+        
         logits = logits.copyfrom(logits_np)
         next_token = self.sample_topp_from_prob_func(logits, top_p, random.random())
         return next_token