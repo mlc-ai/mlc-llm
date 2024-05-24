mlc-ai/mlc-llm

commit 988e9f0523774793723e2a13f8d5b582f954fa9a

add hasattr safecheck for MLCEngineBase (#2400)
Co-authored-by: Huaishun Hu <huaishun.hu@mthreads.com>

python/mlc_llm/serve/engine_base.py
@@ -526,12 +526,14 @@ class MLCEngineBase:  # pylint: disable=too-many-instance-attributes,too-few-pub
 
     def terminate(self):
         """Terminate the engine."""
-        if self._terminated:
+        if hasattr(self, '_terminated') and self._terminated:
             return
         self._terminated = True
         self._ffi["exit_background_loop"]()
-        self._background_loop_thread.join()
-        self._background_stream_back_loop_thread.join()
+        if hasattr(self, '_background_loop_thread'):
+            self._background_loop_thread.join()
+        if hasattr(self, '_background_stream_back_loop_thread'):
+            self._background_stream_back_loop_thread.join()
 
     def _debug_call_func_on_all_worker(self, func_name: str) -> None:
         """Call the given global function on all workers. Only for debug purpose."""

commit e204cc2485e735b243567036f6ad3a7c8a709cf7

[Fix] Fix format issue of MLCEngineBase
This PR fixes a format issue caused by #2400.

python/mlc_llm/serve/engine_base.py
@@ -543,13 +543,13 @@ class MLCEngineBase:  # pylint: disable=too-many-instance-attributes,too-few-pub
 
     def terminate(self):
         """Terminate the engine."""
-        if hasattr(self, '_terminated') and self._terminated:
+        if hasattr(self, "_terminated") and self._terminated:
             return
         self._terminated = True
         self._ffi["exit_background_loop"]()
-        if hasattr(self, '_background_loop_thread'):
+        if hasattr(self, "_background_loop_thread"):
             self._background_loop_thread.join()
-        if hasattr(self, '_background_stream_back_loop_thread'):
+        if hasattr(self, "_background_stream_back_loop_thread"):
             self._background_stream_back_loop_thread.join()
 
     def _debug_call_func_on_all_worker(self, func_name: str) -> None: