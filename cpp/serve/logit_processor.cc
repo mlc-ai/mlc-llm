a63d69c8a270e2a62b33db074bd24a9468e6d3cc
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-05-29 19:43:08 -0400

    [Serving] Use preferred host memory for host NDArrays
    This PR updates the host memory in model, logit processor and GPUSampler
    with the support of preferred host device, so that for CUDA and ROCm
    the pinned memory will be used for the host arrays, which may be faster
    than the default CPU memory during copying.

--- cpp/serve/logit_processor.cc
+++ cpp/serve/logit_processor.cc
@@ -49,16 +49,21 @@ class LogitProcessorImpl : public LogitProcessorObj {
         apply_penalty_func_(ft->apply_penalty_func_),
         apply_bitmask_func_(ft->apply_bitmask_func_),
         trace_recorder_(std::move(trace_recorder)) {
-    DLDevice device_cpu{DLDeviceType::kDLCPU, /*device_id=*/0};
+    Device preferred_host_device = GetPreferredHostDevice(device);
     // Initialize auxiliary arrays on CPU.
-    seq_ids_host_ = NDArray::Empty({max_num_token}, dtype_i32_, device_cpu);
-    pos2seq_id_host_ = NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, device_cpu);
-    token_ids_host_ = NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, device_cpu);
-    token_cnt_host_ = NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, device_cpu);
-    token_logit_bias_host_ = NDArray::Empty({max_num_token * vocab_size}, dtype_f32_, device_cpu);
-    penalties_host_ = NDArray::Empty({max_num_token, 3}, dtype_f32_, device_cpu);
-    bitmask_host_ = NDArray::Empty({max_num_token, bitmask_size_}, dtype_u32_, device_cpu);
-    temperature_host_ = NDArray::Empty({max_num_token}, dtype_f32_, device_cpu);
+    seq_ids_host_ = NDArray::Empty({max_num_token}, dtype_i32_, preferred_host_device);
+    pos2seq_id_host_ =
+        NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, preferred_host_device);
+    token_ids_host_ =
+        NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, preferred_host_device);
+    token_cnt_host_ =
+        NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, preferred_host_device);
+    token_logit_bias_host_ =
+        NDArray::Empty({max_num_token * vocab_size}, dtype_f32_, preferred_host_device);
+    penalties_host_ = NDArray::Empty({max_num_token, 3}, dtype_f32_, preferred_host_device);
+    bitmask_host_ =
+        NDArray::Empty({max_num_token, bitmask_size_}, dtype_u32_, preferred_host_device);
+    temperature_host_ = NDArray::Empty({max_num_token}, dtype_f32_, preferred_host_device);
     // Initialize auxiliary arrays on GPU.
     seq_ids_device_ = NDArray::Empty({max_num_token}, dtype_i32_, device);
     pos2seq_id_device_ = NDArray::Empty({max_num_token * vocab_size}, dtype_i32_, device);
