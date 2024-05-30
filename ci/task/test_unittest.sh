12f7c480d53e86e10d6fd3dbe580759ce3aa4956
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-05-29 20:49:19 -0400

    Update test_unittest.sh

--- ci/task/test_unittest.sh
+++ ci/task/test_unittest.sh
@@ -3,6 +3,7 @@ set -eo pipefail
 set -x
 
 # Install dependency
+python -m pip install --force-reinstall wheels/*.whl
 python -m pip install pytest
 python -m pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cu121
 export LD_LIBRARY_PATH=/usr/local/cuda/compat/:$LD_LIBRARY_PATH
