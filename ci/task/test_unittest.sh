ce976530369e476bd5b1c1030965c5befaf9f3c7
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-05-29 20:31:53 -0400

    Update test_unittest.sh

--- ci/task/test_unittest.sh
+++ ci/task/test_unittest.sh
@@ -6,5 +6,5 @@ set -x
 # add pytestmarker = [pytest.mark.unittest] in the test file
 # so they will be run here
 python -m pip install pytest
-python -m pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly
+python -m pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cu121
 python -m pytest -v tests/python/ -m unittest
