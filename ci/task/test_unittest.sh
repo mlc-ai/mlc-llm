103684d03090f870fb3e72406662e55547f2b15d
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-05-29 20:38:09 -0400

    Update test_unittest.sh

--- ci/task/test_unittest.sh
+++ ci/task/test_unittest.sh
@@ -2,9 +2,12 @@
 set -eo pipefail
 set -x
 
+# Install dependency
+python -m pip install pytest
+python -m pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cu121
+export LD_LIBRARY_PATH=/usr/local/cuda/compat/:$LD_LIBRARY_PATH
+
 # run all tests that are categorized as "unittest"
 # add pytestmarker = [pytest.mark.unittest] in the test file
 # so they will be run here
-python -m pip install pytest
-python -m pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cu121
 python -m pytest -v tests/python/ -m unittest
