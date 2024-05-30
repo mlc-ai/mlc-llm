85511cff2b5e758e5dda16d97524d58559769af8
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-05-29 20:20:09 -0400

    [CI][HotFix] Install pytest
    This commit installs pytest in the unittest script, since pytest is not included in the CI docker.

--- ci/task/test_unittest.sh
+++ ci/task/test_unittest.sh
@@ -5,4 +5,5 @@ set -x
 # run all tests that are categorized as "unittest"
 # add pytestmarker = [pytest.mark.unittest] in the test file
 # so they will be run here
+python -m pip install pytest
 python -m pytest -v tests/python/ -m unittest
