3e56db92c6ad3ffd64ea6daec081354d68c994ec
Yong Wu <yongcale@gmail.com>
2024-06-03 03:42:51 +0000

    Add metric filer

--- python/mlc_llm/bench/metrics.py
+++ python/mlc_llm/bench/metrics.py
@@ -1,6 +1,6 @@
 """ MLC LLM bench Metrics"""
 import json
-from typing import Dict, List, Union
+from typing import Callable, Dict, List, Optional, Union
 
 from pydantic import BaseModel
 
@@ -23,96 +23,179 @@ class Metrics(BaseModel):
     completion_tokens: int
 
 
-def _get_metrics(metrics: List[RequestRecords]) -> List[Metrics]:
-    """
-    Augments raw metrics.
+class MetricsProcessor:
+    """The metrics processor class
 
     Parameters
     ----------
-    metrics : List[Metrics]
-        The list of raw metrics data collected.
+    tokenizer : Optional[Tokenizer]
+        The tokenizer.
 
-    Returns
-    -------
-    metrics : List[Metrics]
-        The list of augmented metrics with additional items.
+    request_records : List[RequestRecords]
+        The list of request records.
     """
-    from transformers import (  # pylint: disable=import-outside-toplevel,import-error
-        LlamaTokenizerFast,
-    )
 
-    tokenizer = LlamaTokenizerFast.from_pretrained("hf-internal-testing/llama-tokenizer")
+    def __init__(self, request_records: List[RequestRecords], tokenizer=None) -> None:
+        self.tokenizer = tokenizer
+        if self.tokenizer is None:
+            from transformers import (  # pylint: disable=import-outside-toplevel,import-error
+                LlamaTokenizerFast,
+            )
 
-    def _count_tokens(text: str, tokenizer) -> int:
-        return len(tokenizer.encode(text))
+            self.tokenizer = LlamaTokenizerFast.from_pretrained(
+                "hf-internal-testing/llama-tokenizer"
+            )
+            logger.warning("No tokenizer provided. Using default tokenizer.")
+        self.all_metrics: List[Metrics] = self.extract_metrics_from_request_records(request_records)
 
-    result = []
-    for metric in metrics:
-        prompt_tokens = _count_tokens(metric.input, tokenizer)
-        completion_tokens = _count_tokens(metric.output, tokenizer)
-        assert prompt_tokens > 0 and completion_tokens >= 0, "Invalid number of tokens"
-        end_to_end_latency = metric.end_to_end_latency
-        # TODO(yongwww): handle the non-streaming case where ttft is 0
-        refined_metric = Metrics(
-            inter_token_latency=end_to_end_latency / completion_tokens,
-            decode_token_latency=(end_to_end_latency - metric.ttft) / completion_tokens,
-            ttft=metric.ttft,
-            end_to_end_latency=end_to_end_latency,
-            prompt_tokens=prompt_tokens,
-            completion_tokens=completion_tokens,
+    def count_tokens(self, prompt: str) -> int:
+        """Count the number of tokens in the text
+
+        Parameters
+        ----------
+        prompt : str
+            The text to count the tokens.
+
+        Returns
+        -------
+        prompt_tokens : int
+            The number of tokens in the prompt.
+        """
+        return len(self.tokenizer.encode(prompt))
+
+    def extract_metrics_from_request_records(
+        self, request_records: List[RequestRecords]
+    ) -> List[Metrics]:
+        """
+        Extract the metrics from request records.
+
+        Parameters
+        ----------
+        request_records : List[RequestRecords]
+            The list of raw request records collected.
+
+        Returns
+        -------
+        metrics : List[Metrics]
+            The list of extracted metrics with additional items.
+        """
+
+        result = []
+        for metric in request_records:
+            prompt_tokens = self.count_tokens(metric.input)
+            completion_tokens = self.count_tokens(metric.output)
+            assert prompt_tokens > 0 and completion_tokens >= 0, "Invalid prompt tokens"
+            end_to_end_latency = metric.end_to_end_latency
+            refined_metric = Metrics(
+                inter_token_latency=end_to_end_latency / completion_tokens,
+                decode_token_latency=(end_to_end_latency - metric.ttft) / completion_tokens,
+                ttft=metric.ttft,
+                end_to_end_latency=end_to_end_latency,
+                prompt_tokens=prompt_tokens,
+                completion_tokens=completion_tokens,
+            )
+            result.append(refined_metric)
+        return result
+
+    def get_metrics(self) -> List[Metrics]:
+        """
+        Get the metrics collected.
+
+        Returns
+        -------
+        all_metrics : List[Metrics]
+            The list of metrics collected.
+        """
+        return self.all_metrics
+
+    def reset_metrics(self, metrics: List[Metrics]) -> None:
+        """Reset the metrics collected.
+
+        Parameters
+        ----------
+        metrics : List[Metrics]
+            The list of metrics to reset.
+        """
+        self.all_metrics = metrics
+
+    def filter_metrics(self, criteria: Optional[Callable[[Metrics], bool]] = None) -> List[Metrics]:
+        """
+        Filters the metrics based on the provided criteria. If no criteria are provided,
+        it filters out metrics with any fields set to None or 0.
+
+        Parameters
+        ----------
+        criteria : Optional[Callable[[Metrics], bool]]
+            A function that takes a metric as input,
+            returns True if the metric should be included.
+
+        Returns
+        -------
+        filtered_metrics : List[Metrics]
+            The list of metrics that meet the specified criteria.
+        """
+        if criteria is None:
+            # Default criteria to filter out metrics with None or 0 in certain fields
+            def criteria(metric: Metrics) -> bool:
+                for field, _ in Metrics.model_fields.items():
+                    val = getattr(metric, field)
+                    if val is None or val == 0:
+                        return False
+                return True
+
+        filered_metrics = [metric for metric in self.all_metrics if criteria(metric)]
+        self.reset_metrics(filered_metrics)
+        return filered_metrics
+
+    def generate_metrics_summary(
+        self, start_time: float, end_time: float
+    ) -> Dict[str, Union[int, float]]:
+        """
+        Computes summary statistics across all metrics collected.
+
+        Parameters
+        ----------
+        all_metrics : List[RequestRecords]
+            All the metrics data collected in the monitoring period.
+
+        start_time : float
+            The start time of the monitoring period.
+
+        end_time : float
+            The end time of the monitoring period.
+
+        Returns
+        -------
+        report : Dict
+            A dictionary containing the summary statistics of the collected metrics.
+        """
+        import pandas as pd  # pylint: disable=import-outside-toplevel,import-error
+
+        if not self.all_metrics:
+            return {}
+
+        metrics = self.all_metrics
+        df = pd.DataFrame([metric.model_dump() for metric in metrics])
+
+        report: Dict = {}
+        for key, _ in Metrics.model_fields.items():
+            if key in df.columns:
+                series = df[key].dropna()
+                report[key] = {
+                    "quantiles": {
+                        f"p{int(q * 100)}": v
+                        for q, v in series.quantile([0.25, 0.5, 0.75, 0.9, 0.95, 0.99]).items()
+                    },
+                    "mean": series.mean(),
+                    "min": series.min(),
+                    "max": series.max(),
+                    "stddev": series.std(),
+                }
+
+        report["num_completed_requests"] = len(metrics)
+        report["overall_output_throughput"] = df["completion_tokens"].sum() / (
+            end_time - start_time
         )
-        result.append(refined_metric)
-    return result
 
-
-def get_metrics_summary(
-    all_metrics: List[Union[RequestRecords, Metrics]], start_time: float, end_time: float
-) -> Dict[str, Union[int, float]]:
-    """
-    Computes summary statistics across all metrics collected.
-
-    Parameters
-    ----------
-    all_metrics : List[RequestRecords]
-        All the metrics data collected in the monitoring period.
-
-    start_time : float
-        The start time of the monitoring period.
-
-    end_time : float
-        The end time of the monitoring period.
-
-    Returns
-    -------
-    report : Dict
-        A dictionary containing the summary statistics of the collected metrics.
-    """
-    import pandas as pd  # pylint: disable=import-outside-toplevel,import-error
-
-    if not all_metrics:
-        logger.warning("No metrics collected.")
-        return {}
-
-    metrics = all_metrics if isinstance(all_metrics[0], Metrics) else _get_metrics(all_metrics)
-    df = pd.DataFrame([metric.model_dump() for metric in metrics])
-
-    report: Dict = {}
-    for key, _ in Metrics.model_fields.items():
-        if key in df.columns:
-            series = df[key].dropna()
-            report[key] = {
-                "quantiles": {
-                    f"p{int(q * 100)}": v
-                    for q, v in series.quantile([0.25, 0.5, 0.75, 0.9, 0.95, 0.99]).items()
-                },
-                "mean": series.mean(),
-                "min": series.min(),
-                "max": series.max(),
-                "stddev": series.std(),
-            }
-
-    report["num_completed_requests"] = len(metrics)
-    report["overall_output_throughput"] = df["completion_tokens"].sum() / (end_time - start_time)
-
-    logger.info("Metrics Summary:\n%s", json.dumps(report, indent=4, default=str))
-    return report
+        logger.info("Metrics Summary:\n%s", json.dumps(report, indent=4, default=str))
+        return report
