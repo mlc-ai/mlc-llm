ae73db70f321c3cf36e48b468cbd37a43c9c9857
Gunjan Dhanuka <gdhanuka192@gmail.com>
2024-06-26 01:44:50 -0400

    add awq support

--- python/mlc_llm/model/cohere/cohere_quantization.py
+++ python/mlc_llm/model/cohere/cohere_quantization.py
@@ -6,7 +6,7 @@ from typing import Tuple
 from tvm.relax.frontend import nn
 
 from mlc_llm.loader import QuantizeMapping
-from mlc_llm.quantization import FTQuantize, GroupQuantize, NoQuantize
+from mlc_llm.quantization import AWQQuantize, FTQuantize, GroupQuantize, NoQuantize
 
 from .cohere_model import CohereConfig, CohereForCausalLM
 
@@ -33,6 +33,22 @@ def ft_quant(
     quantization: FTQuantize,
 ) -> Tuple[nn.Module, QuantizeMapping]:
     """Quantize a Cohere-architecture model using FasterTransformer quantization."""
+    model: nn.Module = CohereForCausalLM(model_config)
+    model.to(quantization.model_dtype)
+    quant_map = QuantizeMapping({}, {})
+    model = quantization.quantize_model(
+        model,
+        quant_map,
+        "",
+    )
+    return model, quant_map
+
+
+def awq_quant(
+    model_config: CohereConfig,
+    quantization: AWQQuantize,
+) -> Tuple[nn.Module, QuantizeMapping]:
+    """Quantize a cohere-Aya model using Activation-aware Weight Quantization(AWQ)."""
     model: nn.Module = CohereForCausalLM(model_config)
     model.to(quantization.model_dtype)
     quant_map = QuantizeMapping({}, {})
