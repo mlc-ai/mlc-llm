63c542b020e1429ac292621bb6001e5617b105aa
Gunjan Dhanuka <gdhanuka192@gmail.com>
2024-07-17 08:28:20 -0400

    modified conversation template for aya (buggy)

--- python/mlc_llm/conversation_template/cohere.py
+++ python/mlc_llm/conversation_template/cohere.py
@@ -1,4 +1,5 @@
 """Cohere default templates"""
+
 # Referred from: https://huggingface.co/CohereForAI/aya-23-8B/blob/main/tokenizer_config.json
 
 from mlc_llm.protocol.conversation_protocol import Conversation, MessagePlaceholders
@@ -9,15 +10,18 @@ from .registry import ConvTemplateRegistry
 ConvTemplateRegistry.register_conv_template(
     Conversation(
         name="aya-23",
-        system_template=f"<|system|>\n{MessagePlaceholders.SYSTEM.value}",
-        system_message="You are a helpful digital assistant. Please provide safe, "
-        "ethical and accurate information to the user.",
-        roles={"user": "<|USER_TOKEN|>", "assistant": "<|CHATBOT_TOKEN|>"},
-        seps=["<|SEP|>\n"],
+        system_template=f"<BOS_TOKEN><|START_OF_TURN_TOKEN|><|SYSTEM_TOKEN|>{MessagePlaceholders.SYSTEM.value}<|END_OF_TURN_TOKEN|>",
+        system_message="You are Command-R, a brilliant, sophisticated, AI-assistant trained to assist human users by providing thorough responses.",
+        roles={
+            "user": "<|START_OF_TURN_TOKEN|><|USER_TOKEN|>",
+            "assistant": "<|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>",
+        },
+        seps=["<|END_OF_TURN_TOKEN|>"],
         role_content_sep="\n",
         role_empty_sep="\n",
-        system_prefix_token_ids=[255008],
+        system_prefix_token_ids=[5],
         stop_str=["<|END_OF_TURN_TOKEN|>"],
         stop_token_ids=[255001],
+        add_role_after_system_message=False,
     )
 )
