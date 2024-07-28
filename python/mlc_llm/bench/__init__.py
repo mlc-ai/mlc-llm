47816537c0beae21dd8eca2f112941530359445b
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-07-28 00:40:15 -0400

    [Bench] Revamp benchmark submodule
    This PR revamp the benchmark submodule with a `__main__` entry
    that enables running the benchmark.

--- python/mlc_llm/bench/__init__.py
+++ python/mlc_llm/bench/__init__.py
@@ -1,6 +1,1 @@
 """Subdirectory of bench."""
-
-from .metrics import MetricsProcessor
-from .prompts import PromptsGenerator
-from .replay import load_replay_log, replay
-from .request import OpenAIRequestSender
