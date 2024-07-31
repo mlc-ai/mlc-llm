4fc38f7f54aa27a4cbc16259e56152015c49ba06
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-07-31 16:39:59 -0400

    Update Gemma2 impl
    This commit updates the Gemma2 implementation, including the following
    aspects:
    
    1. We try to reuse as much code as possible from the Gemma model for
    the overall code structure clarity and management.
    2. We properly set the scaling factor for attention.
    3. We add the final logit soft-capping for Gemma2.

--- python/mlc_llm/model/gemma2/gemma2_model.py
+++ python/mlc_llm/model/gemma2/gemma2_model.py
@@ -1,183 +1,67 @@
 """Implementation for Gemma2 architecture."""
 
 import dataclasses
-from typing import Any, Dict, Optional
 
-from tvm import te, tir
 from tvm.relax.frontend import nn
 from tvm.relax.frontend.nn import Tensor, op
 
-from mlc_llm import op as op_ext
-from mlc_llm.nn import PagedKVCache, RopeMode
+from mlc_llm.model.gemma.gemma_model import (
+    GemmaAttention,
+    GemmaConfig,
+    GemmaForCausalLM,
+    GemmaMLP,
+    GemmaModel,
+)
+from mlc_llm.nn import PagedKVCache
 from mlc_llm.support import logging
 from mlc_llm.support import tensor_parallel as tp
-from mlc_llm.support.config import ConfigBase
-from mlc_llm.support.style import bold
 
 logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
-class Gemma2Config(ConfigBase):  # pylint: disable=too-many-instance-attributes
-    """Configuration of the Gemma2 model."""
+class Gemma2Config(GemmaConfig):
+    """Configuration of the Gemma2 model, in addition to the Gemma model"""
 
-    attention_bias: bool
-    #TODO not implemented, but according to https://huggingface.co/blog/gemma2 The Gemma 2 team observed very minor differences when soft-capping is removed during inference.
-    attn_logit_softcapping: float
-    #TODO this is applied at the sampling stage
-    final_logit_softcapping: float
-    head_dim: int
-    hidden_size: int
-    intermediate_size: int
-    num_attention_heads: int
-    num_hidden_layers: int
-    num_key_value_heads: int
-    query_pre_attn_scalar: int
-    rms_norm_eps: float
-    sliding_window_size: int
-    vocab_size: int
-
-    context_window_size: int = 0
-    hidden_activation: Optional[str] = None
-    max_batch_size: int = 1
-    position_embedding_base: int = 0
-    prefill_chunk_size: int = 0
-    tensor_parallel_shards: int = 1
-
-    kwargs: Dict[str, Any] = dataclasses.field(default_factory=dict)
+    # NOTE: We ignore attn_logit_softcapping in the gemma2 implementation for now.
+    # The Gemma 2 team observed minor differences when soft-capping is removed during inference,
+    # according to https://huggingface.co/blog/gemma2.
+    # The soft-capping is also not supported by HuggingFace transformers `Gemma2SdpaAttention`.
+    attn_logit_softcapping: float = None
+    final_logit_softcapping: float = None
+    query_pre_attn_scalar: int = None
+    sliding_window: int = None
 
     def __post_init__(self):
-        if self.hidden_activation is None:
-            self.hidden_activation = self.kwargs.get("hidden_act", None)
-        if self.hidden_activation not in ("gelu", "gelu_pytorch_tanh"):
-            raise ValueError("Only GeLU is supported as the activation for gemma.")
-        if self.attention_bias:
-            raise ValueError('Only "False" attention_bias is supported for gemma')
-        if self.position_embedding_base == 0:
-            if "rope_theta" in self.kwargs:
-                self.position_embedding_base = self.kwargs.pop("rope_theta")
-            else:
-                self.position_embedding_base = 10000
-        if self.context_window_size == 0:
-            for name in ["max_position_embeddings", "max_sequence_length"]:
-                if name in self.kwargs:
-                    self.context_window_size = self.kwargs.pop(name)
-                    logger.info(
-                        "%s not found in config.json. Falling back to %s (%d)",
-                        bold("context_window_size"),
-                        bold(name),
-                        self.context_window_size,
-                    )
-                    break
-            else:
-                raise ValueError(
-                    "Unable to determine the maximum sequence length, because none of "
-                    "`context_window_size`, `max_position_embeddings` or `max_sequence_length` is "
-                    "provided in `config.json`."
-                )
-        assert self.num_attention_heads % self.num_key_value_heads == 0
-        if self.prefill_chunk_size == 0:
-            logger.info(
-                "%s defaults to %d",
-                bold("prefill_chunk_size"),
-                min(self.context_window_size, 2048),
-            )
-            self.prefill_chunk_size = min(self.context_window_size, 2048)
-        elif self.prefill_chunk_size > self.context_window_size:
-            logger.info(
-                "Overriding %s from %d to %d",
-                bold("prefill_chunk_size"),
-                self.prefill_chunk_size,
-                min(self.context_window_size, 2048),
-            )
-            self.prefill_chunk_size = min(self.context_window_size, 2048)
+        super().__post_init__()
+        # NOTE: override the context window size with the Gemma2 sliding window size,
+        # as the sliding window attention every other layer is yet to be supported.
+        self.context_window_size = self.sliding_window
 
 
 # pylint: disable=invalid-name,missing-docstring
 
 
-class Gemma2Embedding(nn.Embedding):
-    """The embedding module specialized for Gemma2 so that
-    it can be shared with the final lm_head.
-    """
-
-    def lm_head_forward(self, x: nn.Tensor):
-        """The lm_head forwarding, which transposes the weight and multiplies
-        with the input tensor.
-        """
-        weight = nn.op.permute_dims(self.weight)
-        return nn.op.matmul(x, weight, out_dtype="float32")
-
-
-class Gemma2MLP(nn.Module):
+class Gemma2Attention(GemmaAttention):
     def __init__(self, config: Gemma2Config):
-        super().__init__()
-        if config.intermediate_size % config.tensor_parallel_shards != 0:
-            raise ValueError(
-                f"Cannot split MLP intermediate size {config.intermediate_size} "
-                f"evenly to {config.tensor_parallel_shards} GPUs."
-            )
-        self.intermediate_size = config.intermediate_size // config.tensor_parallel_shards
-        self.gate_up_proj = nn.Linear(
-            in_features=config.hidden_size,
-            out_features=2 * self.intermediate_size,
-            bias=False,
-        )
-        self.down_proj = nn.Linear(self.intermediate_size, config.hidden_size, bias=False)
-
-    def forward(self, x: Tensor):
-        concat_x1_x2 = self.gate_up_proj(x)
-        x1, x2 = op.split(concat_x1_x2, 2, axis=-1)
-        return self.down_proj(op.gelu(x1, approximate="tanh") * x2)
-
-
-class Gemma2Attention(nn.Module):  # pylint: disable=too-many-instance-attributes
-    def __init__(self, config: Gemma2Config):
-        self.head_dim = config.head_dim
-        self.scaling_factor = config.query_pre_attn_scalar**-0.5
-        self.num_q_heads = config.num_attention_heads // config.tensor_parallel_shards
-        assert (
-            config.num_key_value_heads % config.tensor_parallel_shards == 0
-        ), f"num_kv_heads({config.num_key_value_heads}) must be divisible by tensor_parallel_shards"
-        assert (
-            config.num_key_value_heads >= config.tensor_parallel_shards
-        ), f"Too large tensor_parallel_shards, must be smaller than {config.num_key_value_heads}"
-        self.num_kv_heads = config.num_key_value_heads // config.tensor_parallel_shards
-        self.qkv_proj = nn.Linear(
-            in_features=config.hidden_size,
-            out_features=(self.num_q_heads + 2 * self.num_kv_heads) * self.head_dim,
-            bias=config.attention_bias,
-        )
-        self.o_proj = nn.Linear(
-            in_features=self.num_q_heads * self.head_dim,
-            out_features=config.hidden_size,
-            bias=config.attention_bias,
-        )
-
-    def forward(self, hidden_states: Tensor, paged_kv_cache: PagedKVCache, layer_id: int):
-        d, h_q, h_kv = self.head_dim, self.num_q_heads, self.num_kv_heads
-        b, s, _ = hidden_states.shape
-        # QKV Projection
-        qkv = self.qkv_proj(hidden_states)
-        qkv = op.reshape(qkv, (b, s, h_q + h_kv + h_kv, d))
-        # Attention
-        output = op.reshape(
-            paged_kv_cache.attention_with_fused_qkv(layer_id, qkv, self.num_q_heads, self.scaling_factor),
-            (b, s, h_q * d),
-        )
-        return self.o_proj(output)
+        super().__init__(config)
+        self.scaling_factor = (config.head_dim / config.query_pre_attn_scalar) ** 0.5
 
 
 class Gemma2DecoderLayer(nn.Module):
     def __init__(self, config: Gemma2Config):
         rms_norm_eps = config.rms_norm_eps
         self.self_attn = Gemma2Attention(config)
-        self.mlp = Gemma2MLP(config)
+        self.mlp = GemmaMLP(config)
         # Gemma RMSNorm adds 1 to the weights. It is already fused in the loader
         self.input_layernorm = nn.RMSNorm(config.hidden_size, -1, rms_norm_eps, bias=False)
         self.post_attention_layernorm = nn.RMSNorm(config.hidden_size, -1, rms_norm_eps, bias=False)
-        self.pre_feedforward_layernorm = nn.RMSNorm(config.hidden_size, -1, rms_norm_eps, bias=False)
-        self.post_feedforward_layernorm = nn.RMSNorm(config.hidden_size, -1, rms_norm_eps, bias=False)
+        self.pre_feedforward_layernorm = nn.RMSNorm(
+            config.hidden_size, -1, rms_norm_eps, bias=False
+        )
+        self.post_feedforward_layernorm = nn.RMSNorm(
+            config.hidden_size, -1, rms_norm_eps, bias=False
+        )
 
         def _set_tp():
             def _set(layer, hint):
@@ -198,203 +82,38 @@ class Gemma2DecoderLayer(nn.Module):
 
     def forward(self, hidden_states: Tensor, paged_kv_cache: PagedKVCache, layer_id: int):
         out = self.self_attn(self.input_layernorm(hidden_states), paged_kv_cache, layer_id)
-        out = self.post_attention_layernorm(out)
-        hidden_states = self._apply_residual(out, residual=hidden_states)
+        out = self._apply_post_matmul_norm(out, norm=self.post_attention_layernorm)
+        hidden_states = out + hidden_states
 
         out = self.pre_feedforward_layernorm(hidden_states)
         out = self.mlp(out)
-        out = self.post_feedforward_layernorm(out)
-        hidden_states = self._apply_residual(out, residual=hidden_states)
+        out = self._apply_post_matmul_norm(out, norm=self.post_feedforward_layernorm)
+        hidden_states = out + hidden_states
 
         return hidden_states
 
-    def _apply_residual(self, out, residual):
+    def _apply_post_matmul_norm(self, out: Tensor, norm: nn.Tensor):
         if self.tensor_parallel_shards > 1:
-            return op.ccl_allreduce(out, "sum") + residual
-        return out + residual
+            return norm(op.ccl_allreduce(out, "sum"))
+        return norm(out)
 
 
-class Gemma2Model(nn.Module):
+class Gemma2Model(GemmaModel):
     def __init__(self, config: Gemma2Config):
-        self.hidden_size = config.hidden_size
-        assert config.hidden_size % config.num_attention_heads == 0
-        self.embed_tokens = Gemma2Embedding("vocab_size", config.hidden_size)
+        super().__init__(config)
         self.layers = nn.ModuleList(
             [Gemma2DecoderLayer(config) for _ in range(config.num_hidden_layers)]
         )
-        self.norm = nn.RMSNorm(config.hidden_size, -1, config.rms_norm_eps, bias=False)
-
-    def forward(self, input_embed: Tensor, paged_kv_cache: PagedKVCache):
-        hidden_states = input_embed
-        hidden_states = hidden_states * (self.hidden_size**0.5)
-        for layer_id, layer in enumerate(self.layers):
-            hidden_states = layer(hidden_states, paged_kv_cache, layer_id)
-        hidden_states = self.norm(hidden_states)
-        return hidden_states
 
 
-class Gemma2ForCausalLM(nn.Module):  # pylint: disable=too-many-instance-attributes
+class Gemma2ForCausalLM(GemmaForCausalLM):  # pylint: disable=too-many-instance-attributes
     def __init__(self, config: Gemma2Config):
+        super().__init__(config)
         self.model = Gemma2Model(config)
-        self.num_hidden_layers = config.num_hidden_layers
-        self.num_attention_heads = config.num_attention_heads
-        self.num_key_value_heads = config.num_key_value_heads
-        self.head_dim = config.head_dim
-        self.hidden_size = config.hidden_size
-        self.vocab_size = config.vocab_size
-        self.rope_theta = config.position_embedding_base
-        self.tensor_parallel_shards = config.tensor_parallel_shards
-        self.dtype = "float32"
+        self.final_logit_softcapping = config.final_logit_softcapping
 
-    def to(self, dtype: Optional[str] = None):
-        super().to(dtype=dtype)
-        if dtype is not None:
-            self.dtype = dtype
-
-    def batch_forward(
-        self,
-        input_embeds: Tensor,
-        paged_kv_cache: PagedKVCache,
-        logit_positions: Optional[Tensor] = None,
-    ):
-        op_ext.configure()
-
-        hidden_states = self.model(input_embeds, paged_kv_cache)
-        if logit_positions is not None:
-            hidden_states = op.take(hidden_states, logit_positions, axis=1)
-        logits = self.model.embed_tokens.lm_head_forward(hidden_states)
-        if logits.dtype != "float32":
-            logits = logits.astype("float32")
+    def get_logits(self, hidden_states: Tensor):
+        logits = super().get_logits(hidden_states)
+        if self.final_logit_softcapping is not None:
+            logits = op.tanh(logits / self.final_logit_softcapping) * self.final_logit_softcapping
         return logits
-
-    def embed(self, input_ids: Tensor):
-        if self.tensor_parallel_shards > 1:
-            input_ids = op.ccl_broadcast_from_worker0(input_ids)
-        return self.model.embed_tokens(input_ids)
-
-    def prefill(self, input_embed: Tensor, paged_kv_cache: PagedKVCache):
-        op_ext.configure()
-
-        def _index(x: te.Tensor):  # x[:-1,:]
-            b, s, d = x.shape
-            return te.compute((b, 1, d), lambda i, _, k: x[i, s - 1, k], name="index")
-
-        hidden_states = self.model(input_embed, paged_kv_cache)
-        hidden_states = op.tensor_expr_op(_index, name_hint="index", args=[hidden_states])
-        logits = self.model.embed_tokens.lm_head_forward(hidden_states)
-        if logits.dtype != "float32":
-            logits = logits.astype("float32")
-        return logits, paged_kv_cache
-
-    def decode(self, input_embed: Tensor, paged_kv_cache: PagedKVCache):
-        op_ext.configure()
-
-        hidden_states = self.model(input_embed, paged_kv_cache)
-        logits = self.model.embed_tokens.lm_head_forward(hidden_states)
-        if logits.dtype != "float32":
-            logits = logits.astype("float32")
-        return logits, paged_kv_cache
-
-    def batch_prefill(
-        self, input_embeds: Tensor, logit_positions: Tensor, paged_kv_cache: PagedKVCache
-    ):
-        if self.tensor_parallel_shards > 1:
-            logit_positions = op.ccl_broadcast_from_worker0(logit_positions)
-        logits = self.batch_forward(input_embeds, paged_kv_cache, logit_positions)
-        return logits, paged_kv_cache
-
-    def batch_decode(self, input_embeds: Tensor, paged_kv_cache: PagedKVCache):
-        logits = self.batch_forward(input_embeds, paged_kv_cache)
-        return logits, paged_kv_cache
-
-    def batch_verify(self, input_embeds: Tensor, paged_kv_cache: PagedKVCache):
-        logits = self.batch_forward(input_embeds, paged_kv_cache)
-        return logits, paged_kv_cache
-
-    def create_paged_kv_cache(  # pylint: disable=too-many-arguments
-        self,
-        max_batch_size: tir.Var,
-        max_total_seq_len: tir.Var,
-        prefill_chunk_size: tir.Var,
-        page_size: tir.Var,
-        support_sliding_window: tir.Var,
-    ) -> PagedKVCache:
-        return PagedKVCache.create_generic(
-            max_batch_size=max_batch_size,
-            max_total_seq_len=max_total_seq_len,
-            prefill_chunk_size=prefill_chunk_size,
-            page_size=page_size,
-            support_sliding_window=support_sliding_window,
-            num_hidden_layers=self.num_hidden_layers,
-            num_attention_heads=self.num_attention_heads // self.tensor_parallel_shards,
-            num_key_value_heads=self.num_key_value_heads // self.tensor_parallel_shards,
-            head_dim=self.head_dim,
-            rope_mode=RopeMode.NORMAL,
-            rope_scale=1,
-            rope_theta=self.rope_theta,
-            dtype=self.dtype,
-        )
-
-    def get_default_spec(self):
-        mod_spec = {
-            "embed": {
-                "input_ids": nn.spec.Tensor(["seq_len"], "int32"),
-                "$": {
-                    "param_mode": "packed",
-                    "effect_mode": "none",
-                },
-            },
-            "prefill": {
-                "input_embed": nn.spec.Tensor([1, "seq_len", self.hidden_size], self.dtype),
-                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
-                "$": {
-                    "param_mode": "packed",
-                    "effect_mode": "none",
-                },
-            },
-            "decode": {
-                "input_embed": nn.spec.Tensor([1, 1, self.hidden_size], self.dtype),
-                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
-                "$": {
-                    "param_mode": "packed",
-                    "effect_mode": "none",
-                },
-            },
-            "batch_prefill": {
-                "input_embeds": nn.spec.Tensor([1, "seq_len", self.hidden_size], self.dtype),
-                "logit_positions": nn.spec.Tensor(["batch_size"], "int32"),
-                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
-                "$": {
-                    "param_mode": "packed",
-                    "effect_mode": "none",
-                },
-            },
-            "batch_decode": {
-                "input_embeds": nn.spec.Tensor(["batch_size", 1, self.hidden_size], self.dtype),
-                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
-                "$": {
-                    "param_mode": "packed",
-                    "effect_mode": "none",
-                },
-            },
-            "batch_verify": {
-                "input_embeds": nn.spec.Tensor([1, "seq_len", self.hidden_size], self.dtype),
-                "paged_kv_cache": nn.spec.Object(object_type=PagedKVCache),
-                "$": {
-                    "param_mode": "packed",
-                    "effect_mode": "none",
-                },
-            },
-            "create_paged_kv_cache": {
-                "max_batch_size": int,
-                "max_total_seq_len": int,
-                "prefill_chunk_size": int,
-                "page_size": int,
-                "support_sliding_window": int,
-                "$": {
-                    "param_mode": "none",
-                    "effect_mode": "none",
-                },
-            },
-        }
-        return nn.spec.ModuleSpec.from_raw(mod_spec, self)
