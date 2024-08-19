1c4dfcf68be1ba018ea1ebdf5e2695c263a5bb3e
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-08-19 11:44:16 -0400

    [MultiGPU] Fix memory estimation for pipeline parallelism
    This PR fixes the memory estimation when pipeline parallelism
    is enabled.

--- cpp/serve/config.cc
+++ cpp/serve/config.cc
@@ -729,6 +729,7 @@ Result<InferrableEngineConfig> InferrableEngineConfig::InferForKVCache(
       }
       params_bytes += param_size;
     }
+    params_bytes /= metadata.pipeline_parallel_stages;
     for (const auto& [func_name, temp_buffer_size] : metadata.memory_usage) {
       temp_buffer_bytes = std::max(temp_buffer_bytes, temp_buffer_size);
     }
