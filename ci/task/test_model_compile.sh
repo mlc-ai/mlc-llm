b8838a1a9a6a40ec8c6ee81503b716cca75e44ef
Ruihang Lai <ruihangl@cs.cmu.edu>
2025-01-20 13:02:53 -0500

    [Fix] Add `--no-index` to pip install commands (#3096)
    This PR adds the field `--no-index` to mlc-ai/mlc-llm pip install
    commands to bypass PyPI.

--- ci/task/test_model_compile.sh
+++ ci/task/test_model_compile.sh
@@ -9,17 +9,17 @@ pip install --force-reinstall wheels/*.whl
 
 if [[ ${GPU} == cuda* ]]; then
     TARGET=cuda
-    pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cu123
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-cu123
     export LD_LIBRARY_PATH=/usr/local/cuda/compat/:$LD_LIBRARY_PATH
 elif [[ ${GPU} == rocm* ]]; then
     TARGET=rocm
-    pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-rocm57
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-rocm57
 elif [[ ${GPU} == metal ]]; then
     TARGET=metal
-    pip install --pre -U --force-reinstal -f https://mlc.ai/wheels mlc-ai-nightly-cpu
+    pip install --pre -U --force-reinstall -f https://mlc.ai/wheels mlc-ai-nightly-cpu
 elif [[ ${GPU} == wasm* ]]; then
     TARGET=wasm
-    pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cpu
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-cpu
     export TVM_SOURCE_DIR=$(dirname $(python -c 'import tvm; print(tvm.__file__)'))
     export TVM_HOME=${TVM_SOURCE_DIR}
     export MLC_LLM_SOURCE_DIR=$(pwd)
@@ -27,14 +27,14 @@ elif [[ ${GPU} == wasm* ]]; then
     cd $MLC_LLM_SOURCE_DIR/web/ && make -j${NUM_THREADS} && cd -
 elif [[ ${GPU} == ios ]]; then
     TARGET=ios
-    pip install --pre -U --force-reinstal -f https://mlc.ai/wheels mlc-ai-nightly-cpu
+    pip install --pre -U --force-reinstall -f https://mlc.ai/wheels mlc-ai-nightly-cpu
 elif [[ ${GPU} == android* ]]; then
     TARGET=android
-    pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cpu
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-cpu
     source /android_env_vars.sh
 else
     TARGET=vulkan
-    pip install --pre -U -f https://mlc.ai/wheels mlc-ai-nightly-cpu
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-cpu
 fi
 
 python tests/python/integration/test_model_compile.py $TARGET $NUM_THREADS
