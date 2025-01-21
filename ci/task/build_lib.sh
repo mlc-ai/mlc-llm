b7a702d48bc877e35ee3b3236dba4f4282907cc9
Johnny <johnnynuca14@gmail.com>
2025-01-21 22:49:53 +0100

    Blackwell Support Codegen

--- ci/task/build_lib.sh
+++ ci/task/build_lib.sh
@@ -24,7 +24,7 @@ if [[ ${GPU} == rocm* ]]; then
 elif [[ ${GPU} == cuda* ]]; then
     echo set\(USE_VULKAN ON\) >>config.cmake
     echo set\(CMAKE_CUDA_COMPILER_LAUNCHER ccache\) >>config.cmake
-    echo set\(CMAKE_CUDA_ARCHITECTURES "80;90"\) >>config.cmake
+    echo set\(CMAKE_CUDA_ARCHITECTURES "80;90;100;120"\) >>config.cmake
     echo set\(CMAKE_CUDA_FLAGS \"\$\{CMAKE_CUDA_FLAGS\} -t $NUM_THREADS\"\) >>config.cmake
     echo set\(USE_CUDA ON\) >>config.cmake
     echo set\(USE_CUBLAS ON\) >>config.cmake
