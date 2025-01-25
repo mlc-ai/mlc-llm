1ec0c6e2b2597023424b2370704f02216ed7c2d3
johnnynunez <johnnynuca14@gmail.com>
2025-01-25 11:15:38 +0100

    update

--- ci/task/test_model_compile.sh
+++ ci/task/test_model_compile.sh
@@ -9,11 +9,11 @@ pip install --force-reinstall wheels/*.whl
 
 if [[ ${GPU} == cuda* ]]; then
     TARGET=cuda
-    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-cu123
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-cu128
     export LD_LIBRARY_PATH=/usr/local/cuda/compat/:$LD_LIBRARY_PATH
 elif [[ ${GPU} == rocm* ]]; then
     TARGET=rocm
-    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-rocm57
+    pip install --pre -U --no-index -f https://mlc.ai/wheels mlc-ai-nightly-rocm63
 elif [[ ${GPU} == metal ]]; then
     TARGET=metal
     pip install --pre -U --force-reinstall -f https://mlc.ai/wheels mlc-ai-nightly-cpu
