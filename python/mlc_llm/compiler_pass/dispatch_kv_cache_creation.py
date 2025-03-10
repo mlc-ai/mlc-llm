a8b62645ad16754295e15cb65c147f50f60c5d4d
Akshat Sehgal <a.sehgal@samsung.com>
2025-03-10 10:09:44 -0700

    Make  argument optional in

--- python/mlc_llm/compiler_pass/dispatch_kv_cache_creation.py
+++ python/mlc_llm/compiler_pass/dispatch_kv_cache_creation.py
@@ -179,8 +179,9 @@ class DispatchKVCacheCreation:  # pylint: disable=too-many-instance-attributes
             "support_sliding_window_", relax.ShapeStructInfo([kwargs["support_sliding_window"]])
         )
 
-        # Remove the 'enable_disaggregation' argument
-        kwargs.pop("enable_disaggregation", None)
+        # Ensure 'enable_disaggregation' is optional
+        enable_disaggregation = kwargs.pop("enable_disaggregation", False)
+        kwargs["enable_disaggregation"] = enable_disaggregation
 
         with bb.function(
             name="create_tir_paged_kv_cache",
