027169ad6dcfb591a9f5b92f4db2d91b3efb8226
davidlightmysterion <ca2r0txb0y@gmail.com>
2025-04-18 19:24:16 +0800

    [Fix] clang-format

--- cpp/json_ffi/conv_template.cc
+++ cpp/json_ffi/conv_template.cc
@@ -328,8 +328,7 @@ Result<std::vector<Data>> CreatePrompt(const Conversation& conv,
 
             NDArray image_data = image_data_res.Unwrap();
             std::vector<int64_t> new_shape = {1, image_size, image_size, 3};
-            NDArray image_ndarray =
-                image_data.CreateView(new_shape, image_data.DataType());
+            NDArray image_ndarray = image_data.CreateView(new_shape, image_data.DataType());
             // TODO: Not sure if commenting will affect other functions. But
             // python part will do clip preprocessing. auto image_ndarray =
             // ClipPreprocessor(image_data_res.Unwrap(), image_size, device);
