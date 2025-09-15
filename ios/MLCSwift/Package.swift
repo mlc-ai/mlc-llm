c2af978e1a04dcda8aa3f77238e889c627d479c3
Ruihang Lai <ruihangl@cs.cmu.edu>
2025-09-15 16:40:53 -0400

    [iOS][Android] Update FFI include path
    This PR updates the FFI include path for android and ios build.

--- ios/MLCSwift/Package.swift
+++ ios/MLCSwift/Package.swift
@@ -18,8 +18,8 @@ let package = Package(
             path: "Sources/ObjC",
             cxxSettings: [
                 .headerSearchPath("../../tvm_home/include"),
-                .headerSearchPath("../../tvm_home/ffi/include"),
-                .headerSearchPath("../../tvm_home/ffi/3rdparty/dlpack/include"),
+                .headerSearchPath("../../tvm_home/3rdparty/tvm-ffi/include"),
+                .headerSearchPath("../../tvm_home/3rdparty/tvm-ffi/3rdparty/dlpack/include"),
                 .headerSearchPath("../../tvm_home/3rdparty/dmlc-core/include")
             ]
         ),
