10a76c7ce0a63cbd1dbebb75393ac007fa11a25e
yash solanki <alphacr792@gmail.com>
2026-02-09 13:10:19 +0530

    fix: remove stale group_size reference in activation scale broadcast

--- python/mlc_llm/quantization/block_scale_quantization.py
+++ python/mlc_llm/quantization/block_scale_quantization.py
@@ -739,8 +739,6 @@ def broadcast_activation_scale(
     transpose: bool,
 ) -> nn.Tensor:
     """Broadcast stored activation scales."""
-
-    _ = group_size
     reshape_shape = (1,) * (x.ndim - 1) + (activation_scale.shape[0],)
     scale = nn.op.reshape(activation_scale, reshape_shape)
     scale = nn.op.broadcast_to(scale, (*x.shape[:-1], activation_scale.shape[0]))
